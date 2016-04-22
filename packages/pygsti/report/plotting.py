719442743b8cecbf147705c68bae361980e9140d
Travis Scholten <travisscholten@gmail.com>
2016-04-22 13:14:28 -0600

    Create new MidPointNorm class
    For our diverging colormaps, it is helpful to specifically
    set the midpoint of the colormap at 0. This commit teaches
    the plotting module a new normalization class, called
    MidPointNorm. This norm will take as input a midpoint,
    and create a matplotlib Normalization class instance
    where the midpoint is mapped to .5 in the norm.
    
    This new norm is useful when we use the "bwr" diverging
    colormap, which goes from blue -> white -> red. With
    the new norm, we can ensure the midpoint of our data
    (which we often want to be 0) is mapped to white, and
    low (high) values are mapped towards blue (red).

--- packages/pygsti/report/plotting.py
+++ packages/pygsti/report/plotting.py
@@ -364,6 +364,76 @@ class LinLogNorm(_matplotlib.colors.Normalize):
         else:
             return return_value.view(_np.ma.MaskedArray)
 
+class MidPointNorm(_matplotlib.colors.Normalize):
+    """
+    A class for normalizing data which takes on
+    positive and negative values.
+
+    Taken from http://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+    """
+
+    def __init__(self, midpoint=0, vmin=None, vmax=None, clip=False):
+        super(MidPointNorm, self).__init__(vmin=vmin, vmax=vmax, clip=clip)
+        self.midpoint = midpoint
+
+    def __call__(self, value, clip=None):
+        if clip is None:
+            clip = self.clip
+
+        result, is_scalar = self.process_value(value)
+
+        self.autoscale_None(result)
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if not (vmin < midpoint < vmax):
+            raise ValueError("midpoint must be between maxvalue and minvalue.")
+        elif vmin == vmax:
+            result.fill(0) # Or should it be all masked? Or 0.5?
+        elif vmin > vmax:
+            raise ValueError("maxvalue must be bigger than minvalue")
+        else:
+            vmin = float(vmin)
+            vmax = float(vmax)
+            if clip:
+                mask = _np.ma.getmask(result)
+                result = _np.ma.array(_np.clip(result.filled(vmax), vmin, vmax),
+                                  mask=mask)
+
+            # ma division is very slow; we can take a shortcut
+            resdat = result.data
+
+            #First scale to -1 to 1 range, than to from 0 to 1.
+            resdat -= midpoint
+            resdat[resdat>0] /= abs(vmax - midpoint)
+            resdat[resdat<0] /= abs(vmin - midpoint)
+
+            resdat /= 2.
+            resdat += 0.5
+            result = _np.ma.array(resdat, mask=result.mask, copy=False)
+
+        if is_scalar:
+            result = result[0]
+        return result
+
+    def inverse(self, value):
+        if not self.scaled():
+            raise ValueError("Not invertible until scaled")
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if _matplotlib.cbook.iterable(value):
+            val = _np.ma.asarray(value)
+            val = 2 * (val-0.5)
+            val[val>0]  *= abs(vmax - midpoint)
+            val[val<0] *= abs(vmin - midpoint)
+            val += midpoint
+            return val
+        else:
+            val = 2 * (val - 0.5)
+            if val < 0:
+                return  val*abs(vmin-midpoint) + midpoint
+            else:
+                return  val*abs(vmax-midpoint) + midpoint
+
 def splice_cmaps(cmaps, name=None, splice_points=None):
     """
     Take a list of cmaps and create a new cmap that joins them at specified
