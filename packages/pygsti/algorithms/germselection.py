1a629041cfb5b96071d6d54c559f8676786c9b3c
Kenny Rudinger <kmrudin@sandia.gov>
2016-05-18 10:24:53 -0600

    Fixed std1Q_XY germs; updated germselection.py

--- packages/pygsti/algorithms/germselection.py
+++ packages/pygsti/algorithms/germselection.py
@@ -12,6 +12,15 @@ import math as _math
 import sys as _sys
 import warnings as _warnings
 
+def num_non_spam_gauge_params(gateset):
+    """Returns number of non-gauge parameters in a gateset, not including SPAM parameters"""
+    gateset = gateset.copy()
+    for prepLabel in gateset.preps.keys():  
+        del gateset.preps[prepLabel]
+    for effectLabel in gateset.effects.keys():  
+        del gateset.effects[effectLabel]
+    return gateset.num_gauge_params()
+
 
 #def _PerfectTwirl(mxToTwirl,wrt,eps):
 #    """ Perform twirl on mxToTwirl with respect to wrt """
@@ -217,6 +226,7 @@ def test_germ_list_finitel(gateset, germsToTest, L, weights=None,
 
 
 
+
 def test_germ_list_infl(gateset, germsToTest, weights=None, 
                            returnSpectrum=False, tol=1e-6, check=False):
     """
@@ -261,6 +271,14 @@ def test_germ_list_infl(gateset, germsToTest, weights=None,
         matrix used to determine parameter amplification.
     """
 
+    #Remove any SPAM vectors from gateset since we only want
+    # to consider the set of *gate* parameters for amplification
+    # and this makes sure our parameter counting is correct
+    gateset = gateset.copy()
+    for prepLabel in gateset.preps.keys():  del gateset.preps[prepLabel]
+    for effectLabel in gateset.effects.keys():  del gateset.effects[effectLabel]
+
+
     germLengths = _np.array( map(len,germsToTest), 'i')
     twirledDeriv = bulk_twirled_deriv(gateset, germsToTest, tol, check) / germLengths[:,None,None]
     twirledDerivDaggerDeriv = _np.einsum('ijk,ijl->ikl', _np.conjugate(twirledDeriv), twirledDeriv) #is conjugate needed? -- all should be real
@@ -280,10 +298,10 @@ def test_germ_list_infl(gateset, germsToTest, weights=None,
     
     return (bSuccess,sortedEigenvals) if returnSpectrum else bSuccess
 
-
 def optimize_integer_germs_slack(gateset, germsList, initialWeights=None, 
                                  maxIter=100, fixedSlack=False, slackFrac=False, 
                                  returnAll=False, tol=1e-6, check=False,
+                                 forceSingletons = True, forceSingletonsScore = 1e100,
                                  verbosity=1):
     """
     Find a locally optimal subset of the germs in germsList.
@@ -301,6 +319,8 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
 
     germsList : list of GateStrings
         List of all germs gate sequences to consider.
+        IMPORTANT:  If forceSingletons is True, the first k elements of
+        germsList must be all k gates in gateset.
 
     initialWeights : list-like
         List or array of either booleans or (0 or 1) integers
@@ -332,6 +352,15 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
       Whether to perform internal consistency checks, at the
       expense of making the function slower.
 
+    forceSingletons : bool, optional (default is True)
+        Whether or not to force all germ sets to contain each gate as a germ.
+        IMPORTANT:  This only works if, for a gate set of k gates, the first k elements
+        of germsList are the k gates.
+    
+    forceSingletonsScore : float, optional (default is 1e100)
+        When forceSingletons is True, what score to assign any germ set
+        that does not contain each gate as a germ.
+
     verbosity : int, optional
         Integer >= 0 indicating the amount of detail to print.
 
@@ -351,6 +380,14 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
         specifying a subset of germs, and values == 1.0/smallest-non-gauge-
         eigenvalue "scores".  
     """
+    
+    #Remove any SPAM vectors from gateset since we only want
+    # to consider the set of *gate* parameters for amplification
+    # and this makes sure our parameter counting is correct
+    gateset = gateset.copy()
+    for prepLabel in gateset.preps.keys():  del gateset.preps[prepLabel]
+    for effectLabel in gateset.effects.keys():  del gateset.effects[effectLabel]
+    
     if (fixedSlack and slackFrac) or (not fixedSlack and not slackFrac):
         raise ValueError("Either fixedSlack *or* slackFrac should be specified")
     lessWeightOnly = False  #Initially allow adding to weight. -- maybe make this an argument??
@@ -366,7 +403,7 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
     #  keys = tuple-ized weight vector of 1's and 0's only
     #  values = 1.0/critical_eval
     scoreD = {} 
-
+    numGates = len(gateset.gates.keys())
     #twirledDerivDaggerDeriv == array J.H*J contributions from each germ (J=Jacobian)
     # indexed by (iGerm, iGatesetParam1, iGatesetParam2)
     # size (nGerms, vec_gateset_dim, vec_gateset_dim)
@@ -375,10 +412,15 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
     twirledDerivDaggerDeriv = _np.einsum('ijk,ijl->ikl', _np.conjugate(twirledDeriv), twirledDeriv)
     
     def compute_score(wts):
-        """ Returns the 1/(first non-gauge eigenvalue) == a "score" in which smaller is better """
-        combinedTDDD = _np.einsum('i,ijk',wts,twirledDerivDaggerDeriv)
-        sortedEigenvals = _np.sort(_np.real(_np.linalg.eigvalsh(combinedTDDD)))
-        score = 1.0/sortedEigenvals[nGaugeParams]
+        """ Returns the 1/(first non-gauge eigenvalue) == a "score" in which smaller is better.
+        Also returns intentionally bad score if wts do not include all individual gates as
+        individual germs, if forceSingletons is True. """
+        if forceSingletons and _np.count_nonzero(wts[:numGates]) != numGates:
+            score = forceSingletonsScore
+        else:
+            combinedTDDD = _np.einsum('i,ijk',wts,twirledDerivDaggerDeriv)
+            sortedEigenvals = _np.sort(_np.real(_np.linalg.eigvalsh(combinedTDDD)))
+            score = 1.0/sortedEigenvals[nGaugeParams]
         scoreD[tuple(wts)] = score # side affect: calling compute_score caches result in scoreD
         return score
 
@@ -405,6 +447,9 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
         
         bFoundBetterNeighbor = False
         for neighborNum, neighbor in enumerate(get_neighbors(weights)):
+#            if force_singletons:
+#                if _np.count_nonzeros(neighbor[:numGates]) != numGates
+#                    continue
             if tuple(neighbor) not in scoreD_keys:
                 neighborL1 = sum(neighbor)
                 neighborScore = compute_score(neighbor)
@@ -443,6 +488,235 @@ def optimize_integer_germs_slack(gateset, germsList, initialWeights=None,
                 break #end main for loop
         
         print "Moving to better neighbor"
+    else:
+        print "Hit max. iterations"
+    
+    print "score = ", score
+    print "weights = ",weights
+    print "L1(weights) = ",sum(weights)
+
+    goodGermsList = []
+    for index,val in enumerate(weights):
+        if val==1:
+            goodGermsList.append(germsList[index])
+
+    if returnAll:
+        return goodGermsList, weights, scoreD
+    else:
+        return goodGermsList
+
+def optimize_integer_germs_slack_multiple_gatesets(gatesetList, germsList, initialWeights=None, 
+                                 maxIter=100, fixedSlack=False, slackFrac=False, 
+                                 returnAll=False, tol=1e-6, check=False,
+                                 forceSingletons = True, forceSingletonsScore = 1e100,
+                                 verbosity=1):
+    """
+    Find a locally optimal subset of the germs in germsList.
+
+    Locally optimal here means that no single germ can be excluded
+    without making the smallest non-gauge eigenvalue of the 
+    Jacobian.H*Jacobian matrix smaller, i.e. less amplified,
+    by more than a fixed or variable amount of "slack", as
+    specified by fixedSlack or slackFrac.
+
+    Parameters
+    ----------
+    gateset : GateSet
+        The gate set (associates gate matrices with gate labels).
+
+    germsList : list of GateStrings
+        List of all germs gate sequences to consider.
+        IMPORTANT:  If forceSingletons is True, the first k elements of
+        germsList must be all k gates in gateset.
+
+    initialWeights : list-like
+        List or array of either booleans or (0 or 1) integers
+        specifying which germs in germList comprise the initial
+        germ set.  If None, then starting point includes all
+        germs.
+
+    maxIter : int, optional
+        The maximum number of iterations before giving up.
+
+    fixedSlack : float, optional
+        If not None, a floating point number which specifies that excluding a 
+        germ is allowed to increase 1.0/smallest-non-gauge-eigenvalue by
+        fixedSlack.  You must specify *either* fixedSlack or slackFrac.
+
+    slackFrac : float, optional
+        If not None, a floating point number which specifies that excluding a 
+        germ is allowed to increase 1.0/smallest-non-gauge-eigenvalue by
+        fixedFrac*100 percent.  You must specify *either* fixedSlack or slackFrac.
+
+    returnAll : bool, optional
+        If True, return the final "weights" vector and score dictionary
+        in addition to the optimal germ list (see below).
+
+    tol : float, optional
+        Tolerance used for eigenvector degeneracy testing in twirling operation.
+
+    check : bool, optional
+      Whether to perform internal consistency checks, at the
+      expense of making the function slower.
+
+    forceSingletons : bool, optional (default is True)
+        Whether or not to force all germ sets to contain each gate as a germ.
+        IMPORTANT:  This only works if, for a gate set of k gates, the first k elements
+        of germsList are the k gates.
+    
+    forceSingletonsScore : float, optional (default is 1e100)
+        When forceSingletons is True, what score to assign any germ set
+        that does not contain each gate as a germ.
+
+    verbosity : int, optional
+        Integer >= 0 indicating the amount of detail to print.
+
+
+    Returns
+    -------
+    finalGermList : list
+        Sublist of germList specifying the final, optimal, set of germs.
+
+    weights : array
+        Integer array, of length len(germList), containing 0s and 1s to
+        indicate which elements of germList were chosen as finalGermList.
+        Only returned when returnAll == True.
+
+    scoreDictionary : dict
+        Dictionary with keys == tuples of 0s and 1s of length len(germList),
+        specifying a subset of germs, and values == 1.0/smallest-non-gauge-
+        eigenvalue "scores".  
+    """
+    
+    #Remove any SPAM vectors from gateset since we only want
+    # to consider the set of *gate* parameters for amplification
+    # and this makes sure our parameter counting is correct
+    
+    num_gatesets = len(gatesetList)
+    
+    gateset0 = gatesetList[0].copy()
+    for prepLabel in gateset0.preps.keys():  del gateset0.preps[prepLabel]
+    for effectLabel in gateset0.effects.keys():  del gateset0.effects[effectLabel]
+    
+    if (fixedSlack and slackFrac) or (not fixedSlack and not slackFrac):
+        raise ValueError("Either fixedSlack *or* slackFrac should be specified")
+    lessWeightOnly = False  #Initially allow adding to weight. -- maybe make this an argument??
+
+    nGaugeParams = gateset0.num_gauge_params()
+    nGerms = len(germsList)
+
+    if verbosity > 0:
+        print "Starting germ set optimization. Lower score is better."
+        print "Gateset has %d gauge params." % nGaugeParams
+
+    #score dictionary:
+    #  keys = tuple-ized weight vector of 1's and 0's only
+    #  values = 1.0/critical_eval
+    scoreD = {} 
+    numGates = len(gateset0.gates.keys())
+    #twirledDerivDaggerDeriv == array J.H*J contributions from each germ (J=Jacobian)
+    # indexed by (iGerm, iGatesetParam1, iGatesetParam2)
+    # size (nGerms, vec_gateset_dim, vec_gateset_dim)
+    
+    germLengths = _np.array( map(len,germsList), 'i')
+    
+    twirledDerivDaggerDerivList = []
+    
+    for gateset in gatesetList:
+        twirledDeriv = bulk_twirled_deriv(gateset, germsList, tol, check) / germLengths[:,None,None]        
+        twirledDerivDaggerDerivList.append(_np.einsum('ijk,ijl->ikl', _np.conjugate(twirledDeriv), twirledDeriv))
+    
+    def compute_score(wts,gateset_num):
+        """ Returns the 1/(first non-gauge eigenvalue) == a "score" in which smaller is better.
+        Also returns intentionally bad score if wts do not include all individual gates as
+        individual germs, if forceSingletons is True. """
+        if forceSingletons and _np.count_nonzero(wts[:numGates]) != numGates:
+            score = forceSingletonsScore
+        else:
+            combinedTDDD = _np.einsum('i,ijk',wts,twirledDerivDaggerDerivList[gateset_num])
+            sortedEigenvals = _np.sort(_np.real(_np.linalg.eigvalsh(combinedTDDD)))
+#            print sortedEigenvals
+            score = 1.0/sortedEigenvals[nGaugeParams]
+#            print gateset_num, score
+        scoreD[gateset_num,tuple(wts)] = score # side affect: calling compute_score caches result in scoreD
+        return score
+
+    def get_neighbors(boolVec):
+        for i in xrange(nGerms):
+            v = boolVec.copy()
+            v[i] = (v[i] + 1) % 2 #toggle v[i] btwn 0 and 1
+            yield v
+
+    if initialWeights is not None:
+        weights = _np.array( [1 if x else 0 for x in initialWeights ] )
+    else:
+        weights = _np.ones( nGerms, 'i' ) #default: start with all germs
+        lessWeightOnly = True #we're starting at the max-weight vector
+
+    scoreList = [compute_score(weights,gateset_num) for gateset_num in xrange(num_gatesets)]
+    score = _np.max(scoreList)
+#    print "scoreList:", scoreList
+#    print score
+    L1 = sum(weights) # ~ L1 norm of weights
+
+    for iIter in xrange(maxIter):
+        scoreD_keys = scoreD.keys() #list of weight tuples already computed
+
+        if verbosity > 0:
+            print "Iteration %d: score=%g, nGerms=%d" % (iIter, score, L1)
+        
+        bFoundBetterNeighbor = False
+        for neighborNum, neighbor in enumerate(get_neighbors(weights)):
+#            if force_singletons:
+#                if _np.count_nonzeros(neighbor[:numGates]) != numGates
+#                    continue
+            neighborScoreList = []
+            for gateset_num, gateset in enumerate(gatesetList):
+                if (gateset_num,tuple(neighbor)) not in scoreD_keys:
+                    neighborL1 = sum(neighbor)
+                    neighborScoreList.append(compute_score(neighbor,gateset_num))
+                else:
+                    neighborL1 = sum(neighbor)
+                    neighborScoreList.append(scoreD[gateset_num,tuple(neighbor)])
+
+            neighborScore = _np.max(neighborScoreList)#Take worst case.
+#            print "neighborScore:", neighborScore
+#            print "score:", score
+            #Move if we've found better position; if we've relaxed, we only move when L1 is improved.
+            if neighborScore <= score and (neighborL1 < L1 or lessWeightOnly == False):
+                weights, score, L1 = neighbor, neighborScore, neighborL1
+                bFoundBetterNeighbor = True
+
+                if verbosity > 1: print "Found better neighbor: nGerms = %d score = %g" % (L1,score)
+
+        if not bFoundBetterNeighbor: # Time to relax our search.
+            lessWeightOnly=True #from now on, don't allow increasing weight L1
+
+            if fixedSlack==False:
+                slack = score*slackFrac #Note score is positive (for sum of 1/lambda)
+#                print "slack =", slack
+            else:
+                slack = fixedSlack
+            assert(slack > 0)
+
+            if verbosity > 1:
+                print "No better neighbor. Relaxing score w/slack: %g => %g" % (score, score+slack)
+            score += slack #artificially increase score and see if any neighbor is better now...
+
+            for neighborNum, neighbor in enumerate(get_neighbors(weights)):
+                scoreList = [scoreD[gateset_num,tuple(neighbor)] for gateset_num in xrange(len(gatesetList))]
+                maxScore = _np.max(scoreList)
+                if sum(neighbor) < L1 and maxScore < score:
+                    weights, score, L1 = neighbor, maxScore, sum(neighbor)
+                    bFoundBetterNeighbor = True
+                    if verbosity > 1: print "Found better neighbor: nGerms = %d score = %g" % (L1,score)
+
+            if not bFoundBetterNeighbor: #Relaxing didn't help!
+                print "Stationary point found!";
+                break #end main for loop
+        
+        print "Moving to better neighbor"
+#        print score
     else:
         print "Hit max. iterations"
     
