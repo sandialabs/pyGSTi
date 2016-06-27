103a7c40c0f86438860c416999a452afa0f34845
Jonathan A. Gross <jarthurgross@gmail.com>
2016-06-27 09:53:21 -0600

    Use subprocess and check for convert utility
    Replace all instances of os.system with appropriate calls from the
    subprocess module. Also make a rudimentary check to see if PNGs are
    generated from PDFs by ImageMagick's convert utility.

--- packages/pygsti/report/results.py
+++ packages/pygsti/report/results.py
@@ -8,6 +8,7 @@
 import sys as _sys
 import os  as _os
 import re  as _re
+import subprocess  as _subprocess
 import collections as _collections
 import matplotlib  as _matplotlib
 import itertools   as _itertools
@@ -307,6 +308,54 @@ class Results(object):
         s += " PDF indicating how tables and figures in the PDF correspond\n"
         s += " to the values of .tables[ ] and .figures[ ] listed above.\n"
         return s
+
+    def _compile_latex_report(self, report_dir, report_base, latex_call,
+                              printer, cwd):
+        """Use the shell command defined in `latex_call` to compile the TeX file
+        `texFilename` to the PDF file `pdfFilename`.
+
+        Parameters
+        ----------
+        report_dir : string
+            The directory for the output file.
+
+        report_base : string
+            The base name for the output file (not including any extensions).
+
+        latex_call : list of string
+            List containing the command and flags in the form that
+            :function:`subprocess.check_call` uses.
+
+        printer : VerbosityPrinter
+            Printer to handle logging and errors.
+
+        cwd : string
+            The directory to change into after completing the compilation.
+
+        """
+        texFilename = report_base + ".tex"
+        pdfPathname = _os.path.join(report_dir, report_base + ".pdf")
+        call = latex_call + [texFilename]
+        try:
+            _subprocess.check_call(call)
+            printer.log("Initial output PDF %s successfully generated." %
+                        pdfPathname)
+            try:
+                # We could check if the log file contains "Rerun" in it,
+                # but we'll just re-run all the time now
+                _subprocess.check_call(call)
+                printer.log("Final output PDF %s successfully generated. " %
+                            pdfPathname + "Cleaning up .aux and .log files.")
+                _os.remove( report_base + ".log" )
+                _os.remove( report_base + ".aux" )
+            except _subprocess.CalledProcessError as e:
+                printer.error("pdflatex returned code %d " % e.returncode +
+                              "Check %s.log to see details." % report_base)
+        except _subprocess.CalledProcessError as e:
+            printer.error("pdflatex returned code %d " % e.returncode +
+                          "Check %s.log to see details." % report_base)
+        finally:
+            _os.chdir(cwd)
 
 
     def _get_table_fns(self):
@@ -1823,33 +1872,8 @@ class Results(object):
         if len(report_dir) > 0:
             _os.chdir(report_dir)
 
-        try:
-            ret = _os.system( "%s %s %s" %
-                              (self.options.latex_cmd,
-                               _os.path.basename(mainTexFilename),
-                               self.options.latex_postcmd) )
-            if ret == 0:
-                #We could check if the log file contains "Rerun" in it,
-                # but we'll just re-run all the time now
-                printer.log("Initial output PDF %s successfully generated." \
-                        % pdfFilename)
-
-                ret = _os.system( "%s %s %s" %
-                                  (self.options.latex_cmd,
-                                   _os.path.basename(mainTexFilename),
-                                   self.options.latex_postcmd) )
-                if ret == 0:
-                    printer.log("Final output PDF %s successfully generated. Cleaning up .aux and .log files." % pdfFilename) #mainTexFilename
-                    _os.remove( report_base + ".log" )
-                    _os.remove( report_base + ".aux" )
-                else:
-                    printer.error("pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-            else:
-                printer.error("pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-        except:
-            printer.error("Error trying to run pdflatex to generate output PDF %s. Is '%s' on your path?" % (pdfFilename,self.options.latex_cmd))
-        finally:
-            _os.chdir(cwd)
+        self._compile_latex_report(report_dir, report_base,
+                                   self.options.latex_call, printer, cwd)
 
         return
 
@@ -2104,32 +2128,8 @@ class Results(object):
         if len(report_dir) > 0:
             _os.chdir(report_dir)
 
-        try:
-            ret = _os.system( "%s %s %s" %
-                              (self.options.latex_cmd,
-                               _os.path.basename(mainTexFilename),
-                               self.options.latex_postcmd) )
-            if ret == 0:
-                #We could check if the log file contains "Rerun" in it,
-                # but we'll just re-run all the time now
-                printer.log("Initial output PDF %s successfully generated." % \
-                        pdfFilename)
-                ret = _os.system( "%s %s %s" %
-                                  (self.options.latex_cmd,
-                                   _os.path.basename(mainTexFilename),
-                                   self.options.latex_postcmd) )
-                if ret == 0:
-                    printer.log("Final output PDF %s successfully generated. Cleaning up .aux and .log files." % pdfFilename) #mainTexFilename
-                    _os.remove( report_base + ".log" )
-                    _os.remove( report_base + ".aux" )
-                else:
-                    printer.error("Error: pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-            else:
-                printer.error("Error: pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-        except:
-            printer.error("Error trying to run pdflatex to generate output PDF %s. Is '%s' on your path?" % (pdfFilename,self.options.latex_cmd))
-        finally:
-            _os.chdir(cwd)
+        self._compile_latex_report(report_dir, report_base,
+                                   self.options.latex_call, printer, cwd)
 
         return
 
@@ -2528,30 +2528,8 @@ class Results(object):
         if len(report_dir) > 0:
             _os.chdir(report_dir)
 
-        try:
-            ret = _os.system( "%s %s %s" %
-                              (self.options.latex_cmd,
-                               _os.path.basename(mainTexFilename),
-                               self.options.latex_postcmd) )
-            if ret == 0:
-                #We could check if the log file contains "Rerun" in it, but we'll just re-run all the time now
-                printer.log("Initial output PDF %s successfully generated." % pdfFilename) #mainTexFilename
-                ret = _os.system( "%s %s %s" %
-                                  (self.options.latex_cmd,
-                                   _os.path.basename(mainTexFilename),
-                                   self.options.latex_postcmd) )
-                if ret == 0:
-                    printer.log("Final output PDF %s successfully generated. Cleaning up .aux and .log files." % pdfFilename) #mainTexFilename
-                    _os.remove( report_base + ".log" )
-                    _os.remove( report_base + ".aux" )
-                else:
-                    printer.error("pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-            else:
-                printer.error("Error: pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-        except:
-            printer.error("Error trying to run pdflatex to generate output PDF %s. Is '%s' on your path?" % (pdfFilename,self.options.latex_cmd))
-        finally:
-            _os.chdir(cwd)
+        self._compile_latex_report(report_dir, report_base,
+                                   self.options.latex_call, printer, cwd)
 
         return
 
@@ -3022,17 +3000,34 @@ class Results(object):
 
             cwd = _os.getcwd()
             _os.chdir(fileDir)
+
             try:
-                ret = _os.system("%s -shell-escape %s.tex %s" \
-                                     % (self.options.latex_cmd, key,
-                                        self.options.latex_postcmd) )
-                if ret == 0:
-                    _os.remove( "%s.tex" % key )
-                    _os.remove( "%s.log" % key )
-                    _os.remove( "%s.aux" % key )
-                else: raise ValueError("pdflatex returned code %d trying to render standalone %s" % (ret,key))
+                output = _subprocess.check_output(self.options.latex_call +
+                                                  ["-shell-escape",
+                                                   "%s.tex" % key],
+                                                  stderr=_subprocess.STDOUT)
+                # Trying to see if ImageMagick's 'convert' command is installed.
+                # Ideally I would check to see if stderr is empty and if it's
+                # not empty I would print this error message should 'convert'
+                # show up anywhere in stderr, but I don't currently understand
+                # subprocess well enough to make this work, so I send stderr to
+                # stdout and look for a more specific string that shows up on
+                # MacOS when 'convert' is not available.
+                if output.find('convert: command not found') > -1:
+                    printer.error("ImageMagick's 'convert' utility may not be" +
+                                  " installed, so a PNG may not have been " +
+                                  "generated for %s." % key)
+                _os.remove( "%s.tex" % key )
+                _os.remove( "%s.log" % key )
+                _os.remove( "%s.aux" % key )
+            except _subprocess.CalledProcessError as e:
+                printer.error("pdflatex returned code %d " % e.returncode +
+                              "trying to render standalone %s. " % key +
+                              "Check %s.log to see details." % key)
             except:
-                raise ValueError("pdflatex failed to render standalone %s" % key)
+                printer.error("pdflatex failed to render standalone %s" % key)
+                raise
+
             finally:
                 _os.chdir(cwd)
 
@@ -3726,33 +3721,8 @@ class Results(object):
         if len(report_dir) > 0:
             _os.chdir(report_dir)
 
-        try:
-            ret = _os.system( "%s %s %s" %
-                              (self.options.latex_cmd,
-                               _os.path.basename(mainTexFilename),
-                               self.options.latex_postcmd) )
-            if ret == 0:
-                #We could check if the log file contains "Rerun" in it,
-                # but we'll just re-run all the time now
-                printer.log("Initial output PDF %s successfully generated." \
-                        % pdfFilename)
-
-                ret = _os.system( "%s %s %s" %
-                                  (self.options.latex_cmd,
-                                   _os.path.basename(mainTexFilename),
-                                   self.options.latex_postcmd) )
-                if ret == 0:
-                    printer.log("Final output PDF %s successfully generated. Cleaning up .aux and .log files." % pdfFilename) #mainTexFilename
-                    _os.remove( report_base + ".log" )
-                    _os.remove( report_base + ".aux" )
-                else:
-                    printer.error("pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-            else:
-                printer.error("pdflatex returned code %d. Check %s.log to see details" % (ret, report_base))
-        except:
-            printer.error("Error trying to run pdflatex to generate output PDF %s. Is '%s' on your path?" % (pdfFilename,self.options.latex_cmd))
-        finally:
-            _os.chdir(cwd)
+        self._compile_latex_report(report_dir, report_base,
+                                   self.options.latex_call, printer, cwd)
 
         return
 
@@ -3773,6 +3743,9 @@ class ResultOptions(object):
         self.table_class = "pygstiTbl"
         self.template_path = "."
         self.latex_cmd = "pdflatex"
+        # Don't allow LaTeX to try and recover from errors interactively.
+        self.latex_opts = ["-interaction=nonstopmode", "-halt-on-error"]
+        self.latex_call = [self.latex_cmd] + self.latex_opts
         if _os.path.exists("/dev/null"):
             self.latex_postcmd = "-halt-on-error </dev/null >/dev/null"
         else:
