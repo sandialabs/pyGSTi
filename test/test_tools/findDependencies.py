ea15b4a9c12978ed4c89926a848573cecce0806b
Lucas Saldyt <lsaldyt@sandia.gov>
2016-06-27 10:39:49 -0600

    Adds shebang lines to scripts in test_tools

--- test/test_tools/findDependencies.py
+++ test/test_tools/findDependencies.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from __future__ import print_function
 from helpers    import tool
 import os
--- test/test_tools/genInfo.py
+++ test/test_tools/genInfo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from __future__   import print_function
 from helpers      import *
 from runBenchmark import benchmark_coverage
