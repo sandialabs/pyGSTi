9bff7d3f1a46eece0169f14c40de6ac14853f7a4
LSaldyt <lucassaldyt@yahoo.com>
2016-06-29 07:36:23 -0600

    Improves the tools for linting

--- pylint/runAdjustables.py
+++ pylint/runAdjustables.py
@@ -1,19 +1,53 @@
-#!/usr/bin/env python
-from __future__ import print_function
+#!/usr/bin/env python3
 from readyaml   import read_yaml
 import subprocess
+import sys
 
-adjustables = read_yaml('adjustables.yml')['adjustables']
-
-for adjustable in adjustables:
-    setting = adjustables[adjustable]
-    commands = ['pylint3', '--disable=all',
-                           '--enable=%s' % adjustable,
-                           '--%s'        % setting,
-                           '--reports=n',
-                           '../packages/pygsti']
+# A wrapper for subprocess
+def get_output(commands):
     try:
         output = subprocess.check_output(commands)
-        print(output.decode('utf-8'))
+        return output.decode('utf-8').splitlines()
     except subprocess.CalledProcessError as e:
-        print(e.output.decode('utf-8'))
+        return e.output.decode('utf-8').splitlines()
+
+def write_output(output, filename):
+    with open(filename, 'w') as outputfile:
+        outputfile.write("\n".join(output))
+
+# A function that lets us adjust the value of the adjustable when linting
+def build_commands(adjustable, setting, value, package='packages/pygsti'):
+	return ['pylint3', '--disable=all',
+				   '--enable=%s' % adjustable,
+				   '--%s=%s'     % (setting, value),
+				   '--reports=n',
+				   '../%s' % package]
+
+if __name__ == "__main__":
+
+    # The wanted size of an output file (ex: too-many-arguments.txt)
+    desiredLength= 20
+    package      = 'packages/pygsti' if len(sys.argv) == 1 else sys.argv[1]
+
+    adjustables = read_yaml('config.yml')['adjustables']
+
+    for adjustable in adjustables:
+
+        default = adjustables[adjustable]
+        setting, defaultvalue = default.rsplit('=')
+
+        # Wrapper around build_commands for adjusting the value of the adjustable's setting :)
+        adjust_commands = lambda value : build_commands(adjustable, setting, value, package)
+
+        currentvalue = defaultvalue
+        output = get_output(adjust_commands(currentvalue))
+         
+        # Adjust the value (ex: max arguments/function) until a properly sized file is generated
+        while(len(output) > desiredLength):
+            currentvalue += defaultvalue
+            output = get_output(adjust_commands(currentvalue))
+ 
+        # Once the file is satisfactory, write the thing
+        write_output(output, 'output/%s.txt' % adjustable)
+    
+    
