b327800ac0a25f454999e4cf750d5204c8aebc61
Erik Nielsen <enielse@sandia.gov>
2016-07-08 12:32:33 -0600

    Applies minor fix to formatter so that:  All Tests Pass
    Adds a few sanity checks to the formatList function and corrects
    the hasattr check to see if "formatter[fmt]" has a 'scratchDir'
    attribute instead of just "formatter" (which is a dictionary).
    
    All tests now pass on python2.7 & 3 on Erik's desktop (so without
    detailed report comparisons).

--- packages/pygsti/report/formatter.py
+++ packages/pygsti/report/formatter.py
@@ -288,6 +288,10 @@ class FigureFormatter():
     def __call__(self, figInfo):
         fig, name, W, H = figInfo
         if self.extension is not None:
+            if self.scratchDir is None:
+                raise ValueError("Must supply scratch " +
+                                 "directory to FigureFormatter")
+
             fig.save_to(_os.path.join(self.scratchDir, name + self.extension))
             if self.custom is not None:
                 return (self.formatstring
@@ -297,6 +301,7 @@ class FigureFormatter():
             else:
                 return self.formatstring % (W, H, self.scratchDir,
                                             name + self.extension)
+
         elif self.custom is not None:
             return self.custom[0](figInfo, **self.custom[1])
         else:
@@ -324,9 +329,16 @@ def formatList(items, formatters, fmt, scratchDir=None):
         if formatter is not None:
             # If the formatter requires a scratch directory to do its job, give
             # it.
-            if hasattr(formatter, 'scratchDir'):
-                formatter.scratchDir = scratchDir
+            if hasattr(formatter[fmt], 'scratchDir'):
+                formatter[fmt].scratchDir = scratchDir
+            formatted_item = formatter[fmt](item)
+            if formatted_item is None:
+                raise ValueError("Formatter " + str(type(formatter[fmt]))
+                                 + " returned None for item = " + str(item))
             formatted_items.append( formatter[fmt](item) )
         else:
+            if item is None:
+                raise ValueError("Unformatted None in formatList")
             formatted_items.append( item )
+
     return formatted_items
