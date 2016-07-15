ee1a7acbee3f9cd570eb5012156205d8436cc4b6
LSaldyt <lucassaldyt@yahoo.com>
2016-07-15 11:14:51 -0600

    Adds hooksettings and refactors hooks
      - adds hooksettings, which contains lists of branches that do certain things. This is essentially a config file
      - creates run_pylint, which will run commands inside of the pylint directory for certain hooks, and return the result

---
+++ hooks/hooksettings.py
@@ -0,0 +1,3 @@
+updates_git_pages       = ['master']
+requires_pylint_pass    = ['beta', 'master']
+generates_pylint_report = ['develop']
--- hooks/utilities.py
+++ hooks/utilities.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import subprocess, os, sys
 
 def get_branchname():
     try:
@@ -9,4 +10,10 @@ def get_branchname():
     branchname = branchname.replace('\n', '')
     return branchname
 
+def run_pylint(commands):
+    old_wd = os.getcwd()
+    os.chdir('pylint/')
+    result = subprocess.call(commands)
+    os.chdir(old_wd)
+    return result
 
