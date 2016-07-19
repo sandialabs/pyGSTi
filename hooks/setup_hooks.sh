38c64abc33623f75ed44a2b849576b158dc09777
LSaldyt <lucassaldyt@yahoo.com>
2016-07-19 07:03:42 -0600

    Refactors hooks directory, adds travis hooks
      - Adds (trivial) after_failure and after_success hooks (travis CI)
      - Seperates git hooks from travis hooks (and modifies setup_hooks to reflect this)
      - Modifies travis.yml to call the travis hooks (hopefully :) )

--- hooks/setup_hooks.sh
+++ hooks/setup_hooks.sh
@@ -1,7 +1,5 @@
 # Don't forget to run this before testing :)
 # (Copies all hooks to .git/hooks (where they will actually be called))
 echo Setting up hooks
-cp * ../.git/hooks
-# Remove ourselves from the hooks directory 
-rm "../.git/hooks/$0"
+cp git/* ../.git/hooks
 echo Hook setup completed
