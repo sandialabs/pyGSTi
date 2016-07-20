ded40f45d8bc9cfbabf53c294456dd319f071f0e
LSaldyt <lucassaldyt@yahoo.com>
2016-07-20 06:39:36 -0600

    Create directory test_packages, redirect runTests.py

--- test/runTests.py
+++ test/runTests.py
@@ -3,7 +3,7 @@ from __future__                  import print_function, division, unicode_litera
 from helpers.test                import *
 from helpers.test.runChanged     import *
 from helpers.test.runPackage     import run_package
-from helpers.automation_tools    import read_yaml, get_args
+from helpers.automation_tools    import read_yaml, directory, get_args
 from helpers.test.genPackageInfo import gen_package_info
 import sys
 
@@ -12,40 +12,41 @@ if __name__ == "__main__":
     config = read_yaml('test_config.yml')
     slowTests = config['slow-tests']
 
-    # Setup arguments and other variables:
-    args, kwargs = get_args(sys.argv)
-    exclude = ['benchmarks', 'output', 'cmp_chk_files', 'temp_test_files', 'test_tools']
+    with directory('test_packages'):
+        # Setup arguments and other variables:
+        args, kwargs = get_args(sys.argv)
+        exclude = ['benchmarks', 'output', 'cmp_chk_files', 'temp_test_files', 'test_tools']
 
-    # Specify the versions of your test :)
-    if 'version' not in kwargs:
-        pythonCommands = ['python%s.%s' % (sys.version_info[0], sys.version_info[1])]
-    else:
-        pythonCommands = ['python%s' % kwargs['version']]
-    
-    if 'fast-only' in kwargs:
-        exclude += slowTests # Shave off ~3 hrs?
-
-    # Since last commit to current branch
-    if len(args) == 0:
-        if 'changed' in kwargs:
-            packageNames = [name for name in get_changed_test_packages() if name not in exclude]
+        # Specify the versions of your test :)
+        if 'version' not in kwargs:
+            pythonCommands = ['python%s.%s' % (sys.version_info[0], sys.version_info[1])]
         else:
-            packageNames = [name for name in get_package_names() if name not in exclude]
-    else:
-        packageNames = args
+            pythonCommands = ['python%s' % kwargs['version']]
+        
+        if 'fast-only' in kwargs:
+            exclude += slowTests # Shave off ~3 hrs?
 
-    print('Running packages %s' % (', '.join(packageNames)))
+        # Since last commit to current branch
+        if len(args) == 0:
+            if 'changed' in kwargs:
+                packageNames = [name for name in get_changed_test_packages() if name not in exclude]
+            else:
+                packageNames = [name for name in get_package_names() if name not in exclude]
+        else:
+            packageNames = args
 
-    # Generating info
-    if 'info' in kwargs:
-        gen_package_info(packageNames)
+        print('Running packages %s' % (', '.join(packageNames)))
 
-    # Only running tests
-    else:
-        if 'nose' in kwargs:
-            pythonCommands += ['-m', 'nose']
+        # Generating info
+        if 'info' in kwargs:
+            gen_package_info(packageNames)
 
-        lastFailed = 'True' if 'lastFailed' in kwargs else None
-        for package in packageNames:
-            run_package(package, precommands=pythonCommands, postcommand=None, lastFailed=lastFailed)
+        # Only running tests
+        else:
+            if 'nose' in kwargs:
+                pythonCommands += ['-m', 'nose']
+
+            lastFailed = 'True' if 'lastFailed' in kwargs else None
+            for package in packageNames:
+                run_package(package, precommands=pythonCommands, postcommand=None, lastFailed=lastFailed)
 
