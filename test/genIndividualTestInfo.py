0e457bd44e4b6765bcc3af98b2df2972e032c96f
LSaldyt <lucassaldyt@yahoo.com>
2016-07-21 08:18:29 -0600

    Rename findSlowTests.py, change coverage output format
    (Coverage output is formatted as yaml, try running ./genIndividualTestInfo.py tools, then looking at the files in the output/individual_coverage folder)

--- test/genIndividualTestInfo.py
+++ test/genIndividualTestInfo.py
@@ -23,6 +23,36 @@ def find_individual_tests(testCase):
     istest = lambda a, case : callable(getattr(case, a)) and a.startswith('test')
     return [attr for attr in dir(testCase) if istest(attr, testCase)]
 
+def parse_coverage_output(filename):
+    with open(filename) as content:
+        specific = content.read().split('-----------------------------------------------')[1]
+        specific = [line for line in specific.replace('-', '').splitlines() if line != '']
+    
+    # At this point, specific looks like this:
+    '''
+    pygsti/objects.py                       21      0   100%
+    pygsti/objects/confidenceregion.py     213    195     8%
+    pygsti/objects/dataset.py              262    200    24%
+    pygsti/objects/evaltree.py             255    235     8%
+    pygsti/objects/exceptions.py             5      0   100%
+    pygsti/objects/gate.py                 331    215    35%
+    pygsti/objects/gateset.py              489    346    29%
+    pygsti/objects/gatestring.py           151    115    24%
+    pygsti/objects/gscalc.py              1189   1095     8%
+    pygsti/objects/labeldicts.py           113     41    64%
+    pygsti/objects/multidataset.py         159    122    23%
+    pygsti/objects/protectedarray.py       121     73    40%
+    pygsti/objects/spamspec.py              14      8    43%
+    pygsti/objects/spamvec.py              158     71    55%
+    pygsti/objects/verbosityprinter.py     113     81    28%
+    '''
+    coverageDict = {}
+    for line in specific:
+        filename, _, _, percentage = line.split()
+        coverageDict[filename] = int(percentage[:-1]) # Cut off percent symbol
+    return coverageDict
+
+
 def get_test_info(filename, packageName, testCaseName, test):
     coverageFile = '../../output/individual_coverage/%s.out' % (test)
     commands = ['nosetests', '-v', '--with-coverage', '--cover-package=pygsti.%s' % packageName, 
@@ -31,6 +61,10 @@ def get_test_info(filename, packageName, testCaseName, test):
     start   = time.time()
     percent = _read_coverage(' '.join(commands), coverageFile)
     end     = time.time()
+
+    coverageDict = parse_coverage_output(coverageFile)
+    write_yaml(coverageDict, coverageFile)
+    
     return ((end - start), percent)
 
 def gen_individual_test_info(packageName):
@@ -50,15 +84,19 @@ def gen_individual_test_info(packageName):
                 info = get_test_info(filename, packageName, testCaseName, testName)
                 testsInfo[filename[:-3] + '.' + testCaseName + '.' + testName] = info
                 testcase.tearDown()
+
     return testsInfo
                 
 if __name__ == '__main__':
 
     gen_info_on = sys.argv[1:]
     infoDict    = read_yaml('output/all_individual_test_info.yml')
+    if infoDict is None: # if the file is empty
+        infoDict = {}
 
     # Update info for the packages given 
     for packageName in gen_info_on:
+        print('Updating info for %s' % packageName)
         infoDict[packageName] = gen_individual_test_info(packageName)
 
     write_yaml(infoDict, 'output/all_individual_test_info.yml')
