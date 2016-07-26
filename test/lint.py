21345cc746ff7e30f2483c9579983809b4205d59
LSaldyt <lucassaldyt@yahoo.com>
2016-07-26 13:29:29 -0600

    Modify pylint settings, create --noerrors option for hooks
    pre-push hook will fail on all branches if any errors are found by pylint.
    This takes thirty-one seconds, but it beats accidentally pushing a syntax error

--- test/lint.py
+++ test/lint.py
@@ -1,48 +1,57 @@
 #!/usr/bin/env python
-from helpers.pylint           import get_score, look_for, find_warnings, find_errors, run_adjustables
+from helpers.pylint           import get_score, look_for, find_warnings, find_errors, run_adjustables, lint_all
 from helpers.automation_tools import read_yaml, write_yaml, get_args
 import sys
 import argparse
 
 
-def check_score():
+def check_score(on=lint_all):
     yamlFile = 'config/pylint_config.yml'
 
     config       = read_yaml(yamlFile)
     desiredScore = config['desired-score']
     print('Score should be: %s' % desiredScore)
-    score        = get_score()
+    score        = get_score(on)
     print('Score was: %s' % score)
 
-    if float(score) >= float(desiredScore):
-        config['desired-score'] = score # Update the score if it is higher than the last one
-        write_yaml(config, yamlFile)
-        sys.exit(0)
+    if on == lint_all:
+        if float(score) >= float(desiredScore):
+            config['desired-score'] = score # Update the score if it is higher than the last one
+            write_yaml(config, yamlFile)
+            sys.exit(0)
+        else:
+            sys.exit(1)
     else:
-        sys.exit(1)
+        if score < 10:
+            sys.exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Lint pygsti')
-    parser.add_argument('specific', nargs='*', default=None,
+    parser.add_argument('specific', nargs='*',
                         help='lint for specific items')
     parser.add_argument('--score', '-s', type=bool,
                         help='check the current repo\'s score against the last-highest score')
     parser.add_argument('--errors', '-e', type=bool,
                         help='check for errors in the repo')
+    parser.add_argument('--noerrors', '-n', type=bool,
+                        help='fail if any errors are found in core pygsti repo (not tests)')
     parser.add_argument('--warnings', '-w', type=bool,
                         help='check for warnings in the repo')
     parser.add_argument('--adjustables', '-a', type=bool,
                         help='check for refactors in the repo')
 
     parsed = parser.parse_args(sys.argv[1:])
+    print(parsed)
 
     if parsed.score:
         check_score()
-    if parsed.errors:
-        find_errors()
+    if parsed.noerrors:
+        check_score(find_errors) # Call find_errors to score by instead of lint_all
+    elif parsed.errors:          # Doesn't need to run if no-errors already did
+        find_errors(coreonly=False)
     if parsed.warnings:
         find_warnings()
     if parsed.adjustables:
         run_adjustables()
-    if parsed.specific is not None:
+    if parsed.specific != []:
         look_for(parsed.specific)
