5b2435f658f04112dade952c0a67279b63cae398
LSaldyt <lucassaldyt@yahoo.com>
2016-08-02 15:28:50 -0600

    Updates info.py, jsonwrapper.py, changes hooksettings
    info.py now depends on json, not yaml
    jsonwrapper handles empty/nonexisting files
    
    hooksettings no longer generates pylint report on develop post-commit,
      this can be done manually with ./lint.py --score
    
    [ci skip]

--- test/helpers/automation_tools/jsonwrapper.py
+++ test/helpers/automation_tools/jsonwrapper.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import json
 
 def read_json(filename):
-    with open(filename, 'r') as jsonfile:
-        return json.load(jsonfile)
+    try:
+        with open(filename, 'r') as jsonfile:
+            return json.load(jsonfile)
+    except:
+        return {}
 
 def write_json(data, filename):
     with open(filename, 'w') as jsonfile:
--- test/info.py
+++ test/info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from __future__                import print_function
-from helpers.automation_tools  import read_yaml, write_yaml
+from helpers.automation_tools  import read_json, write_json
 from helpers.info.genInfo      import gen_package_info
 from helpers.info.process      import find_uncovered_lines, annotate_uncovered, unannotate
 from pprint import pprint
@@ -13,22 +13,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate info on pygsti tests')
     parser.add_argument('packages', nargs='*',
                         help='packages to update metadata on')
-    parser.add_argument('--update', type=bool,
+    parser.add_argument('--update', action='store_true',
                         help='update the packages given')
-    parser.add_argument('--process', type=bool,
+    parser.add_argument('--process', action='store_true',
                         help='process the packages given')
-    parser.add_argument('--unannotate', type=bool,
+    parser.add_argument('--unannotate', action='store_true',
                         help='Clear coverage annotations')
 
     parsed = parser.parse_args(sys.argv[1:])
 
-    infoDict = read_yaml('output/test_info.yml')
+    infoDict = read_json('output/test_info.json')
 
     if parsed.update:
         # Update test info on given modules
         for packageName in parsed.packages:
             infoDict[packageName] = gen_package_info(packageName)
-        write_yaml(infoDict, 'output/test_info.yml')
+        write_json(infoDict, 'output/test_info.json')
 
     if parsed.process:
         for packageName in parsed.packages:
