90db1001cb526ef79bfed43f53562872c4e674d1
Erik Nielsen <enielse@sandia.gov>
2016-08-15 12:03:20 -0600

    Updates runNoseTests shell scripts.  (Changes mpitest => testmpi)

--- test/helpers/scripts/runNoseTests.sh
+++ test/helpers/scripts/runNoseTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #cd ../..
 mkdir -p output
-python3 -m nose -v */test*.py mpi/mpitest*.py 2>&1 | tee output/out.txt
-#OLD nosetests -v */test*.py mpi/mpitest*.py 2>&1 | tee output/out.txt
+python3 -m nose -v */test*.py mpi/testmpi*.py 2>&1 | tee output/out.txt
+#OLD nosetests -v */test*.py mpi/testmpi*.py 2>&1 | tee output/out.txt
