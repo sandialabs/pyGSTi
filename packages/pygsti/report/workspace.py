23f2ff30e26349c18850955ef3c85e3cdd2f08d3
Erik Nielsen <enielse@sandia.gov>
2017-03-22 06:47:59 -0600

    Adds core Switchboard logic & initiates widget->HTML transition.
    This commit transitions from the "widget" model of workspace
    outputs, whereby:
    
    - each output object contains a single "current" state and display()
      shows a widget with this state.
    - update() is called to re-render the widget, and it is the
      responsibility of the Selector / Switchboard to call update() on
      all relevant outputs.
    - interaction is performed via ipywidgets
    
    to the new (and better, I think) "HTML" model whereby:
    
    - each output object holds an entire range of results as dictated
      by its connection to Switchboards.
    - render() creates HTML + javascript necessary for all interaction
      with said Switchboards (which integrates with the HTML+JS produced
      by Switchboard.render()).
    - display() just renders and places the result in an HTML widget.
    - it is the output object's responsibility to listen for relevant
      switch events.
    - interaction is performed via JQueryUI javascipt components.
    
    This significant change has resulted in a new switchedCompute(...)
    method which is similar to cachedCompute(...) but returns *all* of
    the precomputed results along with a mapping to switch positions.
    In the future these two methods should be consolidated.
    
    The motivation behind this shift stems from the following
    reasonings about the new model:
    - ipywidgets don't offer very much logic that's especially hard to
      do without them, and they're specific to ipython notebooks.
    - the new HTML+JS model can be displayed in standalone web pages
      or within a notebook.
    - the interaction model is more extensible and cleaner, thanks to
      JQuery's  allowance of multiple event handlers.
    - interaction seems nearly as flexible as using widgets, and works
      with tables and plots in the same way -- and maybe is faster?
    - probably more I'm forgetting right now.
    
    At this point, WorkspaceValue and derived classe are still present,
    but seem perfunctory and will likely be removed soon.  The code is
    still in a very rough and fragile state, and many features don't
    quite work yet.  Among other things, Switchboards and WorkspacePlots
    can render themselves in HTML but tables can't yet; Switchboards
    don't start with initial values; there are hardcoded IDs for
    debugging; only toggle/radio switch type works.

--- packages/pygsti/report/workspace.py
+++ packages/pygsti/report/workspace.py
@@ -51,13 +51,15 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 import itertools as _itertools
 import collections as _collections
 import numpy as _np
+import uuid as _uuid
 
 import ipywidgets as _widgets
 from IPython.display import display as _display
 from IPython.display import clear_output as _clear_output
 
 import plotly.graph_objs as go
-from plotly.offline import plot, iplot, get_plotlyjs
+from plotly.offline import plot, iplot
+from plotly.offline.offline import get_plotlyjs
 
 
 def _is_hashable(x):
@@ -136,6 +138,116 @@ class Workspace(object):
         self.PolarEigenvaluePlot = makefactory(_wp.PolarEigenvaluePlot, cache=False)
         self.ProjectionsBoxPlot = makefactory(_wp.ProjectionsBoxPlot, cache=False)
         self.ChoiEigenvalueBarPlot = makefactory(_wp.ChoiEigenvalueBarPlot, cache=False)
+
+
+    def switchedCompute(self, fn, *args):
+
+        # Computation functions get stripped-down *value* args
+        # (strip WorkspaceValue stuff away)
+
+        valArgs = [ arg.value if isinstance(arg,WorkspaceValue) else arg
+                    for arg in args ]
+
+        switchboards = []
+        switchBdInfo = []
+        
+        #sbVariableNames = []
+        #sbArgIndices = []
+        
+        nonSwitchedArgs = []
+
+        switchpos_map = {}
+        storedKeys = {}
+        resultValues = []
+        
+        for i,arg in enumerate(args):
+            if isinstance(arg,SwitchVariable):
+                isb = None
+                for j,sb in enumerate(switchboards):
+                    if arg.parent is sb:
+                        isb = j; break
+                else:
+                    isb = len(switchboards)
+                    switchboards.append(arg.parent)
+                    switchBdInfo.append({
+                        'argument indices': [], # indices of arguments that are children of this switchboard
+                        'value names': [], # names of switchboard value correspond to each argument index
+                        'switch indices': set() # indices of the switches that are actually used by the args
+                        })
+                assert(isb is not None)
+                info = switchBdInfo[isb]
+                                        
+                info['argument indices'].append(i)
+                info['value names'].append(arg.groupname)
+                info['switch indices'].update(arg.dependencies)
+            else:
+                nonSwitchedArgs.append( (i,arg) )
+
+        #print("DB: %d arguments" % len(args))
+        #print("DB: found %d switchboards" % len(switchboards))
+        #print("DB: sbArgIndices = ", sbArgIndices)
+        #print("DB: sbVariableNames = ", sbVariableNames)
+        #print("DB: nonSwitchedArgs = ", nonSwitchedArgs)
+        #print("DB: nsArgIndices = ",nonSwitchedArgIndices)
+
+        #Gate a list of lists, each list holding all of the relevant switch positions for each board
+        switch_positions = []
+        for i,sb in enumerate(switchboards):
+            info = switchBdInfo[isb]
+            info['switch indices'] = list(info['switch indices']) # set -> list so definite order
+            
+            switch_ranges = [ list(range(len(sb.positionLabels[i])))
+                              for i in info['switch indices'] ]
+            sb_switch_positions = list(_itertools.product( *switch_ranges ))
+              # a list of all possible positions for the switches being
+              # used for the *single* board sb
+            switch_positions.append( sb_switch_positions )
+
+        #for k,nswarg in zip(nonSwitchedArgIndices,nonSwitchedArgs): #treat each non-switched are like a separate switchboard
+        #    sbValueTups.append([(val,) for val in args[k].values] if isinstance(args[k],WorkspaceValue) else [(args[k],)])
+        #    sbArgIndices.append( (k,) )
+
+        #loop over all relevant switch configurations (across multiple switchboards)
+        for pos in _itertools.product( *switch_positions ):
+            # pos[i] gives the switch configuration for the i-th switchboard
+
+            #fill in the arguments for our function call
+            argVals = [None]*len(args)
+
+            #first, iterate over all the switchboards
+            for sw_pos,sb,info in zip(pos, switchboards, switchBdInfo):
+                # sw_pos is a tuple of the info['switch indices'] switch positions for sb
+                sis = info['switch indices']
+                for nm,j in zip(info["value names"],info["argument indices"]):
+                    value_swpos = [ sw_pos[sis.index(k)] for k in sb[nm].dependencies ]
+                      # potentially a subset of sw_pos, contains only the switch positions
+                      # relevant to the particular SwitchedValue named nm (also the j-th argument)
+                    argVals[j] = sb[nm][value_swpos]
+
+            #next, fill in the non-switched arguments
+            for j,arg in nonSwitchedArgs:
+                argVals[j] = arg.value if isinstance(arg,WorkspaceValue) else arg
+                
+
+            # argVals now contains all the arguments, so call the function if
+            #  we need to and add result.
+
+            #TODO: maybe make this a call to worspace?
+            key = call_key(fn, argVals) # cache by call key            
+            if key not in self.compCache:
+                #print("DB: computing with valArgs = ", valArgs)
+                self.compCache[key] = fn(*argVals)
+            result = self.compCache[key]
+
+            if key not in storedKeys:
+                switchpos_map[pos] = len(resultValues)
+                storedKeys[key] = len(resultValues)
+                resultValues.append( self.compCache[key] )
+            else:
+                switchpos_map[pos] = storedKeys[key]
+
+        switchboard_switch_indices = [ info['switch indices'] for info in switchBdInfo ]
+        return resultValues, switchpos_map, switchboards, switchboard_switch_indices
 
 
     def cachedCompute(self, fn, *args):
@@ -324,7 +436,7 @@ class Switchboard(_collections.OrderedDict):
         self.variables = {}
 
         self.descriptions = descriptions
-        self.widgets = None #[None]*len(switches) #don't build widget unless needed
+        self.widget = None
         super(Switchboard,self).__init__([])
 
 
@@ -356,45 +468,67 @@ class Switchboard(_collections.OrderedDict):
     def __setitem__(self, key, val):
         raise KeyError("Use add(...) to add an item to this swichboard")
 
-    def build_widgets(self):
-        self.widgets = []
-        for i,(name,typ,posLbls) in enumerate(zip(self.switchNames,
-                                                  self.switchTypes,
-                                                  self.positionLabels)):
+    def render(self, typ="html"):
+        assert(typ == "html"), "Can't render Switchboards as anything but HTML"
+
+        switch_html = []; switch_js = []
+        for i,(name,ID,typ,posLbls) in enumerate(zip(self.switchNames,
+                                                     self.switchIDs,
+                                                     self.switchTypes,
+                                                     self.positionLabels)):
+            html = ""; js = ""
             if typ == "buttons":
-                wig = _widgets.ToggleButtons(
-                    options=posLbls,
-                    description=name,
-                    disabled=False,
-                    button_style='', # 'success', 'info', 'warning', 'danger' or ''
-                    tooltip=self.descriptions[i] if (self.descriptions is not None) else "",
-                    #     icon='check'
-                )
+                html = "<fieldset id='%s'>\n" % ID
+                for k,lbl in enumerate(posLbls):
+                    html += "<label for='%s-%d'>%s</label>\n" % (ID, k,lbl)
+                    html += "<input type='radio' name='%s' id='%s-%d' value=%d>\n" \
+                                          % (ID,ID,k,k)
+                html += "</fieldset>\n"
+                js = "$('#%s > input').checkboxradio({ icon: false });" % ID
+
+            elif typ == "dropdown":
+                raise NotImplementedError()
+            
+            elif typ == "slider":
+                raise NotImplementedError()
+                
             else:
-                raise ValueError("Unknown 'typ' argument: %s" % self.typ)
-            self.widgets.append(wig)
+                raise ValueError("Unknown switch type: %s" % typ)
+
+            switch_html.append(html)
+            switch_js.append(js)
+
+        return switch_html, switch_js
+                
+
+    def get_switch_change_handlerjs(self, switchIndex):
+        ID = self.switchIDs[switchIndex]
+        typ = self.switchTypes[switchIndex]
+        if typ == "buttons":
+            return "$('#%s').on('change', function() {" % ID
+        else:
+            raise ValueError("Unknown switch type: %s" % typ)
+
+    def get_switch_valuejs(self, switchIndex):
+        ID = self.switchIDs[switchIndex]
+        typ = self.switchTypes[switchIndex]
+        if typ == "buttons":
+            return "$(\"#%s > input[name='%s']:checked\").val()" % (ID,ID)
+        else:
+            raise ValueError("Unknown switch type: %s" % typ)
+        
+
 
     def display(self):
-        if self.widgets is None:
-            self.build_widgets()
-        for wig in self.widgets:
-            assert(wig is not None)
-            _display(wig)
-            
-            # - observes change event -> calls update on listeners to this variable?
-            wig.observe(self.on_update, 'value')
-
-    def on_update(self, change_dict):
-        print("DB: CALLBACK called : %s!" % change_dict)
-        if change_dict['type'] == "change":
-            pass
-            #iCur = self.names.index(change_dict['new'])
-            #self.update(iCur)
-            #self.ws.trigger_update(id(self))
-            
-            #print("DB: Selected option %d: %s (val=%s)" %
-            #      (self.curindex, self.name, str(self.value)))
-
+        if self.widget is None:
+            self.widget = _widgets.HTML(value="?",
+                                        placeholder='Switch HTML',
+                                        description='Switch HTML',
+                                        disabled=False)
+        html, js = self.render("html")
+        content = "<script>\n" + "\n".join(js) + "</script>" + "\n".join(html)
+        self.widget.value = content
+        _display(self.widget)
 
             
 #    def __getattr__(self, attr):
@@ -553,27 +687,99 @@ class WorkspacePlot(WorkspaceOutput):
         self.plotfn = fn
         self.initargs = args
         self.widget = None #don't build until necessary
-        self.update()
 
-    def update(self):
-        """
-        Update any widgets associated with this object.  Called when underlying
-        data is changed.
-        """
-        self.fig = self.ws.cachedCompute(self.plotfn, *self.initargs)
-        if self.widget:
-            _clear_output()
-            iplot(self.fig) #basehtml = plot(fig, output_type='div', image_filename="test", image='png', auto_open=False)
+        self.figs,self.switchpos_map,self.switchboards,self.sbSwitchIndices = \
+                    self.ws.switchedCompute(self.plotfn, *self.initargs)
+
+        #self.update()
+
+    #def update(self):
+    #    """
+    #    Update any widgets associated with this object.  Called when underlying
+    #    data is changed.
+    #    """
+    #    self.fig = self.ws.cachedCompute(self.plotfn, *self.initargs)
+    #    if self.widget:
+    #        _clear_output()
+    #        iplot(self.fig) #basehtml = plot(fig, output_type='div', image_filename="test", image='png', auto_open=False)
 
     def display(self):
         """Create a new widget associated with this object and display it"""
         if self.widget is None:
-            self.widget = _widgets.Output()
-            self.update()
+            self.widget = _widgets.HTML(value="?",
+                                        placeholder='Plot HTML',
+                                        description='Plot HTML',
+                                        disabled=False)
+        html, js = self.render("html")
+        content = "<script>\n%s\n</script>\n\n%s" % (js,html)
+        self.widget.value = content
+        #with open("debug.html","w") as f:
+        #    f.write(content)
+        #print("DB content:\n",content)
         _display(self.widget)
-    
-    def render(self):
-        pass
+
+        
+    def render(self, typ="html"):
+        assert(typ == "html"), "Only HTML rendering supported currently"
+
+        containerID = "DEBUG1234" #_uuid.uuid4()
+
+        def getPlotlyDivID(html):
+            #could make this more robust using lxml or something later...
+            iStart = html.index('div id="')
+            iEnd = html.index('"', iStart+8)
+            return html[iStart+8:iEnd]
+
+        self.figs,self.switchpos_map,self.switchboards,self.sbSwitchIndices = \
+                    self.ws.switchedCompute(self.plotfn, *self.initargs)
+
+        
+        #build HTML as container div containing one or more plot divs
+        html = "<div id='%s'>\n" % containerID
+
+        figDivIds = []
+        for fig in self.figs:
+            fig_html = plot(fig, include_plotlyjs=False, output_type='div')
+            html += fig_html + "\n"
+            figDivIds.append(getPlotlyDivID(fig_html))
+        html += "</div>\n"
+
+
+        #build javascript to map switch positions to divIDs
+        js = "var %s_switchmap = new Array();" % containerID
+        for switchPositions, iFig in self.switchpos_map.items():
+            #switchPositions is a tuple of tuples of position indices, one tuple per switchboard
+            fig_divid = figDivIds[iFig]
+            flatPositions = []
+            for singleBoardSwitchPositions in switchPositions:
+                flatPositions.extend( singleBoardSwitchPositions )
+                
+            js += "%s_switchmap[ [%s] ] = '%s';\n" % (containerID, ",".join(map(str,flatPositions)), fig_divid)
+        js += "\n"
+
+        
+        #build change event listener javascript
+        for sb, switchInds in zip(self.switchboards, self.sbSwitchIndices):
+            # switchInds is a tuple containing the "used" switch indices of sb
+            
+            for switchIndex in switchInds:
+                #build a handler function to get all of the relevant switch positions,
+                # build a (flattened) position array, and perform the lookup.
+                handler_js = sb.get_switch_change_handlerjs(switchIndex) + "\n"
+                handler_js += "  var curSwitchPos = new Array();\n"
+                for sb2, switchInds2 in zip(self.switchboards, self.sbSwitchIndices):
+                    for switchIndex2 in switchInds2:
+                        handler_js += "  curSwitchPos.push(%s);\n" % sb2.get_switch_valuejs(switchIndex2)
+                handler_js += "  var idToShow = %s_switchmap[ curSwitchPos ];\n" % containerID
+                handler_js += "  $( '#%s' ).children().hide();\n" % containerID
+                handler_js += "  $( '#' + idToShow ).show();\n"
+                handler_js += "});\n"
+                js += handler_js
+
+        return html, js
+                
+                    
+                
 
 
 ## TEST ------------------------------------------------------------------------
