832951b580f83562b4a3c28e7ccb810be512cd46
Erik Nielsen <enielse@sandia.gov>
2017-04-20 13:53:21 -0600

    Minor name updates in seldom-used standard gateset modules.
    For consistency across the various standard gateset modules, the
    names "measurementFiducials" and "prepFiducials" have been changed
    to "prepStrs" and "effectStrs".

--- packages/pygsti/construction/std1Q_XZ.py
+++ packages/pygsti/construction/std1Q_XZ.py
@@ -14,19 +14,19 @@ from . import gatesetconstruction as _setc
 description = "X(pi/2) and Z(pi/2) gates"
 
 gates = ['Gx','Gz']
-prepFiducials = _strc.gatestring_list([(),
-                                       ('Gx',),
-                                       ('Gx','Gz'),
-                                       ('Gx','Gx'),
-                                       ('Gx','Gx','Gx'),
-                                       ('Gx','Gz','Gx','Gx')]) # for 1Q MUB
+prepStrs = _strc.gatestring_list([(),
+                                  ('Gx',),
+                                  ('Gx','Gz'),
+                                  ('Gx','Gx'),
+                                  ('Gx','Gx','Gx'),
+                                  ('Gx','Gz','Gx','Gx')]) # for 1Q MUB
 
-measFiducials = _strc.gatestring_list([(),
-                                       ('Gx',),
-                                       ('Gz','Gx'),
-                                       ('Gx','Gx'),
-                                       ('Gx','Gx','Gx'),
-                                       ('Gx','Gx','Gz','Gx')])
+effectStrs = _strc.gatestring_list([(),
+                                  ('Gx',),
+                                  ('Gz','Gx'),
+                                  ('Gx','Gx'),
+                                  ('Gx','Gx','Gx'),
+                                  ('Gx','Gx','Gz','Gx')])
 
 germs = _strc.gatestring_list( [('Gx',), ('Gz',), ('Gz','Gx','Gx'), ('Gz','Gz','Gx')] )
 
