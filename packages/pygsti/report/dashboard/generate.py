a4a302ae51988746814fdfd9ffeeb33518cd8161
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-08 16:04:10 -0600

    Adds WIP template for dashboard-like report

---
+++ packages/pygsti/report/dashboard/generate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import webbrowser
+
+def readfile(filename):
+    with open(filename, 'r') as infile:
+        return infile.read()
+
+def main():
+    templatefile = 'template.html'
+    outputfile   = 'output.html'
+    showfile     = 'show.js'
+
+    template = readfile(templatefile)
+    showscript = readfile(showfile)
+
+    dashboard = template.format(top='TOP', abody='A', bbody='B', aleft='a-options', bleft='b-options', showscript=showscript)
+
+    with open(outputfile, 'w') as outfile:
+        outfile.write(dashboard)
+    webbrowser.open(outputfile)
+
+if __name__ == '__main__':
+    main()
