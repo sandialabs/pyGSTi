26efecc437a3ffba56293a43727fc2f828a0bcfb
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-12 07:50:59 -0600

    Saves current profiling state

--- packages/pygsti/optimize/profiling/profile_gauge_opt.py
+++ packages/pygsti/optimize/profiling/profile_gauge_opt.py
@@ -10,18 +10,18 @@ est = results.estimates['default']
 gs_target = est.gatesets['target']
 gs = est.gatesets['final iteration estimate']
 
-'''
 with timed_block('Basic gauge opt:'):
     gs_gaugeopt = pygsti.gaugeopt_to_target(gs, gs_target, itemWeights={'spam' : 0.0001, 'gates':1.0})
+'''
 
 with timed_block('Gauge opt with CP Penalty:'):
     gs_gaugeopt = pygsti.gaugeopt_to_target(gs, gs_target, 
             itemWeights={'spam' : 0.0001, 'gates':1.0}, 
             CPpenalty=1.0, 
             validSpamPenalty=1.0)
-'''
 
 with timed_block('TP penalty gauge opt'):
     gs_gaugeopt = pygsti.gaugeopt_to_target(gs, gs_target, 
             itemWeights={'spam' : 0.0001, 'gates':1.0}, 
             TPpenalty=1.0)
+'''
