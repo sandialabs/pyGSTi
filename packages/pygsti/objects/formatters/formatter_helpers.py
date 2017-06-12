098cad34a724d7aa4a8c471ff2527404550bcb3b
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-12 14:20:45 -0600

    Re-adds give_specs (temporary)

--- packages/pygsti/objects/formatters/formatter_helpers.py
+++ packages/pygsti/objects/formatters/formatter_helpers.py
@@ -1,2 +1,25 @@
 def no_format(s):
     return str(s)
+
+def give_specs(formatter, specs):
+    '''
+    Pass parameters down to a formatter
+    Parameters
+    --------
+    formatter : callable, takes arguments
+    specs : dictionary of argnames : values
+    Returns
+    ------
+    None
+    Raises
+    ------
+    ValueError : If a needed spec is not supplied.
+    '''
+    # If the formatter requires a setting to do its job, give the setting
+    if hasattr(formatter, 'specs'):
+        for spec in formatter.specs:
+            if spec not in specs or specs[spec] is None:
+                raise ValueError(
+                        ('The spec %s was not supplied to ' % spec) +
+                        ('FormatSet, but is needed by an active formatter'))
+            formatter.specs[spec] = specs[spec]
--- packages/pygsti/objects/formatters/pieb_formatter.py
+++ packages/pygsti/objects/formatters/pieb_formatter.py
@@ -3,6 +3,7 @@ from .eb_formatter      import EBFormatter
 
 class PiEBFormatter(EBFormatter):
     def __call__(self, t, specs):
-        if str(t.get_val()) == '--' or str(t.get_val()) == '':  return t.get_val()
+        if str(t.get_val()) == '--' or str(t.get_val()) == '':  
+            return t.get_val()
         else:
             return super(PiEBFormatter, self).__call__(t)
