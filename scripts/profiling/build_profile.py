052ac829ed1059d3d1a4b37e17392c601a5d10a1
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-16 13:19:11 -0600

    Renames profile.py, separates profile_gauge_opt.py

--- scripts/profiling/build_profile.py
+++ scripts/profiling/build_profile.py
@@ -3,7 +3,7 @@ import subprocess, sys
 
 def main(args):
     assert len(args) == 2
-    subprocess.call('python2 -m cProfile -s cumtime {} 2>&1 |  tee {}'.format(
+    subprocess.call('python3 -m cProfile -s cumtime {} 2>&1 |  tee {}'.format(
         args[0], args[1]), shell=True)
     return 0
 
