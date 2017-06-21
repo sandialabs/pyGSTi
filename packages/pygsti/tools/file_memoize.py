950ef418d22b419d374dd79764f49789ee92c71a
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-21 09:31:29 -0600

    Adds file memoize

---
+++ packages/pygsti/tools/file_memoize.py
@@ -0,0 +1,131 @@
+import pickle
+import collections
+import functools
+import inspect
+import os.path
+import re
+import unicodedata
+
+class file_memoize(object):
+    '''
+    A function decorated with @file_memoize caches its return
+    value every time it is called. If the function is called
+    later with the same arguments, the cached value is
+    returned (the function is not reevaluated). The cache is
+    stored as a .cache file in the current directory for reuse
+    in future executions. If the Python file containing the
+    decorated function has been updated since the last run,
+    the current cache is deleted and a new cache is created
+    (in case the behavior of the function has changed).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.set_parent_file() # Sets self.parent_filepath and self.parent_filename
+        self.__name__ = self.func.__name__
+        self.set_cache_filename()
+        if self.cache_exists():
+            self.read_cache() # Sets self.timestamp and self.cache
+            if not self.is_safe_cache():
+                self.cache = {}
+        else:
+            self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            self.save_cache()
+            return value
+
+    def set_parent_file(self):
+        """
+        Sets self.parent_file to the absolute path of the
+        file containing the memoized function.
+        """
+        rel_parent_file = inspect.stack()[-1].filename
+        self.parent_filepath = os.path.abspath(rel_parent_file)
+        self.parent_filename = _filename_from_path(rel_parent_file)
+
+    def set_cache_filename(self):
+        """
+        Sets self.cache_filename to an os-compliant
+        version of "file_function.cache"
+        """
+        filename = _slugify(self.parent_filename.replace('.py', ''))
+        funcname = _slugify(self.__name__)
+        self.cache_filename = '.' + filename + '_' + funcname + '.cache'
+
+    def get_last_update(self):
+        """
+        Returns the time that the parent file was last
+        updated.
+        """
+        last_update = os.path.getmtime(self.parent_filepath)
+        return last_update
+
+    def is_safe_cache(self):
+        """
+        Returns True if the file containing the memoized
+        function has not been updated since the cache was
+        last saved.
+        """
+        if self.get_last_update() > self.timestamp:
+            return False
+        return True
+
+    def read_cache(self):
+        """
+        Read a pickled dictionary into self.timestamp and
+        self.cache. See self.save_cache.
+        """
+        with open(self.cache_filename, 'rb') as f:
+            data = pickle.loads(f.read())
+            self.timestamp = data['timestamp']
+            self.cache = data['cache']
+
+    def save_cache(self):
+        """
+        Pickle the file's timestamp and the function's cache
+        in a dictionary object.
+        """
+        with open(self.cache_filename, 'wb+') as f:
+            out = dict()
+            out['timestamp'] = self.get_last_update()
+            out['cache'] = self.cache
+            f.write(pickle.dumps(out))
+
+    def cache_exists(self):
+        '''
+        Returns True if a matching cache exists in the current directory.
+        '''
+        if os.path.isfile(self.cache_filename):
+            return True
+        return False
+
+    def __repr__(self):
+        """ Return the function's docstring. """
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        """ Support instance methods. """
+        return functools.partial(self.__call__, obj)
+
+def _slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes
+    non-alpha characters, and converts spaces to
+    hyphens. From
+    http://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-filename-in-python
+    """
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = re.sub(r'[^\w\s-]', '', value.decode('utf-8', 'ignore'))
+    value = value.strip().lower()
+    value = re.sub(r'[-\s]+', '-', value)
+    return value
+
+def _filename_from_path(filepath):
+    return filepath.split('/')[-1]
