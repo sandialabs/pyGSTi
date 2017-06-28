d2137bdc4508960332805c35c4d6ee5eb5532281
Lucas Saldyt <lucassaldyt@gmail.com>
2017-06-28 09:34:04 -0600

    Separates basisconstructors

--- packages/pygsti/tools/dim.py
+++ packages/pygsti/tools/dim.py
@@ -1,3 +1,10 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+#*****************************************************************
+#    pyGSTi 0.9:  Copyright 2015 Sandia Corporation
+#    This Software is released under the GPL license detailed
+#    in the file "license.txt" in the top-level pyGSTi directory
+#*****************************************************************
+
 import collections as _collections
 import numbers     as _numbers
 import numpy       as _np
