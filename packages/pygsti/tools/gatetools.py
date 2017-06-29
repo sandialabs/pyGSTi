Fixes Jacobian function for gates
This commit also includes commented out tests between the residuals() and frobeniusdist() function.
Some bugs have been addressed here.

Not only is least squares faster, it also provides a better answer in terms of the frobenius distance to the target gateset.

TODO: Add jacobian calculation for prep and effect vectors, generalize all numbers and reshaping so that the jacobian can be calculated with any gates/parameters/whatever

packages/pygsti/tools/gatetools.py
@@ -125,7 +125,7 @@ def residuals(A, B):
     np.array
         residuals
     """
-    return (A-B) ** 2
+    return (A-B).flatten()
 
 
 def tracenorm(A):