3ad770ebae61a39ed624ceac69b7421d68f79072
Lucas Saldyt <lucassaldyt@gmail.com>
2017-07-17 08:58:50 -0600

    Removes old memoize func (now in opttools)
    Also adds a few tests in gate and basis tools.
    
    Current lowest tools coverage is gatetools, at around 40%.
    mpitools is at 67%.
    All other tools subpackages are 90% or above

--- test/test_packages/tools/testGateTools.py
+++ test/test_packages/tools/testGateTools.py
@@ -88,9 +88,12 @@ class GateBaseTestCase(BaseTestCase):
         processMx = gatetools.unitary_to_process_mx(identity)
         self.assertArraysAlmostEqual(processMx, np.identity(4))
 
-    def test_other(self):
+    def test_err_gen(self):
         gs_target = std2Q_XXYYII.gs_target
         gs_datagen = gs_target.depolarize(gate_noise=0.1, spam_noise=0.001)
+
+        projectionTypes = ['hamiltonian', 'stochastic']
+        basisNames      = ['std', 'gm', 'pp', 'qt']
 
         for gateTarget, gate in zip(gs_target.gates.values(), gs_datagen.gates.values()):
             errgen    = gatetools.error_generator(gate, gateTarget)
@@ -98,11 +101,17 @@ class GateBaseTestCase(BaseTestCase):
             with self.assertRaises(ValueError):
                 gatetools.error_generator(gate, gateTarget, 'adsf')
 
+            #std_errgen_projections(errgen, projectionType, basisName)
+
             originalGate    = gatetools.gate_from_error_generator(errgen, gateTarget)
             altOriginalGate = gatetools.gate_from_error_generator(altErrgen, gateTarget, 'logTiG')
             with self.assertRaises(ValueError):
                 gatetools.gate_from_error_generator(errgen, gateTarget, 'adsf')
-
+        '''
+        for projectionType in projectionTypes:
+            for basisName in basisNames:
+                std_error_generators(4, projectionType, basisName)
+        '''
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
