3e10c3492aa255bb3472a58b8c0f4c7b5a8cf9ee
Erik Nielsen <enielse@sandia.gov>
2017-08-03 13:04:25 -0600

    Updates @parameterized decorator to be more self-documenting.
    Changes variable names within the parameterized function and
    adds a brief docstring.

--- packages/pygsti/tools/parameterized.py
+++ packages/pygsti/tools/parameterized.py
@@ -1,10 +1,16 @@
 from functools import wraps
 
 def parameterized(dec):
+    """
+    Used to create decorator functions that take arguments.  Functions
+    decorated with this function (which should be decorators themselves
+    but can have more than the standard single function argument), get
+    morphed into a standard decorator function.
+    """
     @wraps(dec)
-    def layer(*args, **kwargs):
-        @wraps(layer)
-        def repl(f):
+    def decorated_dec(*args, **kwargs): # new function that replaces dec, and returns a *standard* decorator function
+        @wraps(decorated_dec)
+        def standard_decorator(f): #std decorator (function that replaces f) that calls dec with more args
             return dec(f, *args, **kwargs)
-        return repl
-    return layer
+        return standard_decorator
+    return decorated_dec # function this replaces the action of dec
