be98abcb112609c62b45fbcf39ddb429f4d4728a
Erik Nielsen <enielse@sandia.gov>
2017-09-25 08:22:03 -0600

    Tweaks plotly plot sizing again for color box/matrix plots.
    Now hover labels are smaller text and should work even when
    displaying a long row of boxes.

--- packages/pygsti/report/workspaceplots.py
+++ packages/pygsti/report/workspaceplots.py
@@ -427,7 +427,7 @@ def generate_boxplot(subMxs,
                                       tickfont={'size': 10*scale, 'color': "black"})
 
     #Set plot size and margins
-    lmargin = rmargin = tmargin = bmargin = 10
+    lmargin = rmargin = tmargin = bmargin = 20
     if xlabel: bmargin += 30
     if ylabel: lmargin += 30
     if xlabels:
@@ -892,6 +892,7 @@ def matrix_color_boxplot(matrix, m, M, xlabels=None, ylabels=None,
     """
     if xlabels is None:  xlabels = [""] * matrix.shape[1]
     if ylabels is None:  ylabels = [""] * matrix.shape[0]
+    HOVER_PREC = 7 #precision for hover labels
 
     colorbar = colorbar if (colorbar is not None) else (not boxLabels)
     
@@ -904,13 +905,13 @@ def matrix_color_boxplot(matrix, m, M, xlabels=None, ylabels=None,
         def hoverLabelFn(i,j):
             val = flipped_mx[i, j]
             if _np.isnan(val): return ""
-            return "%s" % val #TODO: something better - or user-specifiable
+            return "%s" % round(val,HOVER_PREC) #TODO: something better - or user-specifiable
     else:
         flipped_EBmx = _np.flipud(EBmatrix)  # FLIP so [0,0] matrix el is at *top* left
         def hoverLabelFn(i,j):
             val,eb = flipped_mx[i,j], flipped_EBmx[i,j]
             if _np.isnan(val): return ""
-            return "%s +/- %s" % (val,eb) #TODO: something better - or user-specifiable
+            return "%s <br>+/- %s" % (round(val,HOVER_PREC),round(eb,HOVER_PREC)) #TODO: something better - or user-specifiable
     
     hoverLabels = []
     for i in range(matrix.shape[0]):
@@ -969,13 +970,13 @@ def matrix_color_boxplot(matrix, m, M, xlabels=None, ylabels=None,
                     dict(
                     text=_ph._eformat(matrix[iy,ix],prec),
                     x=ix, y=nY-1-iy, xref='x1', yref='y1',
-                    font=dict(size=scale*10,
+                    font=dict(size=scale*8,
                               color=colormap.besttxtcolor(matrix[iy,ix])),
                         showarrow=False)
                 )
 
     #Set plot size and margins
-    lmargin = rmargin = tmargin = bmargin = 10
+    lmargin = rmargin = tmargin = bmargin = 20
     if xlabel: tmargin += 30
     if ylabel: lmargin += 30
     max_xl = max([len(xl) for xl in xlabels])
@@ -984,8 +985,21 @@ def matrix_color_boxplot(matrix, m, M, xlabels=None, ylabels=None,
     if max_yl > 0: lmargin += max_yl*5
     if colorbar: rmargin = 100
 
-    width = lmargin + 15*matrix.shape[1] + rmargin
-    height = tmargin + 15*matrix.shape[0] + bmargin
+    boxSizeX = boxSizeY = 15
+
+    if boxLabels:
+        if prec in ('compact','compacthp'):
+            precnum = 3
+        else: precnum = abs(prec)+1
+        boxSizeX = boxSizeY = 8*precnum
+
+    if (matrix.shape[0] < 3):
+        # there is issue with hover info not working for
+        # small row of horizontal boxes
+        boxSizeY = max(35, boxSizeY)     
+      
+    width = lmargin + boxSizeX*matrix.shape[1] + rmargin
+    height = tmargin + boxSizeY*matrix.shape[0] + bmargin
 
     width *= scale
     height *= scale
@@ -1165,34 +1179,6 @@ class BoxKeyPlot(WorkspacePlot):
         )
         # margin = go.Margin(l=50,r=50,b=50,t=50) #pad=0
         return go.Figure(data=data, layout=layout)
-
-
-
-
-
-
-        
-#        layout = go.Layout(
-#            width=70*scale*(len(prepStrs)+1),
-#            height=70*scale*(len(effectStrs)+1),
-#
-#
-#        xs = [i+0.5 for i in range(len(prepStrs))]
-#        ys = [i+0.5 for i in range(len(effectStrs))]
-#        allys = []
-#        for y in ys: allys.extend( [y]*len(xs) )
-#        allxs = xs*len(ys)
-#        trace = go.Scatter(x=allxs, y=allys, mode="markers",
-#                           marker=dict(
-#                               size = 12,
-#                               color = 'rgba(0,0,255,0.8)',
-#                               line = dict(
-#                                   width = 2,
-#                               )),
-#                           xaxis='x1', yaxis='y1', hoverinfo='none')
-#                          
-#        fig = go.Figure(data=[trace], layout=layout)
-#        return fig
 
 
     
@@ -1942,15 +1928,17 @@ class ChoiEigenvalueBarPlot(WorkspacePlot):
         
     def _create(self, evals, errbars, scale):
 
+        HOVER_PREC = 7
         xs = list(range(evals.size))
         ys = []; colors = []; texts=[]
         for i,ev in enumerate(evals.flatten()):
             ys.append( abs(ev.real) )
             colors.append('rgb(200,200,200)' if ev.real > 0 else 'red')
             if errbars is not None:
-                texts.append("%g +/- %g" % (ev.real,errbars.flatten()[i].real))
+                texts.append("%g +/- %g" % (round(ev.real,HOVER_PREC),
+                                            round(errbars.flatten()[i].real,HOVER_PREC)))
             else:
-                texts.append("%g" % ev.real)
+                texts.append("%g" % round(ev.real,HOVER_PREC))
                 
         trace = go.Bar(
             x=xs, y=ys, text=texts,
