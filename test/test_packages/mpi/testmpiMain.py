f5cb71de8fb9d70a01b84c7d37886553d7a96cda
Erik Nielsen <enielse@sandia.gov>
2018-01-07 07:05:27 -0700

    2nd attempt to fix mpi unitest hanging: run1Q_end2end -> test_run1Q_end2end
    Not having the 'run1Q_end2end' unittest (in testmpiMain.py) begin with
    'test' was found to be the reason the mpi tests would "hang" (3 of 4
    mpi worker procs wouldn't terminate) on a laptop.  This didn't seem
    to be a problem on TravisCI but maybe it is now (?) - anyway, changing
    the name to 'test_run1Q_end2end' seems better all around, and might
    fix the TravisCI issue (which, as it turns out can sometimes be
    "fixed" by just re-running the single MPI test case/job on TravisCI
    that timed out).

--- test/test_packages/mpi/testmpiMain.py
+++ test/test_packages/mpi/testmpiMain.py
@@ -724,13 +724,13 @@ def test_MPI_derivcols(comm):
     return
 
 @mpitest(4)
-def run1Q_end2end(comm):
+def test_run1Q_end2end(comm):
     from pygsti.construction import std1Q_XYI
     gs_target = std1Q_XYI.gs_target
     fiducials = std1Q_XYI.fiducials
     germs = std1Q_XYI.germs
     maxLengths = [1,2,4]
-
+    
     gs_datagen = gs_target.depolarize(gate_noise=0.1, spam_noise=0.001)
     listOfExperiments = pygsti.construction.make_lsgst_experiment_list(
         list(gs_target.gates.keys()), fiducials, fiducials, germs, maxLengths)
@@ -738,10 +738,10 @@ def run1Q_end2end(comm):
                                                 nSamples=1000,
                                                 sampleError="binomial",
                                                 seed=1234)
-
+    
     results = pygsti.do_long_sequence_gst(ds, gs_target, fiducials, fiducials,
                                           germs, maxLengths, comm=comm)
-
+    
     pygsti.report.create_standard_report(results, "mpi_test_report",
                                          "MPI test report", confidenceLevel=95,
                                          verbosity=2, comm=comm)
