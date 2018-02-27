91c77afc9d0055d97120d0f749f23f13e060d3b5
Kenneth Rudinger <kmrudin@sandia.gov>
2018-02-26 17:28:39 -0700

    Update RPE unit tests; add rpeconfig_GxPi2_GyPi2_00 config file, which should be default config file for use with std1Q_XY and std1Q_XYI.

--- test/test_packages/extras/testRPEObjects.py
+++ test/test_packages/extras/testRPEObjects.py
@@ -6,6 +6,7 @@ import numpy as np
 import pygsti.extras.rpe as rpe
 import pygsti.extras.rpe.rpeconstruction as rc
 from pygsti.extras.rpe.rpeconfig_GxPi2_GyPi2_UpDn import rpeconfig_GxPi2_GyPi2_UpDn
+from pygsti.extras.rpe.rpeconfig_GxPi2_GyPi2_00 import rpeconfig_GxPi2_GyPi2_00
 
 class TestRPEObjectMethods(BaseTestCase):
 
@@ -17,22 +18,23 @@ class TestRPEObjectMethods(BaseTestCase):
         depol_gateset = gateset.depolarize(gate_noise=0.1,spam_noise=0)
 
         #test RPE datasets
-        rpeconfig_inst = rpeconfig_GxPi2_GyPi2_UpDn
-        rpeGS  = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, 0, 0.1, 0.1, True, rpeconfig_inst=rpeconfig_inst)
-        rpeGS2 = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, 0, 0.1, 0.1, False, rpeconfig_inst=rpeconfig_inst)
-        rpeGS3 = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, np.pi/4, 0.1, 0.1, False, rpeconfig_inst=rpeconfig_inst)
+        rpeconfig_inst_list = [rpeconfig_GxPi2_GyPi2_UpDn,rpeconfig_GxPi2_GyPi2_00]
+        for rpeconfig_inst in rpeconfig_inst_list:
+            rpeGS  = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, 0, 0.1, 0.1, True, rpeconfig_inst=rpeconfig_inst)
+            rpeGS2 = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, 0, 0.1, 0.1, False, rpeconfig_inst=rpeconfig_inst)
+            rpeGS3 = rc.make_parameterized_rpe_gate_set(np.pi/2, np.pi/4, np.pi/4, 0.1, 0.1, False, rpeconfig_inst=rpeconfig_inst)
 
-        kList = [0,1,2]
-        lst1 = rc.make_rpe_angle_str_lists(kList, "alpha", rpeconfig_inst)
-        lst2 = rc.make_rpe_angle_str_lists(kList, "epsilon", rpeconfig_inst)
-        lst3 = rc.make_rpe_angle_str_lists(kList, "theta", rpeconfig_inst)
-        lstDict = rc.make_rpe_angle_string_list_dict(2,rpeconfig_inst)
+            kList = [0,1,2]
+            lst1 = rc.make_rpe_angle_str_lists(kList, "alpha", rpeconfig_inst)
+            lst2 = rc.make_rpe_angle_str_lists(kList, "epsilon", rpeconfig_inst)
+            lst3 = rc.make_rpe_angle_str_lists(kList, "theta", rpeconfig_inst)
+            lstDict = rc.make_rpe_angle_string_list_dict(2,rpeconfig_inst)
 
-        rpeDS = rc.make_rpe_data_set(depol_gateset,lstDict,1000,
-                                     sampleError='binomial',seed=1234)
+            rpeDS = rc.make_rpe_data_set(depol_gateset,lstDict,1000,
+                                         sampleError='binomial',seed=1234)
 
-        #Just make sure this runs:
-        #Removed? rc.rpe_ensemble_test(np.pi/2, np.pi/4, 0, 0.1, 2, 1000, 2)
+            #Just make sure this runs:
+            #Removed? rc.rpe_ensemble_test(np.pi/2, np.pi/4, 0, 0.1, 2, 1000, 2)
 
     def test_bad_rpeconfig(self):
         #Note: this doesn't actually raise an exception, it just prints a warning message to stdout
