bd89e8a4849ae06527c39f3b7ffa880e6f3dc80d
Timothy J Proctor <tjproct@.sandia.gov>
2018-08-23 09:49:38 -0700

    Updated SSTVD and added TVD in datacomparator. Moved the hypothesis.py tools to the tools folder

--- packages/pygsti/hypothesis.py
+++ packages/pygsti/hypothesis.py
@@ -1,10 +1,10 @@
+"""Tools for general statistical hypothesis testing"""
 from __future__ import division, print_function, absolute_import, unicode_literals
 #*****************************************************************
 #    pyGSTi 0.9:  Copyright 2015 Sandia Corporation
 #    This Software is released under the GPL license detailed
 #    in the file "license.txt" in the top-level pyGSTi directory
 #*****************************************************************
-"""Functions for general statistical hypothesis testing"""
 
 import numpy as _np
 
