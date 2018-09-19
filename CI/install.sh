bd619adc91924df5240499271685b63967ad5c62
Erik Nielsen <enielse@sandia.gov>
2018-09-19 13:34:55 -0600

    Test if "trusty" TravisCI image can be used without building as much.
    Namely - can we *not* build gcc & cmake in install.sh now (this
    commit comments it out to test)

--- CI/install.sh
+++ CI/install.sh
@@ -24,41 +24,41 @@ apt-get install libsuitesparse-dev  > /dev/null 2>&1
 cp /usr/lib/liblapack.so /usr/lib/libsuitesparseconfig.so  > /dev/null 2>&1
 echo "SuiteSparse complete"
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test > /dev/null 2>&1
-sudo apt-get update > /dev/null 2>&1
-
-echo "Reinstalling gcc/g++ to get newer versions"
-sudo update-alternatives --remove-all gcc  > /dev/null 2>&1
-sudo update-alternatives --remove-all g++  > /dev/null 2>&1
-sudo apt-get install gcc-4.8  > /dev/null 2>&1
-sudo apt-get install g++-4.8  > /dev/null 2>&1
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20  > /dev/null 2>&1
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20  > /dev/null 2>&1
-sudo update-alternatives --config gcc  > /dev/null 2>&1
-sudo update-alternatives --config g++  > /dev/null 2>&1
-sudo apt-get update  > /dev/null 2>&1
-sudo apt-get upgrade -y  > /dev/null 2>&1
-sudo apt-get dist-upgrade  > /dev/null 2>&1
-
-export CXX=g++
-echo "gcc/g++ install complete"
-
-cmake --version
-sudo apt remove cmake  > /dev/null 2>&1
-
-# Install the following version of CMAKE
-echo "Installing newer version of cmake"
-version=3.11  > /dev/null 2>&1
-build=1  > /dev/null 2>&1
-mkdir ~/temp  > /dev/null 2>&1
-cd ~/temp  > /dev/null 2>&1
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz  > /dev/null 2>&1
-tar -xzvf cmake-$version.$build.tar.gz  > /dev/null 2>&1
-cd cmake-$version.$build/  > /dev/null 2>&1
-./bootstrap  > /dev/null 2>&1
-make -j4  > /dev/null 2>&1
-sudo make install  > /dev/null 2>&1
-cd ..  > /dev/null 2>&1
-#rm -r temp  # > /dev/null 2>&1
-export PATH=/usr/local/bin:$PATH
+#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test > /dev/null 2>&1
+#sudo apt-get update > /dev/null 2>&1
+#
+#echo "Reinstalling gcc/g++ to get newer versions"
+#sudo update-alternatives --remove-all gcc  > /dev/null 2>&1
+#sudo update-alternatives --remove-all g++  > /dev/null 2>&1
+#sudo apt-get install gcc-4.8  > /dev/null 2>&1
+#sudo apt-get install g++-4.8  > /dev/null 2>&1
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20  > /dev/null 2>&1
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20  > /dev/null 2>&1
+#sudo update-alternatives --config gcc  > /dev/null 2>&1
+#sudo update-alternatives --config g++  > /dev/null 2>&1
+#sudo apt-get update  > /dev/null 2>&1
+#sudo apt-get upgrade -y  > /dev/null 2>&1
+#sudo apt-get dist-upgrade  > /dev/null 2>&1
+#
+#export CXX=g++
+#echo "gcc/g++ install complete"
+#
+#cmake --version
+#sudo apt remove cmake  > /dev/null 2>&1
+#
+## Install the following version of CMAKE
+#echo "Installing newer version of cmake"
+#version=3.11  > /dev/null 2>&1
+#build=1  > /dev/null 2>&1
+#mkdir ~/temp  > /dev/null 2>&1
+#cd ~/temp  > /dev/null 2>&1
+#wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz  > /dev/null 2>&1
+#tar -xzvf cmake-$version.$build.tar.gz  > /dev/null 2>&1
+#cd cmake-$version.$build/  > /dev/null 2>&1
+#./bootstrap  > /dev/null 2>&1
+#make -j4  > /dev/null 2>&1
+#sudo make install  > /dev/null 2>&1
+#cd ..  > /dev/null 2>&1
+##rm -r temp  # > /dev/null 2>&1
+#export PATH=/usr/local/bin:$PATH
 cmake --version
