c4d9a2763d65ed962d5e3953fabe1cc0f96bf4fe
Erik Nielsen <enielse@sandia.gov>
2019-05-22 23:10:59 -0600

    Updates default `method` in DataSet.get_degrees_of_freedom.
    Changes from "all_outcomes-1" to "present_outcomes-1", as this is
    the more appropriate method for many qubit data.  It's possible
    this will cause problems for 1Q or 2Q data that happens to have
    lots of zeros in it, but I don't think it should.  Time dependent
    data is probably also not handled well here, but we'll put worrying
    about that off until later.

--- packages/pygsti/objects/dataset.py
+++ packages/pygsti/objects/dataset.py
@@ -818,7 +818,7 @@ class DataSet(object):
                     if opLabel not in opLabels: opLabels.append(opLabel)
         return opLabels
 
-    def get_degrees_of_freedom(self, circuitList=None, method="all_outcomes-1",
+    def get_degrees_of_freedom(self, circuitList=None, method="present_outcomes-1",
                                aggregate_times=True):
         """
         Returns the number of independent degrees of freedom in the data for
