837e71a04a981c302a082dd87ec16dee40fc262b
Rob Kelly <rpkelly@sandia.gov>
2019-06-07 16:16:35 -0600

    Marking todos in tools.internalgates tests

--- test/unit/tools/test_internalgates.py
+++ test/unit/tools/test_internalgates.py
@@ -1,16 +1,14 @@
 from ..util import BaseCase
 import numpy as np
 
-import pygsti
-from pygsti import unitary_to_pauligate
 from pygsti.extras import rb
-from pygsti.tools import internalgates
+from pygsti.tools import internalgates, optools as ot
 
 
 class InternalGatesTestCase(BaseCase):
 
-    def test_internalgates(self):
-        # Checks we can get the dicts this file generates.
+    def test_internalgate_definitions(self):
+        # TODO is this test needed?
         std_unitaries = internalgates.get_standard_gatename_unitaries()
         std_quil = internalgates.get_standard_gatenames_quil_conversions()
         std_quil = internalgates.get_standard_gatenames_openqasm_conversions()
@@ -18,9 +16,11 @@ class InternalGatesTestCase(BaseCase):
         # Checks the standard Clifford gate unitaries agree with the Clifford group unitaries.
         group = rb.group.construct_1Q_Clifford_group()
         for key in group.labels:
-            self.assertLess(np.sum(abs(np.array(group.get_matrix(key)) -
-                                       unitary_to_pauligate(std_unitaries[key]))), 10**-10)
+            self.assertLess(np.sum(abs(np.array(group.get_matrix(key))
+                                       - ot.unitary_to_pauligate(std_unitaries[key]))), 10**-10)
 
+    def test_u3_unitary_generator(self):
         # Checks the u3 unitary generator runs
         u = internalgates.qasm_u3(0., 0., 0., output='unitary')
         sup = internalgates.qasm_u3(0., 0., 0., output='superoperator')
+        # TODO assert correctness
