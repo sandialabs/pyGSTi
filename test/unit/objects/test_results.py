45f0941ddc44c843363b12facfc3f67cd8c98d9d
Rob Kelly <rpkelly@sandia.gov>
2019-06-19 13:22:06 -0600

    Refactored unit tests in test.unit.objects.test_results

--- test/unit/objects/test_results.py
+++ test/unit/objects/test_results.py
@@ -7,88 +7,118 @@ from pygsti.objects import results, estimate
 from pygsti.construction import std1Q_XYI as std
 
 
-class ResultsTester(BaseCase):
-    def test_results(self):
-        # TODO break apart into unit tests
-        #prepare a results object
-        model = pkg.mdl_lsgst_go
-        ds = pkg.dataset
-        maxLengthList = pkg.maxLengthList
-        gss = pkg.lsgstStructs
+class ResultsBase:
+    def setUp(self):
+        # prepare a results object
+        self.model = pkg.mdl_lsgst_go
+        self.ds = pkg.dataset
+        self.maxLengthList = pkg.maxLengthList
+        self.gss = pkg.lsgstStructs
 
-        #init results
-        res = results.Results()
+        # Construct results but do not init
+        self.res = results.Results()
 
+
+class BareResultsTester(ResultsBase, BaseCase):
+    def test_init_dataset(self):
+        self.res.init_dataset(self.ds)
+        # TODO assert correctness
+
+    def test_init_circuits(self):
+        self.res.init_dataset(self.ds)
+        self.res.init_circuits(self.gss)
+        # TODO assert correctness
+
+    def test_add_estimate_raises_before_initialization(self):
+        # Raise before initializing dataset
         with self.assertRaises(ValueError):
-            res.add_estimate(None, None, None, None)  # dataset not init yet
-        res.init_dataset(ds)
+            self.res.add_estimate(None, None, None, None)
+        self.res.init_dataset(self.ds)
 
+        # Raise before initializing circuits
         with self.assertRaises(ValueError):
-            res.add_estimate(None, None, None, None)  # gss not init yet
-        res.init_circuits(gss)
+            self.res.add_estimate(None, None, None, None)
 
+
+class ResultsTester(ResultsBase, BaseCase):
+    def setUp(self):
+        super(ResultsTester, self).setUp()
+        # Init results before tests
+        self.res.init_dataset(self.ds)
+        self.res.init_circuits(self.gss)
+
+    def test_init_warns_on_reinitialization(self):
         with self.assertWarns(Warning):
-            res.init_dataset(ds)  # usually don't want to re-init
+            self.res.init_dataset(self.ds)  # usually don't want to re-init
         with self.assertWarns(Warning):
-            res.init_circuits(gss)  # usually don't want to re-init
+            self.res.init_circuits(self.gss)  # usually don't want to re-init
+
+    def test_init_circuits_raises_on_bad_arg(self):
+        # XXX Don't test this unless init_circuits can actually take a string
+        # (and if it can, document it)
         with self.assertRaises(ValueError):
-            res.init_circuits("foobar")
-        res.init_circuits(gss)  # make sure "foobar" test above doesn't leave results in an un-init state
+            self.res.init_circuits("foobar")
 
-        #add estimates
-        res.add_estimate(std.target_model(), std.target_model(),
-                         [model] * len(maxLengthList), parameters={'objective': 'logl'},
-                         estimate_key="default")
+    def test_add_estimate(self):
+        # add estimates
+        self.res.add_estimate(
+            std.target_model(), std.target_model(),
+            [self.model] * len(self.maxLengthList), parameters={'objective': 'logl'},
+            estimate_key="default"
+        )
+        # TODO assert correctness
+
+
+class PopulatedResultsTester(ResultsBase, BaseCase):
+    def setUp(self):
+        super(PopulatedResultsTester, self).setUp()
+        self.res.init_dataset(self.ds)
+        self.res.init_circuits(self.gss)
+        self.res.add_estimate(
+            std.target_model(), std.target_model(),
+            [self.model] * len(self.maxLengthList), parameters={'objective': 'logl'},
+            estimate_key="default"
+        )
+
+    def test_add_estimate_warns_on_overwrite(self):
         with self.assertWarns(Warning):
-            res.add_estimate(std.target_model(), std.target_model(),
-                             [model] * len(maxLengthList), parameters={'objective': 'logl'},
-                             estimate_key="default")  # re-init existing estimate
+            self.res.add_estimate(
+                std.target_model(), std.target_model(),
+                [self.model] * len(self.maxLengthList), parameters={'objective': 'logl'},
+                estimate_key="default"
+            )  # re-init existing estimate
 
-        #Estimate & results render as str
-        print(str(res))
+    def test_to_string(self):
+        s = str(self.res)
+        # TODO assert correctness
 
+    def test_view(self):
         #Results views
-        rview = res.view(['default'])
-        rview2 = res.view('default')  # this works too
+        rview = self.res.view(['default'])
+        rview2 = self.res.view('default')  # this works too
+        # TODO assert correctness
 
+    def test_add_estimate_from_results(self):
         # add_estimates from other results
         res2 = results.Results()
+        res2.init_dataset(self.ds)
+        res2.init_circuits(self.gss)
 
-        with self.assertRaises(ValueError):
-            res2.add_estimates(res, ['default'])  # ds not init yet...
-        res2.init_dataset(ds)
+        res2.add_estimates(self.res, ['default'])
 
-        with self.assertRaises(ValueError):
-            res2.add_estimates(res, ['default'])  # gss not init yet...
-        res2.init_circuits(gss)
-
-        res2.add_estimates(res, ['default'])  # now it works!
-        with self.assertWarns(Warning):
-            res2.add_estimates(res, ['default'])  # b/c re-init exising estimate
-
+    def test_rename_estimate(self):
         # rename estimates
-        res2.rename_estimate('default', 'renamed_default')
-        with self.assertRaises(KeyError):
-            res2.rename_estimate('foobar', 'renamed_foobar')
+        self.res.rename_estimate('default', 'renamed_default')
 
+    def test_rename_estimate_raises_on_missing_key(self):
+        with self.assertRaises(KeyError):
+            self.res.rename_estimate('foobar', 'renamed_foobar')
+
+    def test_add_model_test(self):
         # add estimate from model test
         mdl_guess = std.target_model().depolarize(op_noise=0.07, spam_noise=0.03)
-        res2.add_model_test(std.target_model(), mdl_guess, estimate_key='Test', gauge_opt_keys="auto")
-
-        chi2_res = results.Results()
-        chi2_res.init_dataset(ds)
-        chi2_res.init_circuits(gss)
-        chi2_res.add_estimate(std.target_model(), std.target_model(),
-                              [model] * len(maxLengthList), parameters={'objective': 'chi2'},
-                              estimate_key="default")
-        chi2_res.add_model_test(std.target_model(), mdl_guess, estimate_key='Test', gauge_opt_keys="auto")
-
-        chi2_res.estimates['default'].parameters['objective'] = "foobar"  # sets up error below
-        chi2_res.estimates['Test'].parameters['objective'] = "foobar"
-        #print("DB: ",chi2_res.estimates.keys())
-        with self.assertRaises(ValueError):
-            chi2_res.add_model_test(std.target_model(), mdl_guess,
-                                    estimate_key='Test', gauge_opt_keys="auto")  # invalid "objective"
+        self.res.add_model_test(std.target_model(), mdl_guess, estimate_key='Test', gauge_opt_keys="auto")
+        # TODO assert correctness
 
     def test_results_warns_on_deprecated(self):
         #deprecated functions that issue warnings
