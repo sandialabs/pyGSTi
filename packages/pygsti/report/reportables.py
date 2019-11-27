0efe697e49cd41464219de18cbe96c388055f0d9
Rob Kelly <rpkelly@sandia.gov>
2019-11-27 15:58:56 -0700

    Avoid loading `cvxpy` at import time.
    We only want to know if `cvxpy` is _available_ at import time. We can
    check that directly with `pkgutil`. Saves about ~0.1s from import time
    on my machine.

--- packages/pygsti/report/reportables.py
+++ packages/pygsti/report/reportables.py
@@ -26,18 +26,8 @@ from ..baseobjs import Label as _Lbl
 from ..objects.reportableqty import ReportableQty as _ReportableQty
 from ..objects import modelfunction as _modf
 
-try:
-    import sys as _sys
-    if _sys.version_info < (3, 0):
-        #Attempt "safe" import of cvxpy so that pickle isn't messed up...
-        import pickle as _pickle
-        p = _pickle.Pickler.dispatch.copy()
-        import cvxpy as _cvxpy
-        _pickle.Pickler.dispatch = p
-    else:
-        import cvxpy as _cvxpy
-except ImportError:
-    _cvxpy = None
+import pkgutil
+_CVXPY_AVAILABLE = pkgutil.find_loader('cvxpy') is not None
 
 FINITE_DIFF_EPS = 1e-7
 
@@ -278,7 +268,7 @@ def circuit_jt_diff(modelA, modelB, circuit):
 Circuit_jt_diff = _modf.modelfn_factory(circuit_jt_diff)
 # init args == (modelA, modelB, circuit)
 
-if _cvxpy:
+if _CVXPY_AVAILABLE:
 
     class Circuit_half_diamond_norm(_modf.ModelFunction):
         """ 1/2 diamond norm of difference between productA(circuit)
@@ -444,7 +434,7 @@ def povm_jt_diff(modelA, modelB, povmlbl):
 POVM_jt_diff = _modf.povmfn_factory(povm_jt_diff)
 # init args == (model1, modelB, povmlbl)
 
-if _cvxpy:
+if _CVXPY_AVAILABLE:
 
     def povm_half_diamond_norm(modelA, modelB, povmlbl):
         """
@@ -618,7 +608,7 @@ Jt_diff = _modf.opsfn_factory(jt_diff)
 # init args == (model1, model2, opLabel)
 
 
-if _cvxpy:
+if _CVXPY_AVAILABLE:
 
     class Half_diamond_norm(_modf.ModelFunction):
         """Half the diamond distance bewteen `modelA.operations[opLabel]` and
