d6949df54ac73c714210fbdc5ed9f344be3589bd
Erik Nielsen <enielse@sandia.gov>
2020-01-22 12:55:55 -0700

    Updates FPR unit test to accept either of 2 possible sets of pairs.
    It seems like some underlying differences in machines (maybe numpy
    verions?) causes different (but correct) results when finding
    fiducial pairs sequentially.  This update adds both correct "variants"
    to the unit test so it will pass in either case.  (Eriks laptop
    generates one case and the TravisCI machines may generate the other.)

--- test/unit/algorithms/test_fiducialpairreduction.py
+++ test/unit/algorithms/test_fiducialpairreduction.py
@@ -16,24 +16,24 @@ class FiducialPairReductionStdData(object):
         self.effects = fixtures.fiducials
         self.germs = fixtures.germs
         self.fiducial_pairs = [(0, 0), (0, 1), (0, 2)]
-        #self.fiducial_pairs_per_germ = {
-        #    Circuit(('Gi',)): [(0, 0), (1, 1), (2, 2)],
-        #    Circuit(('Gx',)): [(0, 0), (0, 1), (0, 2), (2, 2)],
-        #    Circuit(('Gy',)): [(0, 0), (0, 1), (0, 2), (1, 1)],
-        #    Circuit(('Gx', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0)],
-        #    Circuit(('Gx', 'Gx', 'Gy')): [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)],
-        #    Circuit(('Gx', 'Gy', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2)],
-        #    Circuit(('Gx', 'Gy', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 0)],
-        #    Circuit(('Gx', 'Gi', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0)],
-        #    Circuit(('Gx', 'Gi', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 2)],
-        #    Circuit(('Gy', 'Gi', 'Gi')): [(0, 0), (0, 1), (0, 2), (1, 1)],
-        #    Circuit(('Gx', 'Gy', 'Gy', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2)],
-        #    Circuit(('Gx', 'Gx', 'Gy', 'Gx', 'Gy', 'Gy')): [(0, 0), (0, 1), (0, 2), (1, 0)]
-        #}
+        self.fiducial_pairs_per_germ = {
+            Circuit(('Gi',)): [(0, 0), (1, 1), (2, 2)],
+            Circuit(('Gx',)): [(0, 0), (0, 1), (0, 2), (2, 2)],
+            Circuit(('Gy',)): [(0, 0), (0, 1), (0, 2), (1, 1)],
+            Circuit(('Gx', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0)],
+            Circuit(('Gx', 'Gx', 'Gy')): [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)],
+            Circuit(('Gx', 'Gy', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2)],
+            Circuit(('Gx', 'Gy', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 0)],
+            Circuit(('Gx', 'Gi', 'Gy')): [(0, 0), (0, 1), (0, 2), (2, 0)],
+            Circuit(('Gx', 'Gi', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 2)],
+            Circuit(('Gy', 'Gi', 'Gi')): [(0, 0), (0, 1), (0, 2), (1, 1)],
+            Circuit(('Gx', 'Gy', 'Gy', 'Gi')): [(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2)],
+            Circuit(('Gx', 'Gx', 'Gy', 'Gx', 'Gy', 'Gy')): [(0, 0), (0, 1), (0, 2), (1, 0)]
+        }
 
         #I think this alternate set is due to a slightly different search ordering - it
         # still looks like a correct output (there are many), so we'll call this OK for now:
-        self.fiducial_pairs_per_germ = {
+        self.fiducial_pairs_per_germ_alt = {
             Circuit(('Gi',)): [(0, 0), (1, 1), (2, 2)],
             Circuit(('Gx',)): [(0, 0), (0, 1), (0, 2), (2, 0)],  # 2,2 => 2,0
             Circuit(('Gy',)): [(0, 0), (0, 1), (0, 2), (1, 0)],  # 1,1 => 1,0
@@ -82,7 +82,8 @@ class FindSufficientFiducialPairsPerGermBase(object):
             self.model, self.preps, self.effects, self.germs,
             searchMode='sequential'
         )
-        self.assertEqual(fiducial_pairs, self.fiducial_pairs_per_germ)
+        self.assertTrue(fiducial_pairs == self.fiducial_pairs_per_germ or
+                        fiducial_pairs == self.fiducial_pairs_per_germ_alt)
 
     def test_find_sufficient_fiducial_pairs_per_germ_random(self):
         fiducial_pairs = fpr.find_sufficient_fiducial_pairs_per_germ(
