d065b59be3f1538a254efeed51822f30733ce849
Rob Kelly <rpkelly@sandia.gov>
2020-02-04 15:33:17 -0700

    Rewrote notebook tests to dynamically collect and test notebooks

--- test/test_packages/Tutorials/notebookstestcase.py
+++ test/test_packages/Tutorials/notebookstestcase.py
@@ -1,93 +1,45 @@
-import unittest
-import os, sys
-import importlib
-import pygsti
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from nose.plugins.attrib import attr
 
-#if __name__ == "__main__":
-#    tutorialFile = "01 Models.ipynb"
-#    tutorialDir = os.path.join("..","jupyter_notebooks","Tutorials")
-#    tutorialModuleName = os.path.splitext(tutorialFile)[0]
-#    os.chdir(tutorialDir)
-#    sys.path.append(".") #tutorialDir)
-#    #cwd = os.getcwd()
-#    #os.chdir(tutorialDir)
-#    tutorial_module = importlib.import_module(tutorialModuleName)
-#    #os.chdir(cwd)
-#    #sys.path.pop()
-#    exit()
 
-class NotebooksTestCase(unittest.TestCase):
+_DEFAULT_IPYNB_VERSION = 4
+_DEFAULT_TIMEOUT = 60 * 10  # 10 minutes
 
-    def setUp(self):
-        self.old = os.getcwd()
-        os.chdir(os.path.abspath(os.path.dirname(__file__)))
+# hardcoded path to pyGSTi root
+# XXX change if refactoring
+_PYGSTI_ROOT = Path(__file__).absolute().parent.parent.parent.parent
 
-        #Set Model objects to non-"strict" mode, as this would be the
-        # tutorial environment
-        pygsti.objects.ExplicitOpModel._strict = False
 
-    def tearDown(self):
-        os.chdir(self.old)
+def run_notebook(path, workdir=None, timeout=_DEFAULT_TIMEOUT):
+    resources = {
+        'metadata': {
+            'path': workdir
+        }
+    } if workdir is not None else None
 
-    def runNotebook_as_script(self, tutorialDir, tutorialFile):
-        tutorialModuleName = os.path.splitext(tutorialFile)[0]
-        cwd = os.getcwd()
-        os.chdir(tutorialDir)
-        os.system('jupyter nbconvert --to script "%s"' % tutorialFile)
-        sys.path.append(".") # not "tutorialDir" b/c of chdir above
+    with open(path, 'r') as f:
+        nb = nbformat.read(f, as_version=_DEFAULT_IPYNB_VERSION)
+    ep = ExecutePreprocessor(timeout=timeout)
+    ep.preprocess(nb, resources=resources)
 
-        orig_stdout = sys.stdout
-        sys.stdout = open(tutorialModuleName + ".out","w")
 
-        try:
-            tutorial_module = importlib.import_module(tutorialModuleName)
-            sys.stdout.close()
-            os.remove(tutorialModuleName + ".py") #only remove if all is OK
-            os.remove(tutorialModuleName + ".pyc")
+def notebooks_in_path(root_path, ignore_checkpoints=True):
+    for path, _, files in os.walk(root_path):
+        if '.ipynb_checkpoints' not in path:
+            for ipynb in filter(lambda s: s.endswith('.ipynb'), files):
+                yield Path(path) / ipynb
 
-            #do comparison with accepted tutorial output?
-            os.remove(tutorialModuleName + ".out") #only remove if all is OK
 
-        finally:
-            sys.stdout.close()
-            sys.stdout = orig_stdout
-            sys.path.pop()
-            os.chdir(cwd)
-
-    def runNotebook_jupyter(self, tutorialDir, tutorialFile, inplace=True):
-
-        #check whether an jupyter notebook contains any "error" cells
-        def containsErrors(fn):
-            with open(fn, 'r') as fn_file:
-                for line in fn_file:
-                    if '"output_type": "error"' in line:
-                        return True
-                return False
-
-        
-        tutorialName,Ext = os.path.splitext(tutorialFile)
-
-        cwd = os.getcwd()
-        os.chdir(tutorialDir)
-        if inplace:
-            os.system('jupyter nbconvert --to notebook --execute ' +
-                      '--ExecutePreprocessor.timeout=3600 --inplace ' +
-                      '"%s"' % tutorialFile)
-            outName = tutorialFile # executed notebook (side benefit of updating notebook)
-            if containsErrors(outName):
-                os.chdir(cwd)
-                raise ValueError("Error(s) occurred when running tutorial '%s'." % tutorialFile +
-                                 "  open notebook to see error details.")
-            # (never remove original notebook)
-        else:
-            os.system('jupyter nbconvert --to notebook --execute "%s"' % tutorialFile)
-            outName = tutorialName + ".nbconvert." + Ext # executed notebook
-            if containsErrors(outName):
-                os.chdir(cwd)
-                raise ValueError("Error(s) occurred when running tutorial '%s'." % tutorialFile +
-                                 "  Open output notebook '%s' for details." % outName)
-
-            else:
-                os.remove(outName)
-        os.chdir(cwd)
-
+def _test_notebooks_in_path(root_path):
+    with TemporaryDirectory() as tmp:
+        tmp_path = Path(tmp)
+        for nb_path in notebooks_in_path(root_path):
+            rel_path = nb_path.relative_to(root_path)
+            workdir = tmp_path / rel_path.parent
+            workdir.mkdir(parents=True, exist_ok=True)
+            description = "Running notebook {}".format(rel_path)
+            yield attr(description=description)(run_notebook), nb_path, workdir
--- test/test_packages/Tutorials/testExamples.py
+++ test/test_packages/Tutorials/testExamples.py
@@ -1,38 +1,7 @@
-from notebookstestcase import NotebooksTestCase
+from notebookstestcase import _PYGSTI_ROOT, _test_notebooks_in_path
 
-class NotebooksMethods(NotebooksTestCase):
-    def test_2QGST_CreatingModels(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","2QGST-CreatingModels.ipynb")
+_EXAMPLES_ROOT = _PYGSTI_ROOT / 'jupyter_notebooks' / 'Examples'
 
-    def test_2QGST_ErrorBars(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","2QGST-ErrorBars.ipynb")
 
-    def test_2QGST_RunningIt(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","2QGST-RunningIt.ipynb")
-
-    def test_BootstrappedErrorBars(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","BootstrappedErrorBars.ipynb")
-
-    def test_ContextDependence(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","ContextDependence.ipynb")
-
-    def test_GOpt_AddingNewOptimizations(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","GOpt-AddingNewOptimizations.ipynb")
-
-    def test_GOpt_NonIdealTargets(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","GOpt-NonIdealTargets.ipynb")
-
-    def test_Leakage(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","Leakage.ipynb")
-
-    def test_MPI_GermSelection(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","MPI-GermSelection.ipynb")
-
-    def test_MPI_RunningGST(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","MPI-RunningGST.ipynb")
-
-    def test_QutritGST(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","QutritGST.ipynb")
-
-    def test_Reports_LGSTonly(self):
-        self.runNotebook_jupyter("../../../jupyter_notebooks/Examples","Reports-LGSTonly.ipynb")
+def test_examples():
+    yield from _test_notebooks_in_path(_EXAMPLES_ROOT)
