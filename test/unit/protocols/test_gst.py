e2272ecb024f794dd6e3d213879bd6f77e71d0c5
Rob Kelly <rpkelly@sandia.gov>
2020-04-13 18:55:49 -0600

    Added unit test stubs for `protocols.gst`, `objects.objectivefns`

--- test/unit/protocols/test_gst.py
+++ test/unit/protocols/test_gst.py
@@ -37,3 +37,283 @@ class GSTUtilTester(BaseCase):
         model_1Q = std1Q_XYI.target_model()
         with self.assertRaises(ValueError):
             gst.gaugeopt_suite_to_dictionary("foobar", model_1Q, verbosity=1)
+
+    def test_add_badfit_estimates(self):
+        raise NotImplementedError()  # TODO: test add_badfit_estimates
+
+    def test_add_gauge_opt(self):
+        raise NotImplementedError()  # TODO: test add_gauge_opt
+
+
+class HasTargetModelTester(BaseCase):
+    """
+    Tests for methods in the HasTargetModel class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+
+class GateSetTomographyDesignTester(BaseCase):
+    """
+    Tests for methods in the GateSetTomographyDesign class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+
+class StructuredGSTDesignTester(BaseCase):
+    """
+    Tests for methods in the StructuredGSTDesign class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+
+class StandardGSTDesignTester(BaseCase):
+    """
+    Tests for methods in the StandardGSTDesign class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+
+class GSTInitialModelTester(BaseCase):
+    """
+    Tests for methods in the GSTInitialModel class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_build(self):
+        raise NotImplementedError()  # TODO: test build
+
+    def test_get_model(self):
+        raise NotImplementedError()  # TODO: test get_model
+
+
+class GSTBadFitOptionsTester(BaseCase):
+    """
+    Tests for methods in the GSTBadFitOptions class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_build(self):
+        raise NotImplementedError()  # TODO: test build
+
+
+class GSTObjFnBuildersTester(BaseCase):
+    """
+    Tests for methods in the GSTObjFnBuilders class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_build(self):
+        raise NotImplementedError()  # TODO: test build
+
+    def test_init_simple(self):
+        raise NotImplementedError()  # TODO: test init_simple
+
+
+class GateSetTomographyTester(BaseCase):
+    """
+    Tests for methods in the GateSetTomography class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_run(self):
+        raise NotImplementedError()  # TODO: test run
+
+
+class LinearGateSetTomographyTester(BaseCase):
+    """
+    Tests for methods in the LinearGateSetTomography class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_check_if_runnable(self):
+        raise NotImplementedError()  # TODO: test check_if_runnable
+
+    def test_run(self):
+        raise NotImplementedError()  # TODO: test run
+
+
+class StandardGSTTester(BaseCase):
+    """
+    Tests for methods in the StandardGST class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_run(self):
+        raise NotImplementedError()  # TODO: test run
+
+
+class ModelEstimateResultsTester(BaseCase):
+    """
+    Tests for methods in the ModelEstimateResults class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass  # TODO
+
+    @classmethod
+    def tearDownClass(cls):
+        pass  # TODO
+
+    def setUp(self):
+        pass  # TODO
+
+    def tearDown(self):
+        pass  # TODO
+
+    def test_from_dir(self):
+        raise NotImplementedError()  # TODO: test from_dir
+
+    def test_dataset(self):
+        raise NotImplementedError()  # TODO: test dataset
+
+    def test_as_nameddict(self):
+        raise NotImplementedError()  # TODO: test as_nameddict
+
+    def test_add_estimates(self):
+        raise NotImplementedError()  # TODO: test add_estimates
+
+    def test_rename_estimate(self):
+        raise NotImplementedError()  # TODO: test rename_estimate
+
+    def test_add_estimate(self):
+        raise NotImplementedError()  # TODO: test add_estimate
+
+    def test_add_model_test(self):
+        raise NotImplementedError()  # TODO: test add_model_test
+
+    def test_view(self):
+        raise NotImplementedError()  # TODO: test view
+
+    def test_copy(self):
+        raise NotImplementedError()  # TODO: test copy
