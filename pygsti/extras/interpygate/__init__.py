ff7f5980022647509f105d545a174b023ce1ea33
Kevin Young <kyoung@sandia.gov>
2020-08-26 20:50:21 -0700

    Added interpygate to extras
    Might not actually WORK yet

---
+++ pygsti/extras/interpygate/__init__.py
@@ -0,0 +1,11 @@
+""" Interpygate Sub-package """
+#***************************************************************************************************
+# Copyright 2015, 2019 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+# Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights
+# in this software.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 or in the LICENSE file in the root pyGSTi directory.
+#***************************************************************************************************
+
+from .core import PhysicalProcess
