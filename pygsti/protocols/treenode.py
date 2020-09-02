42824c29dbdcd0dea40fe6f10eacb638b8e6fd69
Erik Nielsen <enielse@sandia.gov>
2020-09-01 22:24:15 -0600

    Updates the way objects are loaded from protocol directories.
    In particular, when loading the child subdirectories of an object-type
    that has an associated 'meta' subdirectory (e.g. 'edesign' or 'data')
    and a child subdirectory is encoutered that does *not* contain the meta
    directory, that child is not created.
    
    For example, when a ProtocolData object is loaded from a root directory,
    and child subdirectories are uninitialized (have no data/ subdirectory),
    then child elements of the parent ProtocolData are not created at loading
    time.
    
    This is the desired behavior because, as is the case for ProtocolData,
    these children will be created on-demand, and properly initialized, etc,
    by the parent object.  Previously the default ProtocolData class type
    was used to create empty ProtocolData children and precluding them from
    proper on-demand initalization.  Falling back to the parent type is
    a fine behavior when we're missing a meta.json, but not when the entire
    meta subdirectory is absent -- this indicates the child needs to be constructed.
    
    is loaded from a directory that does *not* contain

--- pygsti/protocols/treenode.py
+++ pygsti/protocols/treenode.py
@@ -78,14 +78,20 @@ class TreeNode(object):
 
         for nm, subdir in child_dirs.items():
             subobj_dir = dirname / subdir
+            if not subobj_dir.exists():  # if there's no subdirectory, generate the child value
+                continue  # don't load anything - create child value on demand
+
             if meta_subdir:
                 submeta_dir = subobj_dir / meta_subdir
-                instance = _io.metadir._cls_from_meta_json(submeta_dir) \
-                    if submeta_dir.exists() and (submeta_dir / 'meta.json').exists() \
-                    else self.__class__  # can't find a meta.json - default to same class as self
+                if submeta_dir.exists():  # then load an object from this directory (even if it's meta.json is missing)
+                    # if we can't find a meta.json - default to same class as self
+                    classobj = _io.metadir._cls_from_meta_json(submeta_dir) \
+                        if (submeta_dir / 'meta.json').exists() else self.__class__
+                    self._vals[nm] = classobj.from_dir(subobj_dir, parent=self, name=nm, **kwargs)
+                # **If there's no subdirectory, don't load a value here - generate a child value if needed**
             else:
                 instance = self.__class__  # no meta.json - default to same class as self
-            self._vals[nm] = instance.from_dir(subobj_dir, parent=self, name=nm, **kwargs)
+                self._vals[nm] = instance.from_dir(subobj_dir, parent=self, name=nm, **kwargs)
 
     def keys(self):
         """
