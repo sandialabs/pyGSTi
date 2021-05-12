6ec55b408ff88946d434588e94a4f5405a13adad
Erik Nielsen <enielse@sandia.gov>
2021-05-12 13:13:00 -0600

    Adds an __init__.py to modelmembers/operations.
    So we can `import pygsti.modelmembers.operations as ops` and have
    easy access to all of the operation classes.

---
+++ pygsti/modelmembers/operations/__init__.py
@@ -0,0 +1,29 @@
+"""
+Sub-package holding model operation objects.
+"""
+#***************************************************************************************************
+# Copyright 2015, 2019 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+# Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights
+# in this software.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 or in the LICENSE file in the root pyGSTi directory.
+#***************************************************************************************************
+
+from .composederrorgen import ComposedErrorgen
+from .composedop import ComposedOp
+from .denseop import DenseOperator, DenseOperatorInterface
+from .depolarizeop import DepolarizeOp
+from .eigpdenseop import EigenvalueParamDenseOp
+from .embeddederrorgen import EmbeddedErrorgen
+from .embeddedop import EmbeddedOp
+from .experrorgenop import ExpErrorgenOp
+from .fulldenseop import FullDenseOp
+from .fullunitaryop import FullUnitaryOp
+from .lindbladerrorgen import LindbladErrorgen
+from .linearop import LinearOperator
+from .lpdenseop import LinearlyParamDenseOp
+from .staticdenseop import StaticDenseOp
+from .staticstdop import StaticStandardOp
+from .stochasticop import StochasticNoiseOp
+from .tpdenseop import TPDenseOp
