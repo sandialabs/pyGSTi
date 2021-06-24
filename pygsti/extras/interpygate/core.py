45da61c2652ede88c6e4fc62c7dd45e8d69f5d68
Kevin Young <kyoung@sandia.gov>
2021-06-23 20:36:50 -0700

    Make csaps the default

--- pygsti/extras/interpygate/core.py
+++ pygsti/extras/interpygate/core.py
@@ -21,8 +21,46 @@ from ...modelmembers.operations.opfactory import OpFactory as _OpFactory
 from ...baseobjs.verbosityprinter import VerbosityPrinter as _VerbosityPrinter
 from ...tools import optools as _ot
 
+try:
+    from csaps import NdGridCubicSmoothingSpline as _cubicSpline
+    use_csaps = True
+except ImportError:
+    use_csaps = False
+    import warnings
+    warnings.warn("Warning - Cannot import csaps module for spline interpolation. Interpolated gates will default to linear interpolation.")
 
-#TODO move elsewhere?
+if use_csaps:
+    class custom_interpolator():
+        # This class restructures the csaps.NdGridCubicSmoothingSpline API to be consistent 
+        # with that of scipy.interpolator.LinearNDInterpolator. 
+        def __init__(self, interpolator):
+            self.interpolator = interpolator
+        def __call__(self, *v):
+            output = self.interpolator(v)
+            return output[0]
+
+    def _cubicSplineMod(points, values, **kwargs):
+
+        grids = []
+        points = _np.array(points).T
+        values = _np.array(values)
+
+        n_variables = len(points)
+        
+        # Reformat everything to work with the CSAPS interpolator
+        meshgrids = points.reshape((n_variables,)+tuple(kwargs['shape']), order='C')
+        values = values.reshape(tuple(kwargs['shape']), order='C')
+        grid_points = []
+        for idm, mesh in enumerate(meshgrids):
+            # Progamatically construct slices like [0,0,:,0]
+            s = (0,)*(idm) + (slice(None),) + (0,)*(n_variables-idm-1)
+            grid_points += [mesh[s]]
+        
+        return custom_interpolator(_cubicSpline(grid_points, values, smooth=1))
+
+    # interpolator_and_args = (spline_interpolator, {'shape': (7,3,3)})
+
+
 def _split(n, a, cast_to_array=True):
     k, m = divmod(len(a), n)
     lst = list(a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
@@ -435,7 +473,9 @@ class InterpolatedQuantityFactory(object):
 
         interpolator_and_args : tuple, optional
             Optionally a 2-tuple of an interpolation class and argument dictionary.  If None, the
-            default of `(scipy.interpolate.LinearNDInterpolator, {'rescale': True})` is used.
+            default of `(csaps.NdGridCubicSmoothingSpline, {'shape', self.qty__shape})` is used. If
+            csaps is not available, then `(scipy.interpolate.LinearNDInterpolator, {'rescale': True})`
+            is used instead.
         """
         self.fn_to_interpolate = fn_to_interpolate
         assert(bool(parameter_ranges is not None) ^ bool(parameter_points is not None)), \
@@ -448,6 +488,7 @@ class InterpolatedQuantityFactory(object):
         self.points = None
         self.qty_shape = qty_shape
         self.interpolator_and_args = interpolator_and_args
+        self.grid_shape = None
 
     def compute_data(self, comm=None, mpi_workers_per_process=1, verbosity=0):
 
@@ -492,11 +533,15 @@ class InterpolatedQuantityFactory(object):
                     assert(isinstance(rng, _np.ndarray)), "Parameter ranges must be specified by tuples or arrays!"
                     axial_points.append(rng)
 
+            self.grid_shape = [len(points) for points in axial_points]
             points_to_distribute = _np.array(list(_itertools.product(*axial_points[0:iFirstGrouped])))
             grouped_axial_pts = axial_points[iFirstGrouped:]
             all_points = _np.array(list(_itertools.product(*axial_points)))
         else:
             assert(self._parameter_points is not None and self._num_params_to_evaluate_as_group == 0)
+            if use_csaps:
+                use_csaps = False
+                warnings.warn("Cubic spline interpolation requires rectangular grid. Defaulting to linear interpolator.")
             points_to_distribute = self._parameter_points
             grouped_axial_pts = []
             all_points = points_to_distribute
@@ -575,7 +620,10 @@ class InterpolatedQuantityFactory(object):
         my_interpolators = _np.empty(len(my_index_tuples), dtype=object)
 
         if self.interpolator_and_args is None:
-            interp_cls, interp_kwargs = (_linND, {'rescale': True})
+            if use_csaps:
+                interp_cls, interp_kwargs = (_cubicSplineMod, {'shape': self.grid_shape})
+            else:
+                interp_cls, interp_kwargs = (_linND, {'rescale': True})
         else:
             interp_cls, interp_kwargs = self.interpolator_and_args
 
---
+++ test/unit/extras/interpygate/test_construction.py
@@ -0,0 +1,23 @@
+from ...util import BaseCase
+
+class InterpygateConstructionTester(BaseCase):
+    @classmethod
+    def setUpClass(cls):
+        super(InterpygateConstructionTester, cls).setUpClass()
+        cls.target_model = stdC.target_model()
+        cls.depol_strength = 1e-3
+        cls.mdl = cls.target_model.depolarize(op_noise=cls.depol_strength)
+
+    def test_p_to_r_AGI(self):
+        self.skipTest("RB analysis is known to be broken.  Skip tests until it gets fixed.")
+        AGI = average_gate_infidelity(self.mdl.operations['Gc0'], self.target_model.operations['Gc0'])
+        r_AGI = analysis.p_to_r(1 - self.depol_strength, d=2, rtype='AGI')
+        # TODO assert correctness without comparing to optools AGI
+        self.assertAlmostEqual(AGI, r_AGI, places=10)
+
+    def test_p_to_r_EI(self):
+        self.skipTest("RB analysis is known to be broken.  Skip tests until it gets fixed.")
+        EI = entanglement_infidelity(self.mdl.operations['Gc0'], self.target_model.operations['Gc0'])
+        r_EI = analysis.p_to_r(1 - self.depol_strength, d=2, rtype='EI')
+        # TODO assert correctness without comparing to optools EI
+        self.assertAlmostEqual(EI, r_EI, places=10)
