b169f05229ca803176d6e42815194a7002e9c0f3
Kevin Young <kyoung@sandia.gov>
2021-06-29 13:49:37 -0700

    Minor updates to tests (still incomplete)

--- test/unit/extras/interpygate/test_construction.py
+++ test/unit/extras/interpygate/test_construction.py
@@ -1,6 +1,14 @@
+
+import numpy as np
+from scipy.interpolate import LinearNDInterpolator as _linND
+
+import pygsti
 import pygsti.extras.interpygate as interp
 from pygsti.tools.basistools import change_basis
-import numpy as np
+from pygsti.modelpacks import smq1Q_XY
+from pathlib import Path
+working_dir = Path.cwd()
+
 from ...util import BaseCase
 
 sigI = np.array([[1.,0],[0, 1]], dtype='complex')
@@ -28,29 +36,172 @@ class SingleQubitTargetOp(pygsti.modelmembers.operations.OpFactory):
         mx = self.process([*args])
         return pygsti.modelmembers.operations.StaticArbitraryOp(mx)
 
+class SingleQubitGate(interp.PhysicalProcess):
+    def __init__(self, 
+                 verbose=False,
+                 cont_param_gate = False,
+                 num_params = None,
+#                  process_shape = (4, 4),
+                 item_shape = (4,4),
+                 aux_shape = None,
+                 num_params_evaluated_as_group = 0,
+                 ):
 
+        self.verbose = verbose
+        self.cont_param_gate = cont_param_gate
+        self.num_params = num_params
+        self.item_shape = item_shape
+        self.aux_shape = aux_shape
+        self.num_params_evaluated_as_group = num_params_evaluated_as_group
 
+    def create_process_matrix(self, v, comm=None, return_generator=False):
+        processes = []
+        phi, theta, t = v
+        theta = theta * t
+        target_unitary = (np.cos(theta/2) * sigI + 
+                          1.j * np.sin(theta/2) * (np.cos(phi) * sigX + np.sin(phi) * sigY))
+        superop = change_basis(np.kron(target_unitary.conj(), target_unitary), 'col', 'pp')
+        processes += [superop]
+        return np.array(processes) if (processes is not None) else None
 
+    def create_aux_info(self, v, comm=None):
+        return []  # matches aux_shape=() above
+    
+    def create_process_matrices(self, v, grouped_v, comm=None):
+        assert(len(grouped_v) == 1)  # we expect a single "grouped" parameter
+        processes = []
+        times = grouped_v[0]
+        phi_in, theta_in = v
+        for t in times:
+            phi = phi_in
+            theta = theta_in * t
+            target_unitary = (np.cos(theta/2) * sigI + 
+                              1.j * np.sin(theta/2) * (np.cos(phi) * sigX + np.sin(phi) * sigY))
+            superop = change_basis(np.kron(target_unitary.conj(), target_unitary), 'col', 'pp')
+            processes += [superop]
+        return np.array(processes) if (processes is not None) else None
+
+    def create_aux_infos(self, v, grouped_v, comm=None):
+        import numpy as np
+        times = grouped_v[0]
+        return [ [] for t in times] # list elements must match aux_shape=() above
 
 
 class InterpygateConstructionTester(BaseCase):
     @classmethod
     def setUpClass(cls):
         super(InterpygateConstructionTester, cls).setUpClass()
+        cls.static_target = np.bmat([[np.eye(2),np.zeros([2,2])],
+                                     [np.zeros([2,2]),np.sqrt(2)/2*(sigI++1.j*sigY)]])
+        cls.target_op = SingleQubitTargetOp()
 
-        cls.target_model = stdC.target_model()
-        cls.depol_strength = 1e-3
-        cls.mdl = cls.target_model.depolarize(op_noise=cls.depol_strength)
+        cls.param_ranges = [(0.9,1.1,3)]
+        cls.arg_ranges = [2*np.pi*(1+np.cos(np.linspace(np.pi,0, 7)))/2,
+                      (0, np.pi, 3)] 
+        cls.arg_indices = [0,1]
 
-    def test_create_target(self):
-        target = np.bmat([[np.eye(2),np.zeros([2,2])],[np.zeros([2,2]),np.sqrt(2)/2*(sigI++1.j*sigY)]])
+        cls.gate_process = SingleQubitGate(num_params = 3,num_params_evaluated_as_group = 1)
+        
+        
+    def test_target(self):
+        test = self.target_op.create_target_gate([0,np.pi/4])
+        self.assertArraysAlmostEqual(test, self.static_target)
+        
+    def test_create_gate(self):
+        test = self.gate_process.create_process_matrices([0,np.pi/4], grouped_v=[[1]])[0]
+        self.assertArraysAlmostEqual(test, self.static_target)
+
+    def test_create_opfactory(self):
+        opfactory_linear = interp.InterpolatedOpFactory.create_by_interpolating_physical_process(
+                                self.target_op, self.gate_process, argument_ranges=self.arg_ranges, 
+                                parameter_ranges=self.param_ranges, argument_indices=self.arg_indices, 
+                                interpolator_and_args='linear')
+        op = opfactory_linear.create_op([0,np.pi/4])
+        op.from_vector([1])
+        self.assertArraysAlmostEqual(op, self.static_target)
+
+        opfactory_spline = interp.InterpolatedOpFactory.create_by_interpolating_physical_process(
+                                self.target_op, self.gate_process, argument_ranges=self.arg_ranges, 
+                                parameter_ranges=self.param_ranges, argument_indices=self.arg_indices, 
+                                interpolator_and_args='spline')
+        op = opfactory_spline.create_op([0,np.pi/4])
+        op.from_vector([1])
+        self.assertArraysAlmostEqual(op, self.static_target)
+
+        interpolator_and_args = (_linND, {'rescale': True})
+        opfactory_custom = opfactory_spline = interp.InterpolatedOpFactory.create_by_interpolating_physical_process(
+                                self.target_op, self.gate_process, argument_ranges=self.arg_ranges, 
+                                parameter_ranges=self.param_ranges, argument_indices=self.arg_indices, 
+                                interpolator_and_args=interpolator_and_args)
+        op = opfactory_custom.create_op([0,np.pi/4])
+        op.from_vector([1])
+        self.assertArraysAlmostEqual(op, self.static_target)
+
+
+
+class InterpygateGSTTester(BaseCase):
+    @classmethod
+    def setUpClass(cls):
+        super(InterpygateGSTTester, cls).setUpClass()
         target_op = SingleQubitTargetOp()
-        target_op.create_target_gate([0,np.pi/2])
-        self.assertAlmostEqual(AGI, r_AGI, places=10)
+        param_ranges = [(0.9,1.1,3)]
+        arg_ranges = [2*np.pi*(1+np.cos(np.linspace(np.pi,0, 7)))/2,
+                  (0, np.pi, 3)] 
+        arg_indices = [0,1]
+        gate_process = SingleQubitGate(num_params = 3,num_params_evaluated_as_group = 1)
+        opfactory = interp.InterpolatedOpFactory.create_by_interpolating_physical_process(
+                                target_op, gate_process, argument_ranges=arg_ranges, 
+                                parameter_ranges=param_ranges, argument_indices=arg_indices, 
+                                interpolator_and_args='linear')
+        x_gate = opfactory.create_op([0,np.pi/4])
+        y_gate = opfactory.create_op([np.pi/2,np.pi/4]) 
 
-    def test_p_to_r_EI(self):
-        self.skipTest("RB analysis is known to be broken.  Skip tests until it gets fixed.")
-        EI = entanglement_infidelity(self.mdl.operations['Gc0'], self.target_model.operations['Gc0'])
-        r_EI = analysis.p_to_r(1 - self.depol_strength, d=2, rtype='EI')
-        # TODO assert correctness without comparing to optools EI
-        self.assertAlmostEqual(EI, r_EI, places=10)
+        cls.model = pygsti.models.ExplicitOpModel([0],'pp')
+        cls.model['rho0'] = [ 1/np.sqrt(2), 0, 0, 1/np.sqrt(2) ] # density matrix [[1, 0], [0, 0]] in Pauli basis
+        cls.model['Mdefault'] = pygsti.modelmembers.povms.UnconstrainedPOVM(
+            {'0': [ 1/np.sqrt(2), 0, 0, 1/np.sqrt(2) ],   # projector onto [[1, 0], [0, 0]] in Pauli basis
+             '1': [ 1/np.sqrt(2), 0, 0, -1/np.sqrt(2) ] }, evotype="default") # projector onto [[0, 0], [0, 1]] in Pauli basis
+        cls.model['Gxpi2',0] = x_gate
+        cls.model['Gypi2',0] = y_gate
+
+        
+    def test_gpindices(self):
+        model = self.model.copy()
+        model['rho0'].set_gpindices(slice(0,4),model)
+        model['Mdefault'].set_gpindices(slice(4,12),model)
+        model['Gxpi2',0].set_gpindices(slice(12,13),model)
+        model['Gypi2',0].set_gpindices(slice(12,13),model)
+        model._rebuild_paramvec()
+        self.assertEqual(model.num_params,13)
+        
+    def test_circuit_probabilities(self):
+        datagen_model = self.model.copy()
+        datagen_params = datagen_model.to_vector()
+        datagen_params[-2:] = [1.1,1.1]
+        datagen_model.from_vector(datagen_params)
+        probs = datagen_model.probabilities( (('Gxpi2',0),))
+        self.assertEqual(probs['0'],0.8247240241650917)
+
+    def test_germ_selection(self):
+        datagen_model = self.model.copy()
+        datagen_params = datagen_model.to_vector()
+        datagen_params[-2:] = [1.1,1.1]
+        datagen_model.from_vector(datagen_params)
+        
+        target_model = self.model.copy()
+        
+        final_germs = pygsti.algorithms.germselection.find_germs(
+                self.model, randomize=False, force=None, algorithm='greedy', verbosity=4)
+
+        self.assertEqual(final_germs, [pygsti.circuits.circuit.Circuit('Gxpi2:0')])
+
+
+
+
+
+
+
+
+
+
+
