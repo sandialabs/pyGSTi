ada3887eb86f280286a50c78b0a9e571e0d72354
Erik Nielsen <enielse@sandia.gov>
2021-10-03 07:38:26 -0600

    Cleans up github actions for testing and deployment.
    I think we've finally got things figured out and configured to use the
    newer build system and PyPI deployment.  Here are some of the main
    take-aways from the many recent commits related to GitHub deployment actions:
    
    - pyGSTi now uses a pyproject.toml file, which (as far as I understand things)
      causes "pip install" to setup a separate build environment to build a wheel
      file which is then installed.  This means that we no longer need to (or should)
      install cython & numpy using pip before calling "pip install ." or similar, since
      this won't be installing the "build tools" anyway.  These commands have been
      removed from the github workflow .yml files.  Cython and numpy *are* needed in
      the [build-system] section of pyproject.toml in order to build pyGSTi's
      extensions.
    
    - PyGSTi's pyproject.toml contains cython and numpy, making the default
      type of installation one that tries to compile the cython extensions.
      If, however, a platform doesn't/can't have cython, this file can be
      manually replaced with pyproject.toml.no_cython (also in the repo)
      and then installed using "pip install ." or similar.  This will cause
      the build environment to lack Cython, which setup.py will detect and
      respond to by simply not building any extensions.
    
    - in GitHub actions, in order to have pyGSTi know what version it is
      we need to specify "fetch-depth: 0" to the checkout@v2 action (so
      tags are pulled).  For tests we don't need this information, but for
      deployment we do.  (setuptools_scm now creates a static _version.py
      file upon installation based on the git tags.)

--- .github/ci-scripts/install-wheelbuilding-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#These need to be run prior to building wheels using pip
-
-python -m pip install --upgrade pip
-# see https://github.com/cvxgrp/cvxpy/issues/968 for numpy version
-python -m pip install "numpy>=1.16.0"
-# Cython must be pre-installed to build native extensions on pyGSTi install
-python -m pip install cython
-python -m pip install wheel flake8
