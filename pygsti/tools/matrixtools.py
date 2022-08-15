929c10fa3a4aba4287d22bbd0fb5f438988f879e
Erik Nielsen <enielse@sandia.gov>
2022-08-15 18:14:28 -0400

    Adds catching of more generic ArpackError to svd exception processing in matrixtools.independent_columns
    In a very recent commit, we updated the caught ArpackNoConvergence exception's location
    within scipy, thinking that this was a simple name update.  It seems like scipy.sparse's
    SVD is now failing with the more generic ArpackError exception when previously it failed
    with ArpackNoConvergence, so we're updating the exception block to catch both types of
    exceptions now.  I *think* this is all fine and there's no issue here, but we should keep an
    eye on this as the new exception handling may also mask other more important SVD failures.

--- pygsti/tools/matrixtools.py
+++ pygsti/tools/matrixtools.py
@@ -457,6 +457,7 @@ def independent_columns(m, initial_independent_cols=None, tol=1e-7):
     else:  # sparse case
 
         from scipy.sparse.linalg import ArpackNoConvergence as _ArpackNoConvergence
+        from scipy.sparse.linalg import ArpackError as _ArpackError
         running_indep_cols = initial_independent_cols.copy() \
             if (initial_independent_cols is not None) else _sps.csc_matrix((m.shape[0], 0), dtype=m.dtype)
         num_indep_cols = running_indep_cols.shape[0]
@@ -466,7 +467,7 @@ def independent_columns(m, initial_independent_cols=None, tol=1e-7):
 
             try:
                 lowest_sval = _spsl.svds(trial, k=1, which="SM", return_singular_vectors=False)
-            except _ArpackNoConvergence:
+            except (_ArpackNoConvergence, _ArpackError):
                 lowest_sval = 0  # assume lack of convergence means smallest singular value was too small (?)
 
             if lowest_sval > tol:  # trial fogi dirs still linearly independent (full rank)
