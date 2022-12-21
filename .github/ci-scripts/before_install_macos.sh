e10659c574f0e7a3fb160e4c533891c397982f86
Corey Ostrove <ciostro@sandia.gov>
2022-12-20 18:08:23 -0700

    Remove sudo invocation for brew calls
    I got an error message trying to use brew with sudo, so drop that and see if it works.

--- .github/ci-scripts/before_install_macos.sh
+++ .github/ci-scripts/before_install_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo brew update && \
-sudo brew install \
+brew update && \
+brew install \
 gfortran libblas-dev liblapack-dev openmpi-bin openmpi-common openssh-client \
 openssh-server libopenmpi3 libopenmpi-dev libsuitesparse-dev
 cmake --version
