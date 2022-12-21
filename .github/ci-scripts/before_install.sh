8659ffdc10895b540d171e0d5329c832bf62dbe7
Corey Ostrove <ciostro@sandia.gov>
2022-12-20 17:00:10 -0700

    Fix dependency issue with mpi
    libopenmpi2 has been deprecated in favor of libopenmpi3 in ubuntu 18.10+.

--- .github/ci-scripts/before_install.sh
+++ .github/ci-scripts/before_install.sh
@@ -3,6 +3,6 @@
 sudo apt-get update -qq -y && \
 sudo apt-get install -qq -y \
 gfortran libblas-dev liblapack-dev openmpi-bin openmpi-common openssh-client \
-openssh-server libopenmpi2 libopenmpi-dev libsuitesparse-dev
+openssh-server libopenmpi3 libopenmpi-dev libsuitesparse-dev
 cmake --version
 gcc --version
