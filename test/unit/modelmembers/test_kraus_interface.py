c4844d0dd568827496cb11d9807ab4cf0d5cebed
Corey Ostrove <ciostro@sandia.gov>
2023-01-17 15:43:15 -0700

    Debug pytest environment

--- test/unit/modelmembers/test_kraus_interface.py
+++ test/unit/modelmembers/test_kraus_interface.py
@@ -5,7 +5,7 @@ import numpy as np
 try:
     import pygsti.evotypes.densitymx.statereps
 except ModuleNotFoundError:
-    print('importing pygsti.evotypes.densitymx.statereps failed. ModuleNotFoundError raised.') 
+    print('importing pygsti.evotypes.densitymx.statereps failed. ModuleNotFoundError raised.')
 from pygsti.modelpacks import smq1Q_XYI
 from pygsti.baseobjs import QubitSpace, Basis
 from pygsti.modelmembers.operations import StochasticNoiseOp
