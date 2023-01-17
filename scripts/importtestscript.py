def0d7302a74e00c863a151cf36129d4bde8df4d
Corey Ostrove <ciostro@sandia.gov>
2023-01-17 15:05:17 -0700

    Debug pytest environment

--- scripts/importtestscript.py
+++ scripts/importtestscript.py
@@ -3,6 +3,12 @@ print(pygsti.__path__)
 print(pygsti.__version__)
 
 try:
+    _importlib.import_module(pygsti.evotypes.densitymx.statereps)
+except ModuleNotFoundError:
+    print('importing pygsti.evotypes.densitymx.statereps failed using import lib. ModuleNotFoundError raised.') 
+
+try:
     import pygsti.evotypes.densitymx.statereps
 except ModuleNotFoundError:
     print('importing pygsti.evotypes.densitymx.statereps failed. ModuleNotFoundError raised.') 
+    
