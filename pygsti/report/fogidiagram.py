e715a078274730dc5bb73aed0db61c00f86030f6
Corey Ostrove <ciostro@sandia.gov>
2023-04-20 15:59:01 -0600

    jQuery security update
    There is apparently a security vulnerability in older versions of jQuery. This probably doesn't really matter for our purposes, but it is simple enough to point to a newer version with the appropriate patches, if for nothing else to quiet github's warnings.

--- pygsti/report/fogidiagram.py
+++ pygsti/report/fogidiagram.py
@@ -1201,12 +1201,12 @@ _template = """<html>
 <head>
     <!-- <link rel="stylesheet" href="pygsti_dataviz.css"> -->
     <!-- <script type="text/javascript" src="vis-network.js"></script> -->
-    <!-- <script type="text/javascript" src="jquery-3.2.1.min.js"></script> -->
+    <!-- <script type="text/javascript" src="jquery-3.6.4.min.js"></script> -->
     <script type="text/javascript" src="vis-network.js"></script>
-    <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="jquery-3.6.4.min.js"></script>
     <!-- <script type="text/javascript"
                  src="https://unpkg.com/vis-network/standalone/umd/vis-network.min.js"></script> -->
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"
                  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
 
     <style type="text/css">
