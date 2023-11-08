7ffc78f7c8869f03e90603b755726d16c8431c9a
jordanh6 <jordanh@berkeley.edu>
2023-11-07 16:05:54 -0800

    changed code to simulate non-editable circuit

--- pygsti/algorithms/randomcircuit.py
+++ pygsti/algorithms/randomcircuit.py
@@ -3187,8 +3187,8 @@ def create_udrb_circuit(pspec, length, qubit_labels = None, layer_type='cz-zxzxz
                                    one_q_gate_type='haar',
                                    two_q_gate_args_lists={'Gczr': [(str(a),) for a in angles]})
     #consider adding in a check for continuous-arg gates
-    #elif layer_type=='standard':                                   
-    #    circuit2 = create_random_circuit(pspec, length, qubit_labels=qubit_labels, sampler='edgegrab', samplerargs=[twoQ_gate_density])                                  
+    elif layer_type=='standard':                                   
+        circuit2 = create_random_circuit(pspec, length, qubit_labels=qubit_labels, sampler='edgegrab', samplerargs=[twoQ_gate_density])                                  
                                    
     #for a in range(length):
     #   #generate random 1q unitary layer
@@ -3209,9 +3209,9 @@ def create_udrb_circuit(pspec, length, qubit_labels = None, layer_type='cz-zxzxz
     circuit = circuit2.copy(editable=True)
     circuit.delete_idle_layers_inplace()
 
-    print(circuit)
+    print(circuit.str)
 
-    circ_unitary = model.sim.product(circuit)
+    circ_unitary = model.sim.product(circuit2)
     
     drb_circ.append_circuit_inplace(circuit)
     
