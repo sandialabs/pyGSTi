19b6404972042fd723b7d4e5639bdfb0e76c6590
Kyle Skolfield <jkskolf@sandia.gov>
2023-11-30 14:04:02 -0700

    condensed testing

---
+++ pygsti/extras/idletomography/report_test.py
@@ -0,0 +1,93 @@
+import numpy as np
+from pygsti.baseobjs import Basis
+
+np.set_printoptions(precision=1, linewidth=1000)
+
+import pygsti
+from pygsti.extras import idletomography as idt
+
+from pygsti.extras.idletomography.pauliobjs import NQPauliState
+
+huh = [
+    (NQPauliState("X", (1,)), NQPauliState("X", (1,))),
+    (NQPauliState("X", (1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("X", (1,)), NQPauliState("Z", (1,))),
+    (NQPauliState("Y", (1,)), NQPauliState("X", (1,))),
+    (NQPauliState("Y", (1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("Y", (1,)), NQPauliState("Z", (1,))),
+    (NQPauliState("Z", (1,)), NQPauliState("X", (1,))),
+    (NQPauliState("Z", (1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("Z", (1,)), NQPauliState("Z", (1,))),
+    (NQPauliState("X", (-1,)), NQPauliState("X", (1,))),
+    (NQPauliState("X", (-1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("X", (-1,)), NQPauliState("Z", (1,))),
+    (NQPauliState("Y", (-1,)), NQPauliState("X", (1,))),
+    (NQPauliState("Y", (-1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("Y", (-1,)), NQPauliState("Z", (1,))),
+    (NQPauliState("Z", (-1,)), NQPauliState("X", (1,))),
+    (NQPauliState("Z", (-1,)), NQPauliState("Y", (1,))),
+    (NQPauliState("Z", (-1,)), NQPauliState("Z", (1,))),
+]
+
+n_qubits = 1
+gates = ["Gi", "Gx", "Gy", "Gcnot"]
+max_lengths = [1, 2, 4, 8]
+
+pspec = pygsti.processors.QubitProcessorSpec(
+    n_qubits, gates, geometry="line", nonstd_gate_unitaries={(): 1}
+)
+mdl_target = pygsti.models.create_crosstalk_free_model(pspec)
+paulidicts = idt.determine_paulidicts(mdl_target)
+
+idle_experiments = idt.make_idle_tomography_list(
+    n_qubits,
+    max_lengths,
+    paulidicts,
+    maxweight=1,
+    force_fid_pairs=huh,
+)
+
+print(len(idle_experiments), "idle tomography experiments for %d qubits" % n_qubits)
+from pygsti.baseobjs import Label
+
+updated_ckt_list = []
+for ckt in idle_experiments:
+    new_ckt = ckt.copy(editable=True)
+    for i, lbl in enumerate(ckt):
+        if lbl == Label(()):
+            # new_ckt[i] = [Label(("Gi", i)) for i in range(n_qubits)]
+            new_ckt[i] = Label(("Gi", 0))
+    updated_ckt_list.append(new_ckt)
+
+
+
+mdl_datagen = pygsti.models.create_crosstalk_free_model(
+    pspec, lindblad_error_coeffs={"Gi": {"SX": 0.01}}
+)
+# Error models! Random with right CP constraints from Taxonomy paper
+ds = pygsti.data.simulate_data(
+    mdl_datagen, updated_ckt_list, 100000, seed=8675309, sample_error="none"
+)
+
+results = idt.do_idle_tomography(
+    n_qubits,
+    ds,
+    max_lengths,
+    paulidicts,
+    maxweight=1,
+    advanced_options={"jacobian mode": "together", "pauli_fidpairs": huh},
+    idle_string="Gi:0",
+)
+
+
+print(results.observed_rate_infos)
+print(results.intrinsic_rates)
+
+idt.create_idletomography_report(
+    results, "../IDTTestReport", "Test idle tomography example report", auto_open=True
+)
+
+results.error_list
+# ws = pygsti.report.Workspace()
+# ws.init_notebook_mode(autodisplay=True)
+print(results)
