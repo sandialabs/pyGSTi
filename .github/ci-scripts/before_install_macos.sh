c0bb80e4bfaf89d1a9e4083dff421c44d469ad32
Stefan K. Seritan <sserita@sandia.gov>
2024-01-16 13:39:11 -0800

    Attempt to fix macos 11/Python 3.11 cvxopt runner install

--- .github/ci-scripts/before_install_macos.sh
+++ .github/ci-scripts/before_install_macos.sh
@@ -6,3 +6,11 @@ gfortran openblas lapack openmpi \
 openssh suite-sparse
 cmake --version
 gcc --version
+
+# Get the SuiteSparse source to allow compiling cvxopt when wheel is not available
+# Not sure why brew install is not working for macos-11/Python 3.11, but it isn't
+git clone https://github.com/DrTimothyAldenDavis/SuiteSparse.git
+pushd SuiteSparse
+git checkout v7.5.1
+popd
+export CVXOPT_SUITESPARSE_SRC_DIR=$(pwd)/SuiteSparse
