8af46a125365c5b7c369cc0f82364041b1d0e9b6
Corey Ostrove <ciostro@sandia.gov>
2024-02-20 23:31:43 -0700

    Minor unit test fix
    Minor fix to a unit test to account for changes to qutrit construction code.

--- test/unit/construction/test_qutrit.py
+++ test/unit/construction/test_qutrit.py
@@ -11,7 +11,7 @@ class QutritConstructionTester(BaseCase):
     def test_noisy_qutrit(self):
         mdl_sim = qutrit.create_qutrit_model(error_scale=0.1, similarity=True, seed=1234, basis='qt')
         mdl_ideal = qutrit.create_qutrit_model(error_scale=0.1, similarity=True, seed=1234, basis='qt')
-        self.assertArraysAlmostEqual(mdl_sim['Gi'], mdl_ideal['Gi'])
+        self.assertArraysAlmostEqual(mdl_sim['Gi', 'QT'], mdl_ideal['Gi', 'QT'])
 
         #just test building a gate in the qutrit basis
         # Can't do this b/c need a 'T*' triplet space designator for "triplet space" and it doesn't seem
