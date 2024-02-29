d3b5fe96839fcb8176aec218a9c2685fd16966f1
Ashe Miller <nrmille@sandia.gov>
2024-02-29 15:49:24 -0700

    Added Propagation Code for EOC Error Generators

---
+++ pygsti/propErrorGens/ErrorPropagator.py
@@ -0,0 +1,228 @@
+import stim
+from pygsti.propErrorGens.propagatableerrorgen import *
+from pygsti.propErrorGens.pyGSTiStimTranslator import *
+from numpy import abs
+from numpy.linalg import multi_dot
+from scipy.linalg import expm
+
+
+'''
+takes a pygsti circuit where each gate has a defined error model and returns the errorgenerators necessary to create an
+end of circuit error generator under a variety of scenarios
+
+circ: pygsti circuit
+errorModel: Dictionary defined the small markovian error generators and their rates for each gate
+BCHOrder: in cases where the BCH approximation is used, carries it out to the desired order (can currently only handle order 1 or 2)
+BCHLayerWise: If true will push the errors through one layer of gatesand then combines them using the bch approximation at each layer
+If false will simply push all errors to the end
+NonMarkovian: Pushes the error generators to the end and then formats them to work with the cumulant expansion code
+MultiGateDict: Containts the translation between a numbered gate Gxpi22 and the PyGSTi standard gate used when a singular gate has
+multiple error iterations
+MultiGate: lets the code know 
+returns: list of propagatableerrorgens
+'''
+def ErrorPropagator(circ,errorModel,MultiGateDict={},BCHOrder=1,BCHLayerwise=False,NonMarkovian=False,MultiGate=False):
+    qubits=len(circ.line_labels)
+    stim_layers=[]
+    for j in range(circ.depth):
+        layer = circ.layer(j)
+        stim_layer=pyGSTiLayer_to_stimLayer(layer,qubits,MultiGateDict,MultiGate)
+        stim_layers.append(stim_layer)
+    stim_layers.pop(0)  #Immeditielty toss the first layer because it is not important,
+
+    propagation_layers=[]
+    if not BCHLayerwise or NonMarkovian:
+        while len(stim_layers) != 0:
+            top_layer=stim_layers.pop(0)
+            for layer in stim_layers:
+                top_layer = layer*top_layer
+            propagation_layers.append(top_layer)
+    else:
+        propagation_layers = stim_layers
+
+    errorLayers=buildErrorlayers(circ,errorModel,qubits)
+
+    num_error_layers=len(errorLayers)
+    fully_propagated_layers=[]
+    for _ in range(0,num_error_layers-1):
+        err_layer=errorLayers.pop(0)
+        layer=propagation_layers.pop(0)
+        for err_order in err_layer:
+            for errorGenerator in err_order:
+                errorGenerator.propagate_error_gen_inplace_tableau(layer)
+        if BCHLayerwise and not NonMarkovian:
+            following_layer = errorLayers.pop(0)
+            new_errors=BCH_Handler(err_layer,following_layer,BCHOrder)
+            errorLayers.insert(new_errors,0)
+        else:
+            fully_propagated_layers.append(err_layer)
+
+    fully_propagated_layers.append(errorLayers.pop(0))
+    if BCHLayerwise and not NonMarkovian:
+        for order in errorLayers:
+            for error in order:
+                if len(fully_propagated_layers)==0:
+                    fully_propagated_layers.append(error)
+                elif error in fully_propagated_layers:
+                    idy=fully_propagated_layers.index(error)
+                    new_error=error+fully_propagated_layers[idy]
+                    fully_propagated_layers.pop(idy)
+                    fully_propagated_layers.append(new_error)
+                else:
+                    fully_propagated_layers.append(error)
+        return fully_propagated_layers
+        
+    elif not BCHLayerwise and not NonMarkovian:
+        simplified_EOC_errors=[]
+        if BCHOrder == 1:
+            for layer in fully_propagated_layers:
+                for order in layer:
+                    for error in order:
+                        if len(simplified_EOC_errors)==0:
+                            simplified_EOC_errors.append(error)
+                        elif error in simplified_EOC_errors:
+                            idy=simplified_EOC_errors.index(error)
+                            new_error=error+simplified_EOC_errors[idy]
+                            simplified_EOC_errors.pop(idy)
+                            if not (abs(new_error.get_Error_Rate()) <.000001):
+                                simplified_EOC_errors.append(new_error)
+                        else:
+                            if not (abs(error.get_Error_Rate())<.000001):
+                                simplified_EOC_errors.append(error)
+        else:
+            Exception("Higher propagated through Errors are not Implemented Yet")
+        return simplified_EOC_errors
+    
+    else:
+        return fully_propagated_layers
+
+
+'''
+takes two error layers (list of propagatableerrorgens) and find the bch combination of the two
+err_layer: list lists of propagatableerrorgens
+following_layer: list of propagatableerrorgens
+BCHOrder: Order to carry the bch expansion out to, can currently be set to one or two
+returns list of lists of propagatableerrorgens. The outer list contains each of them individual list denote order
+'''
+def BCH_Handler(err_layer,following_layer,BCHOrder):         
+    new_errors=[]
+    for curr_order in range(0,BCHOrder):
+        working_order=[]
+        if curr_order == 0:
+            used_indexes=[]
+            for error in err_layer[curr_order]:
+                try:
+                    idy=following_layer[curr_order].index(error)
+                    working_order.append(error+following_layer[curr_order][idy])
+                    used_indexes.append(idy)
+                except:
+                    working_order.append(error)
+            for idy,error in enumerate(following_layer[curr_order]):
+                if idy in used_indexes:
+                    continue
+                else:
+                    working_order.append(error)
+            
+            new_errors.append(working_order)
+        elif curr_order ==1:
+            working_order=[]
+            for error1 in err_layer[curr_order-1]:
+                for error2 in following_layer[curr_order-1]:
+                    errorlist = commute_errors(error1,error2,BCHweight=1/2)
+                    for error3 in errorlist:
+                        if len(working_order)==0:
+                            working_order.append(error3)
+                        elif error3 in working_order:
+                            idy=working_order.index(error3)
+                            new_error=error3+working_order[idy]
+                            working_order.pop(idy)
+                            working_order.append(new_error)
+                        else:
+                            working_order.append(error3)
+            if len(err_layer)==2:
+                for error3 in err_layer[1]:
+                    if len(working_order)==0:
+                        working_order.append(error3)
+                    elif error3 in working_order:
+                        idy=working_order.index(error3)
+                        new_error=error3+working_order[idy]
+                        working_order.pop(idy)
+                        working_order.append(new_error)
+                    else:
+                        working_order.append(error3)
+            if len(following_layer)==2:
+                for error3 in following_layer[1]:
+                    if len(working_order)==0:
+                        working_order.append(error3)
+                    elif error3 in working_order:
+                        idy=working_order.index(error3)
+                        new_error=error3+working_order[idy]
+                        working_order.pop(idy)
+                        if new_error.get_Error_Rate() != 0j:
+                            working_order.append(new_error)
+                    else:
+                        working_order.append(error3)
+            new_errors.append(working_order)
+
+        else:
+            Exception("Higher Orders are not Implemented Yet")
+
+
+'''
+takes a pygst circuit object and error Dictionary and creates error layers
+
+inputs
+circ: pygsti circuit
+errorDict: Dictionary defined the small markovian error generators and their rates for each gate
+qubits: number of qubits in the circuit
+
+output
+ErrorGens, a list of error gen layers (which are list of propagatable errorgens)
+
+'''
+def buildErrorlayers(circ,errorDict,qubits):
+    ErrorGens=[]
+    #For the jth layer of each circuit
+    for j in range(circ.depth):
+        l = circ.layer(j) # get the layer
+        errorLayer=[]
+        for _, g in enumerate(l): # for gate in layer l
+            gErrorDict = errorDict[g.name] #get the errors for the gate
+            p1=qubits*'I' # make some paulis why?
+            p2=qubits*'I'
+            for errs in gErrorDict: #for an error in the accompanying error dictionary 
+                errType=errs[0]
+                paulis=[]
+                for ind,el in enumerate(g): #enumerate the gate ind =0 is name ind = 1 is first qubit ind = 2 is second qubit
+                    if ind !=0:  #if the gate element of concern is not the name
+                        p1=p1[:el] + errs[1][ind-1] +p1[(el+1):]
+                
+                paulis.append(p1)
+                if errType in "CA":
+                    for ind,el in enumerate(g):
+                        if ind !=0:
+                            p2=p2[:el] + errs[2][ind-1] +p2[(el+1):]
+                    paulis.append(p2)     
+                errorLayer.append(propagatableerrorgen(errType,paulis,gErrorDict[errs]))
+        ErrorGens.append([errorLayer])
+    return ErrorGens
+
+
+
+# There's a factor of a half missing in here. 
+def nm_propagators(corr, Elist):
+    Kms = []
+    for idm in range(len(Elist)):
+        Am = Elist[idm].toWeightedErrorBasisMatrix
+        # This assumes that Elist is in reverse chronological order
+        partials = []
+        for idn in range(idm, len(Elist)):
+            An = Elist[idn].toWeightedErrorBasisMatrix()
+            partials += [corr[idm,idn] * Am @ An]
+        partials[0] = partials[0]/2
+        Kms += [sum(partials,0)]
+    return Kms
+
+def averaged_evolution(corr, Elist):
+    Kms = nm_propagators(corr, Elist)
+    return multi_dot([expm(Km) for Km in Kms])
---
+++ pygsti/propErrorGens/propagatableerrorgen.py
@@ -0,0 +1,361 @@
+from pygsti.baseobjs.errorgenlabel import ElementaryErrorgenLabel
+from pygsti.propErrorGens.pyGSTiStimTranslator import *
+import stim
+from numpy import array,kron
+from pygsti.tools import change_basis
+from pygsti.tools.lindbladtools import create_elementary_errorgen
+'''
+Similar to errorgenlabel but has an errorrate included as well as additional classes
+'''
+class propagatableerrorgen(ElementaryErrorgenLabel):
+    '''
+    Labels an elementary errorgen by a type, pauli and error rate
+    '''
+
+    @classmethod
+    def cast(cls, obj, sslbls=None, identity_label='I'):
+        raise NotImplementedError("TODO: Implement casts for this method")
+
+
+    '''
+    Initiates the errorgen object
+    Inputs
+    errorgen_type: charecture can be set to 'H' Hamiltonian, 'S' Stochastic, 'C' Correlated or 'A' active following the conventions
+    of the taxonomy of small markovian errorgens paper
+
+    Outputs:
+    propagatableerrorgen object
+    '''
+    def __init__(self,errorgen_type,basis_element_labels,error_rate):
+        self.errorgen_type=str(errorgen_type)
+        self.basis_element_labels=tuple(basis_element_labels)
+        self.error_rate=error_rate
+
+    '''
+    hashes the error gen object
+    '''
+    def __hash__(self):
+        return hash((self.errorgen_type,self.basis_element_labels))
+
+    '''
+    checks and if two error gens have the same type and labels
+    '''
+    def __eq__(self, other):
+        return (self.errorgen_type == other.errorgen_type
+                and self.basis_element_labels == other.basis_element_labels)
+    
+    '''
+    displays the errorgens as strings
+    '''
+    def __str__(self):
+        return self.errorgen_type + "(" + ",".join(map(str, self.basis_element_labels)) + ")" + ": " + self.error_rate
+    
+
+    def __repr__(self):
+        return str((self.errorgen_type, self.basis_element_labels, self.error_rate))
+    
+    '''
+    adds the error rates together oftwo error generators of the same type and label
+    '''
+    def __add__(self,other):
+        if self.errorgen_type == other.errorgen_type and self.basis_element_labels == other.basis_element_labels:
+            return propagatableerrorgen(self.errorgen_type,self.basis_element_labels,self.error_rate + other.error_rate)
+        else:
+            raise Exception("ErrorGens are not equal")
+
+    '''
+    returns the dictionary representation of the error generator inline with pygsti notation
+    '''    
+    def to_dict(self):
+        return {self: self.error_rate}
+    
+
+    '''
+    returns the error rate
+    '''
+    def get_Error_Rate(self):
+        return self.error_rate
+    
+    '''
+    returns the string representation of the first pauli label
+    '''
+    def getP1(self):
+        return self.basis_element_labels[0]
+    
+    '''
+    returns the string representation of the second pauli label
+    '''
+    def getP2(self):
+        return self.basis_element_labels[1]
+
+    '''
+    propagates a propagatableerrorgen object through a clifford layer, returns the created error gen
+    '''
+    def propagate_error_gen_inplace(self, player):
+        slayer = pyGSTiLayer_to_stimLayer(player)
+        new_basis_labels = []
+        weightmod = 1
+        for pauli in self.basis_element_labels:
+            temp=pyGSTiPauli_2_stimPauli(pauli)
+            temp = slayer(temp)
+            weightmod=weightmod*temp.sign
+            new_basis_labels.append(stimPauli_2_pyGSTiPauli(temp))
+
+        if self.errorgen_type in 'HCA':
+            self.error_rate=self.error_rate*weightmod
+        self.basis_element_labels =tuple(new_basis_labels)
+
+    '''
+    using stim propagates the associated pauli labels through a stim tableu object, the object is modified inplace
+    '''
+    def propagate_error_gen_inplace_tableau(self, slayer):
+        new_basis_labels = []
+        weightmod = 1
+        for pauli in self.basis_element_labels:
+            temp=pyGSTiPauli_2_stimPauli(pauli)
+            temp = slayer(temp)
+            weightmod=weightmod*temp.sign
+            new_basis_labels.append(stimPauli_2_pyGSTiPauli(temp))
+
+        if self.errorgen_type in 'HCA':
+            self.error_rate=self.error_rate*weightmod
+        self.basis_element_labels =tuple(new_basis_labels)
+
+    '''
+    returns the strings representing the pauli labels in the pygsti representation of paulis as stim PauliStrings
+    '''
+    def returnStimPaulis(self):
+        paulis_string=[]
+        for pauli in self.basis_element_labels:
+            paulis_string.append(stim.PauliString(pauli))
+        return tuple(paulis_string)
+
+    '''
+    Returns the errorbasis matrix for the associated errorgenerator mulitplied by its error rate
+
+    input: A pygsti defined matrix basis by default can be pauli-product, gellmann 'gm' or then pygsti standard basis 'std'
+    functions defaults to pauli product if not specified
+    '''
+    def toWeightedErrorBasisMatrix(self,matrix_basis='pp'):
+        PauliDict={
+            'I' : array([[1.0,0.0],[0.0,1.0]]),
+            'X' : array([[0.0j, 1.0+0.0j], [1.0+0.0j, 0.0j]]),
+            'Y' : array([[0.0, -1.0j], [1.0j, 0.0]]),
+            'Z' : array([[1.0, 0.0j], [0.0j, -1.0]])
+        }
+        paulis=[]
+        for paulistring in self.basis_element_labels:
+            for idx,pauli in enumerate(paulistring):
+                if idx == 0:
+                    pauliMat = PauliDict[pauli]
+                else:
+                    pauliMat=kron(pauliMat,PauliDict[pauli])
+            paulis.append(pauliMat)
+        if self.errorgen_type in 'HS':
+            return self.error_rate*change_basis(create_elementary_errorgen(self.errorgen_type,paulis[0]),'std',matrix_basis)
+        else:
+            return self.error_rate*change_basis(create_elementary_errorgen(self.errorgen_type,paulis[0],paulis[1]),'std',matrix_basis)     
+
+
+
+
+
+'''
+Returns the Commutator of two errors
+'''
+def commute_errors(ErG1,ErG2, weightFlip=1.0, BCHweight=1.0):
+    def com(p1,p2):
+        P1 = pyGSTiPauli_2_stimPauli(p1)
+        P2=pyGSTiPauli_2_stimPauli(p2)
+        P3=P1*P2-P2*P1
+        return (P3.weight,stimPauli_2_pyGSTiPauli(P3))
+    
+    def acom(p1,p2):
+        P1 = pyGSTiPauli_2_stimPauli(p1)
+        P2=pyGSTiPauli_2_stimPauli(p2)
+        P3=P1*P2+P2*P1
+        return (P3.weight,stimPauli_2_pyGSTiPauli(P3))
+    
+    def labelMultiply(p1,p2):
+        P1 = pyGSTiPauli_2_stimPauli(p1)
+        P2=pyGSTiPauli_2_stimPauli(p2)
+        P3=P1*P2
+        return (P3.weight,stimPauli_2_pyGSTiPauli(P3))
+    
+    errorGens=[]
+    
+    wT=ErG1.getWeight()*ErG2.getWeight()*weightFlip*BCHweight
+    
+    if ErG1.getType()=='H' and ErG2.getType()=='H':
+        pVec=com(ErG1.getP1() , ErG2.getP2())
+        errorGens.append( propagatableerrorgen( 'H' , [pVec[1]] , -1j*wT *pVec[0] ) )
+        
+    elif ErG1.getType()=='H' and ErG2.getType()=='S':
+        pVec=com(ErG2.getP1() , ErG1.getP1())
+        errorGens.append( propagatableerrorgen( 'C' , [ErG2.getP1() , pVec[1]] , 1j*wT*pVec[0] ) )
+         
+    elif ErG1.getType()=='S' and ErG2.getType()=='H':
+        pVec=com(ErG2.getP1() , ErG1.getP1())
+        errorGens.append( propagatableerrorgen( 'C' , [ErG2.getP1() , pVec[1]] , -1j*wT *pVec[0] ) )
+          
+    elif ErG1.getType()=='H' and ErG2.getType()=='C':
+        pVec1=com(ErG2.getP1() , ErG1.getP1())
+        errorGens.append( propagatableerrorgen('C' , [pVec1[1], ErG2.getP2()] , 1j*wT*pVec1[0] ) )
+        pVec2=com(ErG2.getP2() , ErG1.getP1())
+        errorGens.append( propagatableerrorgen('C' , [pVec2[1] , ErG2.getP1()] , 1j*wT*pVec2[0] ) )
+                          
+    elif ErG1.getType()=='C' and ErG2.getType()=='H':
+        errorGens = commute_errors(ErG2,ErG1,weightFlip=-1.0,BCHweight=BCHweight)
+    
+    elif ErG1.getType()=='H' and ErG2.getType()=='A':
+        pVec1 = com(ErG1.getP1() , ErG2.getP1())
+        errorGens.append( propagatableerrorgen('A'  , [pVec1[1] , ErG2.getP2()] , -1j*wT*pVec1[0]) )
+        pVec2 = com(ErG1.getP1() , ErG2.getP2())
+        errorGens.append( propagatableerrorgen('A'  , [ErG2.getP1(), pVec2[1]] , -1j*wT*pVec2[0] ) )
+                          
+    elif ErG1.getType()=='A' and ErG2.getType()=='H':
+        errorGens = commute_errors(ErG2,ErG1,weightFlip=-1.0,BCHweight=BCHweight)
+
+    elif ErG1.getType()=='S' and ErG2.getType()=='S':
+        errorGens.append( propagatableerrorgen('H', ErG1.getP1(),0 ))
+                         
+    elif ErG1.getType()=='S' and ErG2.getType()=='C':
+       pVec1=labelMultiply(ErG1.getP1() , ErG2.getP1())
+       pVec2=labelMultiply(ErG2.getP2() , ErG1.getP1())
+       errorGens.append( propagatableerrorgen( 'A' , [pVec1[1], pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]  ))
+       pVec1 = labelMultiply(ErG1.getP1() , ErG2.getP2())
+       pVec2 = labelMultiply(ErG2.getP1() , ErG1.getP1())
+       errorGens.append( propagatableerrorgen( 'A', [pVec1[1], pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]))
+       pVec1 =acom(ErG2.getP1(), ErG2.getP2())
+       pVec2 = labelMultiply(pVec1[1],ErG1.getP1())
+       errorGens.append( propagatableerrorgen( 'A'  ,[pVec2[1], ErG1.getP1()] , -1j*.5*wT*pVec1[0]*pVec2[0]))
+       pVec1=acom(ErG2.getP1(), ErG2.getP2())
+       pVec2=labelMultiply(ErG1.getP1(),pVec1[1])
+       errorGens.append( propagatableerrorgen( 'A',   [ErG1.getP1() ,pVec2[1]],-1j*.5*wT*pVec1[0]*pVec2[0]))
+                         
+    elif ErG1.getType() == 'C' and ErG2.getType() == 'S':
+       errorGens = commute_errors(ErG2,ErG1,weightFlip=-1.0,BCHweight=BCHweight)
+                         
+    elif ErG1.getType() == 'S' and ErG2.getType() == 'A':
+       pVec1 =labelMultiply(ErG1.getP1() , ErG2.getP1())
+       pVec2=labelMultiply(ErG2.getP2() , ErG1.getP1())
+       errorGens.append( propagatableerrorgen( 'C', [pVec1[1], pVec2[1]] ,1j*wT*pVec1[0]*pVec2[0] ))
+       pVec1=labelMultiply(ErG1.getP1() , ErG2.getP2())
+       pVec2=labelMultiply(ErG2.getP1() , ErG1.getP1())
+       errorGens.append( propagatableerrorgen( 'C', [pVec1[1], pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]))
+       pVec1 = com(ErG2.getP1() , ErG2.getP2())
+       pVec2 = com(ErG1.getP1(),pVec1[1])
+       errorGens.append( propagatableerrorgen( 'A', [ErG1.getP1(), pVec2[1]] ,-.5*wT*pVec1[0]*pVec2[0]))
+                         
+    elif ErG1.getType() == 'A' and ErG1.getType() == 'S':
+       errorGens = commute_errors(ErG2,ErG1,weightFlip=-1.0,BCHweight=BCHweight)
+                         
+    elif ErG1.getType() == 'C' and ErG2.getType() == 'C':
+        A=ErG1.getP1()
+        B=ErG1.getP2()
+        P=ErG2.getP1()
+        Q=ErG2.getP2()
+        pVec1 = labelMultiply(A,P)
+        pVec2 =labelMultiply(Q,B)
+        errorGens.append( propagatableerrorgen( 'A', [pVec1[1], pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0] ))        
+        pVec1 = labelMultiply(A,Q)
+        pVec2 =labelMultiply(P,B)
+        errorGens.append( propagatableerrorgen( 'A'  , [pVec1[1] , pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = labelMultiply(B,P)
+        pVec2 =labelMultiply(Q,A)                 
+        errorGens.append( propagatableerrorgen( 'A'  , [pVec1[1] , pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = labelMultiply(B,Q)
+        pVec2 =labelMultiply(P,A)
+        errorGens.append( propagatableerrorgen( 'A'  , [pVec1[1] , pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=acom(A,B)
+        pVec2=com(P,pVec1[1])
+        errorGens.append( propagatableerrorgen( 'A'  , [pVec2[1] , Q ], -.5*1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=acom(A,B)
+        pVec2=com(Q,pVec1[1])
+        errorGens.append( propagatableerrorgen( 'A'  , [pVec2[1], P] , -.5*1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=acom(P,Q)
+        pVec2=com(pVec1[1],A)
+        errorGens.append( propagatableerrorgen( 'A' , [pVec2[1] , B] , -.5*1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=acom(P,Q)
+        pVec2=com(pVec1[1],B)
+        errorGens.append( propagatableerrorgen( 'A' , [pVec2[1] , A ] , -.5*1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=acom(A,B)
+        pVec2=acom(P,Q)
+        pVec3=com(pVec1[1],pVec2[1])
+        errorGens.append( propagatableerrorgen( 'H', [pVec3[1]] ,.25*1j*wT*pVec1[0]*pVec2[0]*pVec3[0]))
+
+    elif ErG1.getType() == 'C' and ErG2.getType() == 'A':
+        A=ErG1.getP1()
+        B=ErG1.getP2()
+        P=ErG2.getP1()
+        Q=ErG2.getP2()
+        pVec1 = labelMultiply(A,P)
+        pVec2 =labelMultiply(Q,B)
+        errorGens.append( propagatableerrorgen('C' , [pVec1[1],pVec2[1]] , 1j*wT*pVec1[0]*pVec2[0]))
+        pVec1 = labelMultiply(A,Q)
+        pVec2 =labelMultiply(P,B)
+        errorGens.append( propagatableerrorgen('C' ,[pVec1[1],pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]))
+        pVec1 = labelMultiply(B,P)
+        pVec2 =labelMultiply(Q,A)
+        errorGens.append( propagatableerrorgen('C' , [pVec1[1],pVec2[1]] , 1j*wT*pVec1[0]*pVec2[0]))
+        pVec1 = labelMultiply(P,A)
+        pVec2 =labelMultiply(B,Q)
+        errorGens.append( propagatableerrorgen('C'  ,[pVec1[1],pVec2[1]] , -1j*wT*pVec1[0]*pVec2[0]))
+        pVec1 = com(P,Q)
+        pVec2 =com(A,pVec1[1])
+        errorGens.append( propagatableerrorgen('A'  , [pVec2[1] , B] , .5*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = com(P,Q)
+        pVec2 =com(B,pVec1[1])
+        errorGens.append( propagatableerrorgen('A' , [pVec2[1],  A ], .5*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = acom(A,B)
+        pVec2 =com(P,pVec1[1])
+        errorGens.append( propagatableerrorgen('C', [pVec2[1] , Q ], .5*1j*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = acom(A,B)
+        pVec2 =com(Q,pVec1[1])
+        errorGens.append( propagatableerrorgen('C',[pVec2[1],P ],-.5*1j*wT*pVec1[0]*pVec2[0] ))
+        pVec1 = com(P,Q)
+        pVec2 =acom(A,B)
+        pVec3=com(pVec1[1],pVec2[1])
+        errorGens.append( propagatableerrorgen('H',[pVec3[1]],-.25*wT*pVec1[0]*pVec2[0]*pVec3[0]))
+    
+    elif ErG1.getType() == 'A' and ErG2.getType() == 'C':
+        errorGens = commute_errors(ErG2,ErG1,weightFlip=-1.0,BCHweight=BCHweight)
+                         
+    elif ErG1.getType() == 'A' and ErG2.getType() == 'A':
+        A=ErG1.getP1()
+        B=ErG1.getP2()
+        P=ErG2.getP1()
+        Q=ErG2.getP2()
+        pVec1=labelMultiply(Q,B)
+        pVec2=labelMultiply(A,P)
+        errorGens.append(propagatableerrorgen('A',[pVec1[1],pVec2[1]] ,-1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=labelMultiply(P,A)
+        pVec2=labelMultiply(B,Q)
+        errorGens.append(propagatableerrorgen('A',[pVec1[1],pVec2[1]],-1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=labelMultiply(B,P)
+        pVec2=labelMultiply(Q,A)
+        errorGens.append(propagatableerrorgen('A',[pVec1[1],pVec2[1]],-1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=labelMultiply(A,Q)
+        pVec2=labelMultiply(P,B)
+        errorGens.append(propagatableerrorgen('A',[pVec1[1],pVec2[1]],-1j*wT*pVec1[0]*pVec2[0]))
+        pVec1=com(P,Q)
+        pVec2=com(B,pVec1[1])
+        errorGens.append(propagatableerrorgen('C',[pVec2[1],A],.5*wT*pVec1[0]*pVec2[0]))
+        pVec1=com(P,Q)
+        pVec2=com(A,pVec1[1])
+        errorGens.append(propagatableerrorgen('C',[pVec2[1],B] ,-.5*wT*pVec1[0]*pVec2[0]))
+        pVec1=com(A,B)
+        pVec2=com(P,pVec1[1])
+        errorGens.append(propagatableerrorgen('C', [pVec2[1],Q] ,.5*wT*pVec1[0]*pVec2[0]))
+        pVec1=com(A,B)
+        pVec2=com(Q,pVec1[1])
+        errorGens.append(propagatableerrorgen('C', [pVec2[1],P]  ,-.5*wT*pVec1[0]*pVec2[0]))
+        pVec1=com(P,Q)
+        pVec2=com(A,B)
+        pVec3=com(pVec1[1],pVec2[1])
+        errorGens.append( propagatableerrorgen('H',[pVec3[1]] ,.25*wT*pVec1[0]*pVec2[0]*pVec3[0]))
+           
+    
+    return errorGens
+
+
