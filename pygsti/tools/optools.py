3f04a929b5c54ee4dd44085d9aff6ed627475086
Riley Murray <rjmurr@sandia.gov>
2024-05-06 10:28:00 -0400

    remove _hack_sqrtm and improve efficiency of fidelity calculation

--- pygsti/tools/optools.py
+++ pygsti/tools/optools.py
@@ -43,19 +43,6 @@ def _flat_mut_blks(i, j, block_dims):
     return ret
 
 
-def _hack_sqrtm(a):
-    sqrt, _ = _spl.sqrtm(a, disp=False)  # Travis found this scipy function
-    # to be incorrect in certain cases (we need a workaround)
-    if _np.any(_np.isnan(sqrt)):  # this is sometimes a good fallback when sqrtm doesn't work.
-        ev, U = _np.linalg.eig(a)
-        sqrt = _np.dot(U, _np.dot(_np.diag(_np.sqrt(ev)), _np.linalg.inv(U)))
-    # Scipy 1.10 fix for PR 16294 (which doubles precision of complex to complex)
-    if _np.iscomplexobj(a):
-        sqrt = sqrt.astype(a.dtype, copy=False)
-
-    return sqrt
-
-
 def fidelity(a, b):
     """
     Returns the quantum state fidelity between density matrices.
@@ -81,32 +68,81 @@ def fidelity(a, b):
     float
         The resulting fidelity.
     """
-    evals, U = _np.linalg.eig(a)
-    if len([ev for ev in evals if abs(ev) > 1e-8]) == 1:
+    __SCALAR_TOL__ = _np.finfo(a.dtype).eps ** 0.75
+    # ^ use for checks that have no dimensional dependence; about 1e-12 for double precision.
+    __VECTOR_TOL__ = (a.shape[0] ** 0.5) * __SCALAR_TOL__
+    # ^ use for checks that do have dimensional dependence (will naturally increase for larger matrices)
+    hermiticity_error = _np.abs(a - a.T.conj())
+    if _np.any(hermiticity_error > __SCALAR_TOL__):
+        message = f"""
+            Input matrix 'a' is not Hermitian, up to tolerance {__SCALAR_TOL__}.
+            The absolute values of entries in (a - a^H) are \n{hermiticity_error}. 
+        """
+        raise ValueError(message)
+
+    def check_rank_one_density(mat):
+        # Check if mat = alpha * np.outer(v, v.conj()) for some unit vector v and scalar alpha > 0.
+        #     If this holds up to some numerical tolerance, then we return (alpha, v)
+        #     If (we believe) no such vector exists, then we return None.
+        # 
+        # This function runs on O(n^2) time, where mat is n-by-n.
+        #
+        _np.random.seed(0)
+        n = mat.shape[0]
+        test_vec = _np.random.randn(n)
+        if _np.iscomplexobj(mat):
+            test_vec = test_vec + 1j * _np.random.randn(n)
+        test_vec /= _np.linalg.norm(test_vec)
+        candidate_v = mat @ test_vec
+        candidate_v /= _np.linalg.norm(candidate_v)
+        alpha = _np.real(candidate_v.conj() @ mat @ candidate_v)
+        reconstruction = alpha * _np.outer(candidate_v, candidate_v.conj())
+        if _np.linalg.norm(mat - reconstruction) < __VECTOR_TOL__:
+            return alpha, candidate_v
+        else:
+            return None
+
+    alphavec = check_rank_one_density(a)
+    if alphavec is not None:
         # special case when a is rank 1, a = vec * vec^T and sqrt(a) = a
-        ivec = _np.argmax(evals)
-        vec = U[:, ivec:(ivec + 1)]
-        F = evals[ivec].real * _np.dot(_np.conjugate(_np.transpose(vec)), _np.dot(b, vec)).real  # vec^T * b * vec
-        return float(F[0, 0])
+        alpha, vec = alphavec
+        f = alpha * (vec.T.conj() @ b @ vec).real  # vec^T * b * vec
+        return f
 
-    evals, U = _np.linalg.eig(b)
-    if len([ev for ev in evals if abs(ev) > 1e-8]) == 1:
+    alphavec = check_rank_one_density(b)
+    if alphavec is not None:
         # special case when b is rank 1 (recally fidelity is sym in args)
-        ivec = _np.argmax(evals)
-        vec = U[:, ivec:(ivec + 1)]
-        F = evals[ivec].real * _np.dot(_np.conjugate(_np.transpose(vec)), _np.dot(a, vec)).real  # vec^T * a * vec
-        return float(F[0, 0])
+        alpha, vec = alphavec
+        f = alpha * (vec.T.conj() @ a @ vec).real  # vec^T * a * vec
+        return f
+    
+    # Neither a nor b are rank-1. We need to actually evaluate the matrix square root of
+    # one of them. We do this with an eigendecomposition, since this lets us check for 
+    # negative eigenvalues and raise a warning if needed.
 
-    #if _np.array_equal(a, b): return 1.0  # HACK - some cases when a and b are perfecty equal sqrtm(a) fails...
-    sqrtA = _hack_sqrtm(a)  # _spl.sqrtm(a)
-    # test the scipy sqrtm function - sometimes fails when rank defficient
-    #assert(_np.linalg.norm(_np.dot(sqrtA, sqrtA) - a) < 1e-8)
-    if _np.linalg.norm(_np.dot(sqrtA, sqrtA) - a) > 1e-8:
-        evals = _np.linalg.eigvals(a)
-        _warnings.warn(("sqrtm(a) failure when computing fidelity - beware result. "
-                        "Maybe due to rank defficiency - eigenvalues of a are: %s") % evals)
-    F = (_np.trace(_hack_sqrtm(_np.dot(sqrtA, _np.dot(b, sqrtA)))).real)**2  # Tr( sqrt{ sqrt(a) * b * sqrt(a) } )^2
-    return float(F)
+    def psd_square_root(mat):
+        evals, U = _np.linalg.eigh(mat)
+        if _np.min(evals) < -__SCALAR_TOL__:
+            message = f"""
+            Input matrix is not PSD up to tolerance {__SCALAR_TOL__}.
+            We'll project out the bad eigenspaces to only work with the PSD part.
+            """
+            _warnings.warn(message)
+        evals[evals < 0] = 0.0
+        tr = _np.sum(evals)
+        if abs(tr - 1) > __VECTOR_TOL__:
+            message = f"""
+            The PSD part of the input matrix is not trace-1 up to tolerance {__VECTOR_TOL__}.
+            Beware result!
+            """
+            _warnings.warn(message)
+        sqrt_mat = U @ (_np.sqrt(evals).reshape((-1, 1)) * U.T.conj())
+        return sqrt_mat
+    
+    sqrt_a = psd_square_root(a)
+    tr_arg = psd_square_root(sqrt_a @ b @ sqrt_a)
+    f = _mt.trace(tr_arg).real ** 2  # Tr( sqrt{ sqrt(a) * b * sqrt(a) } )^2
+    return f
 
 
 def frobeniusdist(a, b):
