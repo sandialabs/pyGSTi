4dc0fcce0aa44d1ac03eb9e96a0e523c66cfc4d5
Stefan K. Seritan <sserita@sandia.gov>
2024-05-06 15:46:58 -0700

    Untested port of Pauli commutation code.
    This should be a one-to-one port of `commute_errors` in propagatableerrorgen.py.
    The difference is that this does not use STIM or a subclass of the ErrorgenLabels.
    Once tested, this will form the core routine in the rest of my BCH utilities.

--- pygsti/tools/lindbladtools.py
+++ pygsti/tools/lindbladtools.py
@@ -10,13 +10,19 @@ Utility functions relevant to Lindblad forms and projections
 # http://www.apache.org/licenses/LICENSE-2.0 or in the LICENSE file in the root pyGSTi directory.
 #***************************************************************************************************
 
+from collections import defaultdict
 from collections.abc import Iterable
 import numpy as _np
 import scipy.sparse as _sps
-from typing import Tuple, Union
+from typing import Dict, Tuple, Union
 
 from pygsti.tools import matrixtools as _mt
 from pygsti.tools.basistools import basis_matrices
+from pygsti.baseobjs.errorgenlabel import (
+    ElementaryErrorgenLabel as _EEGLabel,
+    LocalElementaryErrorgenLabel as _LocalEEGLabel,
+    GlobalElementaryErrorgenLabel as _GlobalEEGLabel
+)
 
 
 def create_elementary_errorgen_dual(typ, p, q=None, sparse=False, normalization_factor='auto'):
@@ -351,3 +357,314 @@ def paulistr_anticommutator(pstr1: Iterable[str], pstr2: Iterable[str]) -> Tuple
     
     # Double weight from product for the two terms in the anticommutator
     return (2, output[1])
+
+def _errorgen_commutator_symbolic_term(
+    lbl1: _LocalEEGLabel, lbl2: _LocalEEGLabel
+) -> Dict[_LocalEEGLabel, complex]:
+    """Compute the symbolic commutator of two elementary error generators.
+    
+    **Note:** This is using the code currently in feature-Propagatable-ErrorGens
+    rather than the original notes. I'm assuming that the code is more debugged
+    than the notes, but I'm marking differences in red.
+    
+    $$\large
+    \begin{align*}
+    [H_P, H_Q] &= -iH_{[P,Q]}\\
+    [H_P, S_Q] &= iC_{Q,[Q,P]}\\
+    [H_P, C_{A,B}] &= iC_{[A,P],B} + iC_{\textcolor{red}{[B,P]},A}\\
+    [H_P, A_{A,B}] &= -iA_{\textcolor{red}{[P,A]},B} - iA_{A,[P,B]}\\
+    [S_P, S_Q] &= 0\\
+    [S_P, C_{A,B}] &= -iA_{\textcolor{red}{PA},BP} -iA_{PB,\textcolor{red}{AP}} -\frac{i}{2}\big(A_{\{A,B\}P,P} + A_{P,P\{A,B\}}\big)\\
+    [S_P, A_{A,B}] &= iC_{PA,BP} -iC_{PB,AP} -\frac{textcolor{red}{1}}{2}A_{P,[P,[A,B]]}\\
+    [C_{A,B}, C_{P,Q}] &= -i\big(A_{AP,QB} + A_{AQ,\textcolor{red}{PB}} \textcolor{red}{+} A_{BP,QA} \textcolor{red}{+} A_{BQ,PA}\big)\\
+    &-\frac{i}{2}\big(A_{[P,\{A,B\}],Q} + A_{[Q,\{A,B\}],P} + A_{[\{P,Q\},A],B} + A_{[\{P,Q\},B],A}\big)\\
+    &+\frac{i}{4}H_{[\{A,B\},\{P,Q\}]}\\
+    [C_{A,B}, A_{P,Q}] &= i\big(C_{AP,QB} - C_{AQ,PB} + C_{BP,QA} - C_{PA,BQ}\big)\\
+    &+\frac{1}{2}\big(A_{[A,[P,Q]],B} + A_{[B,[P,Q]],A} + iC_{[P,\{A,B\}],Q} - \textcolor{red}{i}C_{[Q,\{A,B\}],P}\big)\\
+    &-\frac{1}{4}H_{[[P,Q],\{A,B\}]}\\
+    [A_{A,B}, A_{P,Q}] &= -i\big(A_{QB,AP} + A_{PA,BQ} + A_{BP,QA} + A_{AQ,PB}\big)\\
+    &+\frac{1}{2}\big(C_{[B,[P,Q]],A} - C_{[A,[P,Q]],B} + C_{[P,[A,B]],Q} - C_{[Q,[A,B]],P}\big)\\
+    &+\frac{\textcolor{red}{1}}{4}H_{[[P,Q],[A,B]]}
+    \end{align*}
+    $$
+
+    Parameters
+    ----------
+    lbl1:
+        The first error generator label
+    
+    lbl2:
+        The second error generator label
+    
+    Returns
+    -------
+    terms
+        Dicts with the resulting error generator labels as keys
+        and corresponding weights as values
+    """
+    lbl1 = _LocalEEGLabel.cast(lbl1)
+    lbl2 = _LocalEEGLabel.cast(lbl2)
+    
+    # Sanity checks. If this is not true, the else below will be an infinite recursion
+    assert lbl1.errorgen_type in "HSCA"
+    assert lbl2.errorgen_type in "HSCA"
+    
+    # So that we can += and have a default value of 0
+    # Should take care of most of our collision logic
+    terms = defaultdict(complex)
+    
+    if lbl1.errorgen_type == "H" and lbl2.errorgen_type == "H":
+        # [H_P, H_Q] = -i H_[P,Q]
+        P = lbl1.basis_element_labels[0]
+        Q = lbl2.basis_element_labels[0]
+        
+        wt_cPQ, cPQ = paulistr_commutator(P, Q)
+        if wt_cPQ:
+            terms[_LocalEEGLabel("H", [cPQ])] += -1j*wt_cPQ
+    elif lbl1.errorgen_type == "H" and lbl2.errorgen_type == "S":
+        # [H_P, S_Q] = iC_{Q,[Q,P]}
+        P = lbl1.basis_element_labels[0]
+        Q = lbl2.basis_element_labels[0]
+        
+        wt_cQP, cQP = paulistr_commutator(Q, P)
+        if wt_cQP:
+            terms[_LocalEEGLabel("C", [Q, cQP])] += 1j*wt_cQP
+    elif lbl1.errorgen_type == "H" and lbl2.errorgen_type == "C":
+        # [H_P, C_{A,B}] = iC_{[A,P],B} + iC_{\textcolor{red}{[B,P]},A}
+        P = lbl1.basis_element_labels[0]
+        A,B = lbl2.basis_element_labels
+        
+        wt_cAP, cAP = paulistr_commutator(A, P)
+        if wt_cAP:
+            terms[_LocalEEGLabel("C", [cAP, B])] += 1j*wt_cAP
+        
+        wt_cBP, cBP = paulistr_commutator(B, P)
+        if wt_cBP:
+            terms[_LocalEEGLabel("C", [cBP, A])] += 1j*wt_cBP
+    elif lbl1.errorgen_type == "H" and lbl2.errorgen_type == "A":
+        # [H_P, A_{A,B}] = -iA_{\textcolor{red}{[P,A]},B} - iA_{A,[P,B]}
+        P = lbl1.basis_element_labels[0]
+        A,B = lbl2.basis_element_labels
+        
+        wt_cPA, cPA = paulistr_commutator(P, A)
+        if wt_cPA:
+            terms[_LocalEEGLabel("A", [cPA, B])] += -1j*wt_cPA
+        
+        wt_cPB, cPB = paulistr_commutator(P, B)
+        if wt_cPB:
+            terms[_LocalEEGLabel("C", [A, cPB])] += -1j*wt_cPB
+    elif lbl1.errorgen_type == "S" and lbl2.errorgen_type == "S":
+        pass
+    elif lbl1.errorgen_type == "S" and lbl2.errorgen_type == "C":
+        # [S_P, C_{A,B}] = -iA_{\textcolor{red}{PA},BP} -iA_{PB,\textcolor{red}{AP}}
+        # -\frac{i}{2}\big(A_{\{A,B\}P,P} + A_{P,P\{A,B\}}\big)
+        P = lbl1.basis_element_labels[0]
+        A,B = lbl2.basis_element_labels
+        
+        wt_PA, PA = paulistr_multiply(P, A)
+        wt_PB, PB = paulistr_multiply(P, B)
+        wt_AP, AP = paulistr_multiply(A, P)
+        wt_BP, BP = paulistr_multiply(B, P)
+        
+        # We have A_{PA,BP} + A_{PB,AP} = A_{PA,BP} - A_{AP,PB}
+        # These cancel if PA == AP and PB == BP
+        if PA != AP or PB != BP:
+            terms[_LocalEEGLabel("A", [PA, BP])] += -1j*wt_PA*wt_BP
+            terms[_LocalEEGLabel("A", [PB, AP])] += -1j*wt_PA*wt_BP
+        
+        wt_aAB, aAB = paulistr_anticommutator(A, B)
+        if wt_aAB:
+            wt_aABP, aABP = paulistr_multiply(aAB, P)
+            if wt_aABP:
+                terms[_LocalEEGLabel("A", [aABP, P])] += -0.5*1j*wt_aAB*wt_aABP
+            
+            wt_PaAB, PaAB = paulistr_multiply(P, aAB)
+            if wt_PaAB:
+                terms[_LocalEEGLabel("A", [P, PaAB])] += -0.5*1j*wt_aAB*wt_PaAB
+    elif lbl1.errorgen_type == "S" and lbl2.errorgen_type == "A":
+        # [S_P, A_{A,B}] = iC_{PA,BP} -iC_{PB,AP} -\frac{textcolor{red}{1}}{2}A_{P,[P,[A,B]]}
+        P = lbl1.basis_element_labels[0]
+        A,B = lbl2.basis_element_labels
+            
+        wt_PA, PA = paulistr_multiply(P, A)
+        wt_PB, PB = paulistr_multiply(P, B)
+        wt_AP, AP = paulistr_multiply(A, P)
+        wt_BP, BP = paulistr_multiply(B, P)
+        
+        # We have C_{PA,BP} - C_{PB,AP} = C_{PA,BP} - C_{AP,PB}
+        # These cancel if PA == AP and PB == BP
+        if PA != AP or PB != BP:
+            terms[_LocalEEGLabel("C", [PA, BP])] += -1j*wt_PA*wt_BP
+            terms[_LocalEEGLabel("C", [PB, AP])] += -1j*wt_PA*wt_BP
+        
+        wt_cAB, cAB = paulistr_commutator(A, B)
+        wt_cPcAB, cPcAB = paulistr_commutator(P, cAB)
+        if wt_cAB and wt_cPcAB:
+            terms[_LocalEEGLabel("A", [P, cPcAB])] += -0.5*wt_cAB*wt_cPcAB
+    elif lbl1.errorgen_type == "C" and lbl2.errorgen_type == "C":
+        # [C_{A,B}, C_{P,Q}] = -i\big(A_{AP,QB} + A_{AQ,\textcolor{red}{PB}}
+        # \textcolor{red}{+} A_{BP,QA} \textcolor{red}{+} A_{BQ,PA}\big)\\
+        # -\frac{i}{2}\big(A_{[P,\{A,B\}],Q} + A_{[Q,\{A,B\}],P} + A_{[\{P,Q\},A],B} + A_{[\{P,Q\},B],A}\big)\\
+        # +\frac{i}{4}H_{[\{A,B\},\{P,Q\}]}\\
+        A,B = lbl1.basis_element_labels
+        P,Q = lbl2.basis_element_labels
+
+        wt_PA, PA = paulistr_multiply(P, A)
+        wt_PB, PB = paulistr_multiply(P, B)
+        wt_AP, AP = paulistr_multiply(A, P)
+        wt_BP, BP = paulistr_multiply(B, P)
+        
+        wt_QA, QA = paulistr_multiply(Q, A)
+        wt_QB, QB = paulistr_multiply(Q, B)
+        wt_AQ, AQ = paulistr_multiply(A, Q)
+        wt_BQ, BQ = paulistr_multiply(B, Q)
+
+        # We have A_{AP,QB} + A_{BQ,PA} = A_{AP,QB} - A_{PA,BQ}
+        # These cancel if PA == AP and QB == BQ
+        if PA != AP or QB != BQ:
+            terms[_LocalEEGLabel("A", [AP, QB])] += -1j*wt_AP*wt_QB
+            terms[_LocalEEGLabel("A", [BQ, PA])] += -1j*wt_BQ*wt_PA
+        
+        # We also have A_{AQ,PB} + A_{BP,QA} = A_{AQ,PB} - A_{QA,BP}
+        # These cancel if PB == BP and QA == AQ
+        if PB != BP or QA != AQ:
+            terms[_LocalEEGLabel("A", [AQ, PB])] += -1j*wt_AQ*wt_PB
+            terms[_LocalEEGLabel("A", [BP, QA])] += -1j*wt_BP*wt_QA
+        
+        wt_aAB, aAB = paulistr_anticommutator(A, B)
+        if wt_aAB:
+            wt_cPaAB, cPaAB = paulistr_commutator(P, aAB)
+            if wt_cPaAB:
+                terms[_LocalEEGLabel("A", [cPaAB, Q])] += -0.5*1j*wt_aAB*wt_cPaAB
+
+            wt_cQaAB, cQaAB = paulistr_commutator(Q, aAB)
+            if wt_cQaAB:
+                terms[_LocalEEGLabel("A", [cQaAB, P])] += -0.5*1j*wt_aAB*wt_cQaAB
+        
+        wt_aPQ, aPQ = paulistr_anticommutator(P, Q)
+        if wt_aPQ:
+            wt_caPQA, caPQA = paulistr_commutator(aPQ, A)
+            if wt_caPQA:
+                terms[_LocalEEGLabel("A", [caPQA, B])] += -0.5*1j*wt_aPQ*wt_caPQA
+            
+            wt_caPQB, caPQB = paulistr_commutator(aPQ, B)
+            if wt_caPQB:
+                terms[_LocalEEGLabel("A", [caPQB, A])] += -0.5*1j*wt_aPQ*wt_caPQB
+        
+        if wt_aAB and wt_aPQ:
+            wt_caABaPQ, caABaPQ = paulistr_commutator(aAB, aPQ)
+            if wt_caABaPQ:
+                terms[_LocalEEGLabel("H", [caABaPQ])] += 0.25*1j*wt_aAB*wt_aPQ*wt_caABaPQ
+    elif lbl1.errorgen_type == "C" and lbl2.errorgen_type == "A":
+        # [C_{A,B}, A_{P,Q}] = i\big(C_{AP,QB} - C_{AQ,PB} + C_{BP,QA} - C_{PA,BQ}\big)\\
+        # +\frac{1}{2}\big(A_{[A,[P,Q]],B} + A_{[B,[P,Q]],A} + iC_{[P,\{A,B\}],Q} - \textcolor{red}{i}C_{[Q,\{A,B\}],P}\big)\\
+        # -\frac{1}{4}H_{[[P,Q],\{A,B\}]}\\
+        A,B = lbl1.basis_element_labels
+        P,Q = lbl2.basis_element_labels
+
+        wt_PA, PA = paulistr_multiply(P, A)
+        wt_PB, PB = paulistr_multiply(P, B)
+        wt_AP, AP = paulistr_multiply(A, P)
+        wt_BP, BP = paulistr_multiply(B, P)
+        
+        wt_QA, QA = paulistr_multiply(Q, A)
+        wt_QB, QB = paulistr_multiply(Q, B)
+        wt_AQ, AQ = paulistr_multiply(A, Q)
+        wt_BQ, BQ = paulistr_multiply(B, Q)
+
+        # We have C_{AP,QB} - C_{PA,BQ}
+        # These cancel if PA == AP and QB == BQ
+        if PA != AP or QB != BQ:
+            terms[_LocalEEGLabel("C", [AP, QB])] += 1j*wt_AP*wt_QB
+            terms[_LocalEEGLabel("C", [BQ, PA])] += -1j*wt_BQ*wt_PA
+        
+        # We also have -C_{AQ,PB} + C_{BP,QA}
+        # These cancel if PB == BP and QA == AQ
+        if PB != BP or QA != AQ:
+            terms[_LocalEEGLabel("C", [AQ, PB])] += -1j*wt_AQ*wt_PB
+            terms[_LocalEEGLabel("C", [BP, QA])] += 1j*wt_BP*wt_QA
+        
+        wt_cPQ, cPQ = paulistr_commutator(P, Q)
+        if wt_cPQ:
+            wt_cAcPQ, cAcPQ = paulistr_commutator(A, cPQ)
+            if wt_cAcPQ:
+                terms[_LocalEEGLabel("A", [cAcPQ, B])] += 0.5*wt_cPQ*wt_cAcPQ
+            
+            wt_cBcPQ, cBcPQ = paulistr_commutator(B, cPQ)
+            if wt_cBcPQ:
+                terms[_LocalEEGLabel("A", [cBcPQ, A])] += 0.5*wt_cPQ*wt_cBcPQ
+        
+        wt_aAB, aAB = paulistr_anticommutator(A, B)
+        if wt_aAB:
+            wt_cPaAB, cPaAB = paulistr_commutator(P, aAB)
+            if wt_cPaAB:
+                terms[_LocalEEGLabel("C", [cPaAB, Q])] += 0.5*1j*wt_aAB*wt_cPaAB
+
+            wt_cQaAB, cQaAB = paulistr_commutator(Q, aAB)
+            if wt_cQaAB:
+                terms[_LocalEEGLabel("C", [cQaAB, P])] += -0.5*1j*wt_aAB*wt_cQaAB
+        
+        if wt_aAB and wt_cPQ:
+            wt_ccPQaAB, ccPQaAB = paulistr_commutator(cPQ, aAB)
+            if wt_ccPQaAB:
+                terms[_LocalEEGLabel("H", [ccPQaAB])] += 0.25*wt_aAB*wt_cPQ*wt_ccPQaAB
+    elif lbl1.errorgen_type == "A" and lbl2.errorgen_type == "A":
+        # [A_{A,B}, A_{P,Q}] = -i\big(A_{QB,AP} + A_{PA,BQ} + A_{BP,QA} + A_{AQ,PB}\big)\\
+        # +\frac{1}{2}\big(C_{[B,[P,Q]],A} - C_{[A,[P,Q]],B} + C_{[P,[A,B]],Q} - C_{[Q,[A,B]],P}\big)\\
+        # +\frac{\textcolor{red}{1}}{4}H_{[[P,Q],[A,B]]}
+        A,B = lbl1.basis_element_labels
+        P,Q = lbl2.basis_element_labels
+
+        wt_PA, PA = paulistr_multiply(P, A)
+        wt_PB, PB = paulistr_multiply(P, B)
+        wt_AP, AP = paulistr_multiply(A, P)
+        wt_BP, BP = paulistr_multiply(B, P)
+        
+        wt_QA, QA = paulistr_multiply(Q, A)
+        wt_QB, QB = paulistr_multiply(Q, B)
+        wt_AQ, AQ = paulistr_multiply(A, Q)
+        wt_BQ, BQ = paulistr_multiply(B, Q)
+
+        # We have A_{QB,AP} + A_{PA,BQ} = A_{QB,AP} - A_{BQ,PA}
+        # These cancel if PA == AP and QB == BQ
+        if PA != AP or QB != BQ:
+            terms[_LocalEEGLabel("A", [QB, AP])] += -1j*wt_QB*wt_AP
+            terms[_LocalEEGLabel("A", [BQ, PA])] += -1j*wt_BQ*wt_PA
+        
+        # We also have A_{BP,QA} + A_{AQ,PB} = A_{BP,QA} - A_{PB,AQ}
+        # These cancel if PB == BP and QA == AQ
+        if PB != BP or QA != AQ:
+            terms[_LocalEEGLabel("A", [BP, QA])] += -1j*wt_BP*wt_QA
+            terms[_LocalEEGLabel("A", [AQ, PB])] += -1j*wt_AQ*wt_PB
+        
+        wt_cPQ, cPQ = paulistr_commutator(P, Q)
+        if wt_cPQ:
+            wt_cBcPQ, cBcPQ = paulistr_commutator(B, cPQ)
+            if wt_cBcPQ:
+                terms[_LocalEEGLabel("C", [cBcPQ, A])] += 0.5*wt_cPQ*wt_cBcPQ
+            
+            wt_cAcPQ, cAcPQ = paulistr_commutator(A, cPQ)
+            if wt_cAcPQ:
+                terms[_LocalEEGLabel("C", [cAcPQ, B])] += -0.5*wt_cPQ*wt_cAcPQ
+        
+        wt_cAB, cAB = paulistr_commutator(A, B)
+        if wt_cAB:
+            wt_cPcAB, cPcAB = paulistr_commutator(P, cAB)
+            if wt_cPaAB:
+                terms[_LocalEEGLabel("C", [cPcAB, Q])] += 0.5*wt_cAB*wt_cPcAB
+
+            wt_cQcAB, cQcAB = paulistr_commutator(Q, cAB)
+            if wt_cQcAB:
+                terms[_LocalEEGLabel("C", [cQcAB, P])] += -0.5*wt_cAB*wt_cQcAB
+        
+        if wt_cAB and wt_cPQ:
+            wt_ccPQcAB, ccPQcAB = paulistr_commutator(cPQ, cAB)
+            if wt_ccPQcAB:
+                terms[_LocalEEGLabel("H", [ccPQcAB])] += 0.25*wt_cAB*wt_cPQ*wt_ccPQcAB
+    else:
+        # This should be a negative of one of the cases above, i.e. [B,A] = -[A,B]
+        neg_terms = _errorgen_commutator_symbolic_term(lbl2, lbl1)
+        for k,v in neg_terms.items():
+            terms[k] += -1*v
+    
+    return terms
