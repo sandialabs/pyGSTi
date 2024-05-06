2f3ca3fb6fd56c645483d13e9dee046e77def0f9
Riley Murray <rjmurr@sandia.gov>
2024-05-06 15:15:35 -0400

    left out of last commit

--- pygsti/report/reportables.py
+++ pygsti/report/reportables.py
@@ -538,10 +538,6 @@ if _CVXPY_AVAILABLE:
                 nearby_model.sim.product(self.circuit), mxBasis)
             JBstd = self.d * _tools.fast_jamiolkowski_iso_std(self.B, mxBasis)
             J = JBstd - JAstd
-            # Old code. Keep for now, to make it easier to see correctness of new code.
-            #   Jt = J.T
-            #   val = 0.5 * _np.trace(_np.dot(Jt.real, self.W.real) + _np.dot(Jt.imag, self.W.imag))
-            # New code.
             val = 0.5 * (_np.vdot(J.real, self.W.real) + _np.vdot(J.imag, self.W.imag))
             return val
 
@@ -1248,10 +1244,6 @@ if _CVXPY_AVAILABLE:
             JAstd = self.d * _tools.fast_jamiolkowski_iso_std(A, mxBasis)
             JBstd = self.d * _tools.fast_jamiolkowski_iso_std(self.B, mxBasis)
             J = JBstd - JAstd
-            # Old code. Keep for now, to make it easier to see correctness of new code.
-            #   Jt = J.T
-            #   val = 0.5 * _np.trace(_np.dot(Jt.real, self.W.real) + _np.dot(Jt.imag, self.W.imag))
-            # New code.
             val = 0.5 * (_np.vdot(J.real, self.W.real) + _np.vdot(J.imag, self.W.imag))
             return val
 
@@ -2031,20 +2023,7 @@ def robust_log_gti_and_projections(model_a, model_b, synthetic_idle_circuits):
                 noise = first_order_noise(opstr, errOnGate, gl)
                 jac[:, i * nSuperOps + k] = [_np.vdot(errOut.flatten(), noise.flatten()) for errOut in error_superops]
 
-                # DEBUG CHECK  # keep old code for now, to show correctness of new code.
-                #
-                # ------------------------------ original code ----------------------------
-                # check = [_np.trace(_np.dot(
-                #     _tools.jamiolkowski_iso(errOut, mxBasis, mxBasis).conj().T,
-                #     _tools.jamiolkowski_iso(noise,  mxBasis, mxBasis))) * 4  # for 1-qubit...
-                #     for errOut in error_superops]
-                # --------------------------- more readable code --------------------------
-                # check = []
-                # for errOut in error_superops:
-                #     arg1 = _tools.jamiolkowski_iso(errOut, mxBasis, mxBasis).conj().T
-                #     arg2 = _tools.jamiolkowski_iso(noise,  mxBasis, mxBasis)
-                #     check.append(_np.trace(_np.dot(arg1, arg2)) * 4)
-                # ---------------------------- efficient code -----------------------------
+                # DEBUG CHECK
                 check = []
                 for errOut in error_superops:
                     arg1 = _tools.jamiolkowski_iso(errOut, mxBasis, mxBasis)
