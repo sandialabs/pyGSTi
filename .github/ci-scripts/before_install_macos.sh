e3652703d3909c9d368146e98f91305d1961f137
Stefan K. Seritan <sserita@sandia.gov>
2024-05-15 14:11:47 -0700

    Fix CHP compilation for tests.

--- .github/ci-scripts/before_install_macos.sh
+++ .github/ci-scripts/before_install_macos.sh
@@ -6,8 +6,3 @@ gfortran openblas lapack openmpi \
 openssh suite-sparse
 cmake --version
 gcc --version
-
-#download chp source code
-curl -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c https://www.scottaaronson.com/chp/chp.c
-#compile chp
-gcc -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c
