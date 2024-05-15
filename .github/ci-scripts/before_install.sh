1f1b73dd059483b4b5225c00dbb30cab4ca5829b
Stefan K. Seritan <sserita@sandia.gov>
2024-05-15 12:49:34 -0700

    Add compiled CHP to Action environment.

--- .github/ci-scripts/before_install.sh
+++ .github/ci-scripts/before_install.sh
@@ -6,3 +6,8 @@ gfortran libblas-dev liblapack-dev openmpi-bin openmpi-common openssh-client \
 openssh-server libopenmpi3 libopenmpi-dev libsuitesparse-dev
 cmake --version
 gcc --version
+
+#download chp source code
+curl -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c https://www.scottaaronson.com/chp/chp.c
+#compile chp
+gcc -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c
--- .github/ci-scripts/before_install_macos.sh
+++ .github/ci-scripts/before_install_macos.sh
@@ -6,3 +6,8 @@ gfortran openblas lapack openmpi \
 openssh suite-sparse
 cmake --version
 gcc --version
+
+#download chp source code
+curl -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c https://www.scottaaronson.com/chp/chp.c
+#compile chp
+gcc -o ./jupyter_notebooks/Tutorials/algorithms/advanced/chp ./jupyter_notebooks/Tutorials/algorithms/advanced/chp.c
