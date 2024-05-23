d0e1bde22ee23c1399f349a12d55ab7d8ad8459e
Riley Murray <rjmurr@sandia.gov>
2024-05-23 10:28:37 -0400

    remove change that wasnt strictly in-scope for the PR

--- pygsti/extras/interpygate/process_tomography.py
+++ pygsti/extras/interpygate/process_tomography.py
@@ -30,7 +30,7 @@ def vec(matrix):
     """
     matrix = _np.array(matrix)
     if matrix.shape == (len(matrix), len(matrix)):
-        return matrix.reshape((-1, 1), order='F')
+        return _np.array([_np.concatenate(_np.array(matrix).T)]).T
     else:
         raise ValueError('The input matrix must be square.')
 
@@ -50,9 +50,9 @@ def unvec(vectorized):
 
     """
     vectorized = _np.array(vectorized)
-    dim = int(_np.sqrt(max(vectorized.shape)))
-    if len(vectorized) == dim ** 2:
-        return vectorized.reshape((dim, dim), order='F')
+    length = int(_np.sqrt(max(vectorized.shape)))
+    if len(vectorized) == length ** 2:
+        return _np.reshape(vectorized, [length, length]).T
     else:
         raise ValueError(
             'The input vector length must be a perfect square, but this input has length %d.' % len(vectorized))
