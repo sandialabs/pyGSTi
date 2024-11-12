c87500a8ceb68cae81e09d2be698c523d3a4ab43
Corey Ostrove <ciostro@sandia.gov>
2024-11-11 21:24:34 -0700

    Random CPTP error generators
    This adds a stab at functionality for random CP constrained error generators. In addition to enforcing the CP constraint, this also allows features like: specifying a target error rate, fixing particular error generator rates (while selecting the rest randomly), setting weights for the H and S sectors, and specifying error generator type/weight/sslbl overlap constraints.

--- pygsti/tools/lindbladtools.py
+++ pygsti/tools/lindbladtools.py
@@ -14,7 +14,10 @@ import numpy as _np
 import scipy.sparse as _sps
 
 from pygsti.tools.basistools import basis_matrices
-
+import pygsti.baseobjs as _bo
+from pygsti.baseobjs.errorgenlabel import GlobalElementaryErrorgenLabel as _GlobalElementaryErrorgenLabel
+from pygsti.baseobjs.statespace import QubitSpace as _QubitSpace
+import warnings as _warnings
 
 def create_elementary_errorgen_dual(typ, p, q=None, sparse=False, normalization_factor='auto'):
     """
@@ -229,3 +232,258 @@ def create_lindbladian_term_errorgen(typ, Lm, Ln=None, sparse=False):  # noqa N8
 
     if sparse: lind_errgen = lind_errgen.tocsr()
     return lind_errgen
+
+
+def random_error_generator_rates(num_qubits, errorgen_types=('H', 'S', 'C', 'A'), max_weights=None,
+                                 H_params=(0.,.01), SCA_params=(0.,.01), error_metric=None, error_metric_value=None, 
+                                 relative_HS_contribution=None, fixed_errorgen_rates=None, sslbl_overlap=None, seed = None):
+    """
+    Function for generating a random set of CPTP error generator rates.
+    
+    Parameters
+    ----------
+    num_qubits : int
+        Number of qubits the error generator acts upon.
+    
+    errorgen_types : tuple of str, optional (default('H', 'S', 'C', 'A'))
+        Tuple of strings designating elementary error generator types to include in this
+        basis. Note that due to the CP constraint, certain values are not allowed,
+        and any tuple containing 'C' or 'A' terms must also include 'S'.
+    
+    max_weights : dict, optional (default None)
+        An optional dictionary specifying the maximum weight
+        for each of the elementary error generator types, with keys
+        given by the strings 'H', 'S', 'C' and 'A'. If None then 
+        there is no maximum weight. If specified, any error generator
+        types without entries will have no maximum weight associated
+        with them.
+        
+    H_params : tuple of floats, optional (default (0.,.01))
+        Mean and standard deviation parameters for a normal distribution
+        from which the H rates will be sampled. Note that specifying a non-zero
+        value for the mean with generator_infidelity set to a non-trivial value
+        is not supported, and will raise an error.
+    
+    SCA_params : tuple of floats, optional (default (0.,.01))
+        Mean and standard deviation parameters for a normal distribution
+        from which the entries of the matrix used in the construction of the S, C and A rates
+        will be construction is sampled. Note that specifying a non-zero
+        value for the mean with generator_infidelity set to a non-trivial value
+        is not supported, and will raise an error.
+    
+    error_metric : str, optional (default None)
+        An optional string, used in conjunction with the error_metric_value
+        kwarg which specifies which metric to use in setting the sampled
+        channel's overall error rate. If None, no target value for the channel's
+        overall error rate is used. Currently supported options include:
+        
+        - 'generator_infidelity'
+        - 'total_generator_error'
+    
+    error_metric_value : float, optional (default None)
+        An float between 0 and 1 which gives the target value of the
+        error metric specified in 'error_metric' for the channel induced by
+        the randomly produced error generator. If None
+        then no target value is used and the returned error generator
+        will have a random generator infidelity.
+        
+    relative_HS_contribution : tuple, optional (default None)
+        An optional tuple, used in conjunction with the `generator_infidelity` kwarg,
+        specifying the relative contributions of the H and S error generators to the
+        generator infidelity. The values in this tuple should sum to 1. The first entry
+        corresponds to the H sector, and the second the S sector.
+        
+    sslbl_overlap : list of sslbls, optional (default None)
+        A list of state space labels corresponding to qudits the support of
+        an error generator must overlap with (i.e. the support must include at least
+        one of these qudits) in order to be included in this basis.
+        
+    fixed_errorgen_rates : dict, optional (default None)
+        An optional dictionary whose keys are `GlobalElementaryErrorgenLabel`
+        objects, and whose values are error generator rates. When specified, the
+        rates in this dictionary will override any randomly selected values in the
+        final returned error generator rate dictionary. The inclusion of these
+        rates is performed independently of any of the kwargs which otherwise
+        control the weight and allowed types of the error generators in this
+        model. If specifying fixed C and A rates it is possible for the final
+        error generator to be non-CP.
+        
+    seed : int, optional (default None)
+        An optional integer used in seeding the RNG.
+    
+    Returns
+    -------
+    Dictionary of error generator coefficient labels and rates 
+    """
+    
+    #Add various assertions
+    if fixed_errorgen_rates is None:
+        fixed_errorgen_rates = dict()
+    
+    if error_metric is not None:
+        assert H_params[0] == 0. and SCA_params[0] == 0., 'Specifying non-zero HSCA means together with a target error metric is not supported.'
+        if error_metric not in ('generator_infidelity', 'total_generator_error'):
+            raise ValueError('Unsupported error metric type. Currently supported options are generator_infidelity and total_generator_error')
+        #Add a check that the desired error metric value is attainable given the values of fixed_errorgen_rates.
+        if fixed_errorgen_rates:
+            #verify that all of the keys are GlobalElementaryErrorgenLabel objects.
+            msg = 'All keys of fixed_errorgen_rates must be GlobalElementaryErrorgenLabel.'
+            assert all([isinstance(key, _GlobalElementaryErrorgenLabel) for key in fixed_errorgen_rates.keys()]), msg
+            
+            #get the H and S rates from the dictionary.
+            fixed_H_rates = _np.array([val for key, val in fixed_errorgen_rates.items() if key.errorgen_type == 'H'])
+            fixed_S_rates = _np.array([val for key, val in fixed_errorgen_rates.items() if key.errorgen_type == 'S'])
+            fixed_S_contribution = _np.sum(fixed_S_rates)
+            if error_metric == 'generator_infidelity':
+                fixed_H_contribution = _np.sum(fixed_H_rates**2)
+                fixed_error_metric_value = fixed_S_contribution + fixed_H_contribution
+            elif error_metric == 'total_generator_error':
+                fixed_H_contribution = _np.sum(_np.abs(fixed_H_rates))
+                fixed_error_metric_value = fixed_S_contribution + fixed_H_contribution
+            msg = f'Incompatible values of error_metric_value and fixed_errorgen_rates. The value of {error_metric}={error_metric_value}'\
+                  + f' is less than the value of {fixed_error_metric_value} corresponding to the given fixed_errorgen_rates_dict.'
+            assert fixed_error_metric_value < error_metric_value, msg
+            
+            if relative_HS_contribution is not None:
+                msg_H = f'Fixed H contribution to {error_metric} of {fixed_H_contribution} exceeds overall H contribution target value of {relative_HS_contribution[0]*error_metric_value}.'
+                msg_S = f'Fixed S contribution to {error_metric} of {fixed_S_contribution} exceeds overall S contribution target value of {relative_HS_contribution[1]*error_metric_value}.'
+                assert fixed_H_contribution < relative_HS_contribution[0]*error_metric_value, msg_H
+                assert fixed_S_contribution < relative_HS_contribution[1]*error_metric_value, msg_S
+        else:
+            fixed_H_contribution = 0
+            fixed_S_contribution = 0
+            
+            
+    if relative_HS_contribution is not None:
+        assert ('H' in errorgen_types and 'S' in errorgen_types), 'Invalid relative_HS_contribution, one of either H or S is not in errorgen_types.'
+        if error_metric is None:
+            _warnings.warn('The relative_HS_contribution kwarg is only utilized when error_metric is not None, the specified value is ignored otherwise.')
+        else:
+            assert abs(1-sum(relative_HS_contribution))<=1e-7, 'The relative_HS_contribution should sum to 1.'
+        
+    if max_weights is not None:
+        assert max_weights['C'] <= max_weights['S'] and max_weights['A'] <= max_weights['S'], 'The maximum weight of the C and A terms should be less than or equal to the maximum weight of S.'
+        
+    rng = _np.random.default_rng(seed)
+        
+    #create a state space with this dimension.
+    state_space = _QubitSpace.cast(num_qubits)
+    
+    #create an error generator basis according the our weight specs
+    errorgen_basis = _bo.CompleteElementaryErrorgenBasis('pp', state_space, elementary_errorgen_types=errorgen_types,
+                                                     max_weights=max_weights, sslbl_overlap=sslbl_overlap)
+    
+    #Get the labels, broken out by sector, of each of the error generators in this basis.
+    errgen_labels_H = errorgen_basis.sublabels('H')
+    errgen_labels_S = errorgen_basis.sublabels('S')
+    errgen_labels_C = errorgen_basis.sublabels('C')
+    errgen_labels_A = errorgen_basis.sublabels('A')
+    
+    #Get the number of H and S error generators. These are stored in HSCA order in the labels 
+    num_H_rates = len(errgen_labels_H)
+    num_S_rates = len(errgen_labels_S)
+    
+    random_rates_dicts = dict()
+    #Generate random H rates
+    random_rates_dicts['H'] = {lbl: val for lbl,val in zip(errgen_labels_H, rng.normal(loc=H_params[0], scale=H_params[1], size = num_H_rates))}
+    
+    #Create a random matrix with complex gaussian entries which will be used to generator a PSD matrix for the SCA rates.
+    random_SCA_gen_mat = rng.normal(loc=SCA_params[0], scale=SCA_params[1], size=(num_S_rates, num_S_rates)) + \
+                         1j* rng.normal(loc=SCA_params[0], scale=SCA_params[1], size=(num_S_rates, num_S_rates))
+    random_SCA_mat = random_SCA_gen_mat @ random_SCA_gen_mat.conj().T
+    #The random S rates are just the diagonal of random_SCA_mat.
+    random_rates_dicts['S'] = {lbl: val for lbl,val in zip(errgen_labels_S,  _np.real(_np.diag(random_SCA_mat)).copy())}
+    
+    #The random C rates are the real part of the off diagonal entries, and the A rates the imaginary part.
+    random_rates_dicts['C'] =  {lbl: val for lbl,val in zip(errgen_labels_C, random_SCA_mat[_np.triu_indices_from(random_SCA_mat, k=1)].real)}
+    random_rates_dicts['A'] =  {lbl: val for lbl,val in zip(errgen_labels_A, random_SCA_mat[_np.triu_indices_from(random_SCA_mat, k=1)].imag)}
+    
+    #Add in/override the fixed rates for each of the sectors.
+    H_fixed_keys = []
+    S_fixed_keys = []
+    C_fixed_keys = []
+    A_fixed_keys = []
+    for key in fixed_errorgen_rates:
+        if key.errorgen_type == 'H':
+            H_fixed_keys.append(key)
+        elif key.errorgen_type == 'S':
+            S_fixed_keys.append(key)
+        elif key.errorgen_type == 'C':
+            C_fixed_keys.append(key)
+        else:
+            A_fixed_keys.append(key)
+          
+    random_rates_dicts['H'].update({key:fixed_errorgen_rates[key] for key in H_fixed_keys})
+    random_rates_dicts['S'].update({key:fixed_errorgen_rates[key] for key in S_fixed_keys})
+    random_rates_dicts['C'].update({key:fixed_errorgen_rates[key] for key in C_fixed_keys})
+    random_rates_dicts['A'].update({key:fixed_errorgen_rates[key] for key in A_fixed_keys})
+    
+    #For each sector construct a complementary structure of the free(ish) parameters error generator parameters for
+    #that sector.
+    H_free_keys = [key for key in errgen_labels_H if key not in fixed_errorgen_rates] #membership checking is (often) faster with dicts
+    S_free_keys = [key for key in errgen_labels_S if key not in fixed_errorgen_rates]
+    C_free_keys = [key for key in errgen_labels_C if key not in fixed_errorgen_rates]
+    A_free_keys = [key for key in errgen_labels_A if key not in fixed_errorgen_rates]
+    
+    #Now it is time to apply the various normalizations necessary to get the desired target
+    #generator infidelity and sector weights.
+    if error_metric is not None:
+        #Get the free parameter's  For both generator infidelity we use the sum of the S rates
+        current_S_sum_free = _np.sum([random_rates_dicts['S'][key] for key in S_free_keys])
+        if error_metric == 'generator_infidelity':
+            #for generator infidelity we use the sum of the squared H rates.
+            current_H_sum_free = _np.sum([random_rates_dicts['H'][key]**2 for key in H_free_keys])
+        elif error_metric == 'total_generator_error':
+            #for total generator error we use the sum of the H rates directly.
+            current_H_sum_free = _np.sum([abs(random_rates_dicts['H'][key]) for key in H_free_keys])
+        
+        total_H_sum = current_H_sum_free + fixed_H_contribution
+        total_S_sum = current_S_sum_free + fixed_S_contribution
+        
+        if relative_HS_contribution is not None:
+            #calculate the target values of the H and S contributions to the error metric 
+            #given the specified contributions
+            req_H_sum = relative_HS_contribution[0]*error_metric_value
+            req_S_sum = relative_HS_contribution[1]*error_metric_value
+                        
+        #If we haven't specified a relative contribution for H and S then we will scale these
+        #to give the correct generator infidelity while preserving whatever relative contribution
+        #to the generator infidelity they were randomly sampled to have.
+        else:
+            #Get the current relative contributions.
+            current_H_contribution = total_H_sum/(total_H_sum+total_S_sum)
+            current_S_contribution = 1-current_H_contribution
+            req_H_sum = current_H_contribution*error_metric_value
+            req_S_sum = current_S_contribution*error_metric_value
+            
+        #this is how much we still need to be contributed by the free parameters
+        needed_H_free = req_H_sum - fixed_H_contribution
+        needed_S_free = req_S_sum - fixed_S_contribution
+        
+        if error_metric == 'generator_infidelity':
+            #The scale factor for the H rates is sqrt(req_squared_H_sum/current_squared_H_sum)
+            H_scale_factor = _np.sqrt(needed_H_free/current_H_sum_free)
+        elif error_metric == 'total_generator_error':
+            #The scale factor for the S rates is req_S_sum/current_S_sum
+            H_scale_factor = needed_H_free/current_H_sum_free    
+        #The scale factor for the S rates is req_S_sum/current_S_sum
+        S_scale_factor = needed_S_free/current_S_sum_free    
+
+        #Rescale the free random rates, note that the free SCA terms will all be scaled by the S_scale_factor
+        #to preserve PSD.
+        for key in H_free_keys:
+            random_rates_dicts['H'][key]*=H_scale_factor
+        for key in S_free_keys:
+            random_rates_dicts['S'][key]*=S_scale_factor
+        for key in C_free_keys:
+            random_rates_dicts['C'][key]*=S_scale_factor
+        for key in A_free_keys:
+            random_rates_dicts['A'][key]*=S_scale_factor
+                
+    #Now turn this into a rates dict
+    errorgen_rates_dict = dict()
+    for errgen_type in errorgen_types:
+        errorgen_rates_dict.update(random_rates_dicts[errgen_type])
+    
+    return errorgen_rates_dict
+
