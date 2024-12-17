5959a5325dad41544614f47cc788b893e813dced
Riley Murray <rjmurr@sandia.gov>
2024-12-17 15:18:11 -0500

    move simple MPI test from test/test_packages into test/unit, and rename a file in test/performance so that it doesnt get discovered by testing frameworks like pytest

--- test/unit/mpi/test_mpi.py
+++ test/unit/mpi/test_mpi.py
@@ -1,16 +1,22 @@
 import subprocess
 import pytest
+import os
+from pathlib import Path
+
 try:
     from mpi4py import MPI
 except ImportError:
     MPI = None
 
-
 class MPITester:
 
     @pytest.mark.skipif(MPI is None, reason="mpi4py is not installed.")
     def test_all(self, capfd: pytest.LogCaptureFixture):
-        result = subprocess.run("mpiexec -np 4 python -W ignore core_test_mpi.py".split(' '), capture_output=True, text=True)
+        current_filepath = Path(os.path.abspath(__file__))
+        to_run = current_filepath.parents[0] / Path('run_me_with_mpiexec.py')
+        subprocess_args = (f"mpiexec -np 4 python -W ignore {str(to_run)}").split(' ')
+        
+        result = subprocess.run(subprocess_args, capture_output=False, text=True)
         out, err = capfd.readouterr()
         if len(out) + len(err) > 0:
             msg = out + '\n'+ 80*'-' + err
