e9eabd9f48814e6df36df8c533ea355f9e3b4bd7
Stefan K. Seritan <sserita@sandia.gov>
2025-01-16 09:28:59 -0800

    Catch one more MPI beta test failure.

--- test/test_packages/extras/test_interpygate.py
+++ test/test_packages/extras/test_interpygate.py
@@ -12,7 +12,7 @@ try:
     _comm = MPI.COMM_WORLD
     _rank = _comm.Get_rank()
     _size = _comm.Get_size()
-except ImportError:
+except (ImportError, RuntimeError):
     _comm = None
     _rank = 0
     _size = 1
