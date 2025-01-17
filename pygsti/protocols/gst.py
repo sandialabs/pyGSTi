9abc7586fbb7ddfe37169651a63cfe4095dcd469
Riley Murray <rjmurr@sandia.gov>
2025-01-17 12:20:50 -0500

    add comments

--- pygsti/protocols/gst.py
+++ pygsti/protocols/gst.py
@@ -2091,6 +2091,17 @@ def _add_gauge_opt(results, base_est_label, gaugeopt_suite_or_gsdict, starting_m
     printer = _baseobjs.VerbosityPrinter.create_printer(verbosity, comm)
 
     #Get gauge optimization dictionary
+    #
+    #   Riley note: it helped me write the leakage experiment notebooks
+    #   if I expanded the role of the "gaugeopt_suite" argument to 
+    #   "gaugeopt_suite_or_gsdict". The expansion seemed reasonable
+    #   in light of the docstring description for gaugeopt_suite.
+    #   But, at the end of the day, it's probably better for me to go
+    #   back and edit those notebooks to avoid calling what's clearly
+    #   a private function. While I'm at it I should update the docstring
+    #   for this function (which incorrectly states that gaugeopt_suite
+    #   can be anything castable to a GaugeoptSuite).
+    #
     if isinstance(gaugeopt_suite_or_gsdict, dict):
         gaugeopt_suite_dict = gaugeopt_suite_or_gsdict
     else:
