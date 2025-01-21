56242ee2a7543e34176a69ba0048ec567e4b2434
Riley Murray <rjmurr@sandia.gov>
2025-01-21 08:38:18 -0500

    style

--- pygsti/protocols/crosstalkfreeexperimentdesign.py
+++ pygsti/protocols/crosstalkfreeexperimentdesign.py
@@ -1,6 +1,7 @@
 import numpy as np
-from pygsti.protocols import CircuitListsDesign, CombinedExperimentDesign, HasProcessorSpec
+from pygsti.protocols import CircuitListsDesign, HasProcessorSpec
 import copy
+
 
 def find_neighbors(vertices: list, edges: list) -> dict:
     """
@@ -22,73 +23,6 @@ def find_neighbors(vertices: list, edges: list) -> dict:
     for e in edges:
         neighbors[e[0]].append(e[1])
     return neighbors
-
-# # NOTE: Ignore this function. I'm pretty sure it is not needed.
-# def generate_edge_colorings(vertices: list, edges: list) -> list:
-#     """
-#     Generate a set of edge colorings for a graph until all edges are colored.
-
-#     This function takes an edge set of a simple undirected graph and repeatedly 
-#     applies the Misra & Gries edge coloring algorithm until every edge is 
-#     contained in some edge coloring. It returns a dictionary mapping colors 
-#     to the edges colored with that color.
-
-#     Parameters:
-#     vertices (list): A list of vertices in the graph.
-#     edges (list): A list of edges represented as tuples (u, v) where u and v 
-#                   are vertices in the graph.
-
-#     Returns:
-#     list: A list of edge colorings (dictionaries whose keys are colors and items are lists colored edges)
-#     """
-#     list_of_edge_colorings = []
-#     uncolored_edges = set(edges)
-
-#     while uncolored_edges:
-#         # Determine which vertices are neighbors in a graph with only uncolored edges
-#         # Could call find_neighbors here...
-#         updated_neighbors = {v: [] for v in vertices}
-#         for u, v in uncolored_edges:
-#             updated_neighbors[u].append(v)
-
-#         # Calculate the maximum degree of the graph
-#         deg = max(len(updated_neighbors[v]) for v in vertices)
-
-#         # Find an edge coloring
-#         new_color_patches = find_edge_coloring(deg, vertices, list(uncolored_edges), updated_neighbors)
-
-#         # Update color patches and remove newly colored edges from uncolored_edges
-#         list_of_edge_colorings.append(new_color_patches)
-#         for _, edge_list in new_color_patches.items():
-#             uncolored_edges.difference_update(edge_list)
-#             uncolored_edges.difference_update([(v,u) for u, v in edge_list]) # need to symmetrize
-
-#     return list_of_edge_colorings
-
-# # NOTE: This class is superfluous. Keeping it around in case I realize that it isn't - Daniel H.
-# class CrosstalkFreeCombinedExperimentDesign(CombinedExperimentDesign, HasProcessorSpec):
-#     def __init__(self, processor_spec, oneq_gstdesign, twoq_gstdesign, seed = None, interleave = False):
-        
-#         HasProcessorSpec.__init__(self, processor_spec)
-        
-#         randstate = np.random.RandomState(seed)
-#         self.interleave = interleave
-#         self.oneq_gstdesign = oneq_gstdesign
-#         self.twoq_gstdesign = twoq_gstdesign
-#         self.vertices = self.processor_spec.qubit_labels
-#         self.edges = self.processor_spec.compute_2Q_connectivity().edges()
-#         self.neighbors = find_neighbors(self.vertices, self.edges)
-#         self.deg = max([len(self.neighbors[v]) for v in self.vertices])
-
-
-#         # Generate the sub-experiment designs
-#         self.edge_colorings = generate_edge_colorings(self.vertices, self.edges)
-#         self.sub_designs = [CrosstalkFreeSubExperimentDesign(self.processor_spec, 
-#                                                             self.oneq_gstdesign,
-#                                                             self.twoq_gstdesign,
-#                                                             edge_coloring,
-#                                                             randstate) for edge_coloring in self.edge_colorings]
-#         CombinedExperimentDesign.__init__(self, sub_designs = self.sub_designs, qubit_labels = self.vertices, interleave = self.interleave)
 
 
 def stitch_circuits_by_germ_power_only(color_patches: dict, vertices: list, 
@@ -185,6 +119,7 @@ def stitch_circuits_by_germ_power_only(color_patches: dict, vertices: list,
 
     return circuit_lists, aux_info
 
+
 class CrosstalkFreeExperimentDesign(CircuitListsDesign, HasProcessorSpec):
     '''
     This class initializes a crosstalk-free GST experiment design by combining 
@@ -229,10 +164,10 @@ class CrosstalkFreeExperimentDesign(CircuitListsDesign, HasProcessorSpec):
         
         CircuitListsDesign.__init__(self, self.circuit_lists, qubit_labels=self.vertices)
 
+
 '''
 Everything below is used to find an edge coloring of a graph.
 '''
-import copy
 
 def order(u, v):
     """
@@ -249,9 +184,8 @@ def order(u, v):
     Returns:
     tuple: A tuple containing the values `u` and `v` in sorted order.
     """
-    a = [u, v]
-    a.sort()
-    return tuple(a)
+    return (min(u, v), max(u, v))
+
 
 def find_fan_candidates(fan: list, u: int, vertices: list, edge_colors: dict, free_colors: dict) -> list:
     '''
@@ -275,6 +209,7 @@ def find_fan_candidates(fan: list, u: int, vertices: list, edge_colors: dict, fr
     last_vertex = fan[-1]
     free_vertex_colors = free_colors[last_vertex]
     return [v for v in vertices if edge_colors[(u, v)] in free_vertex_colors]
+
 
 def build_maximal_fan(u: int, v: int, vertex_neighbors: dict, 
                       free_colors: dict, edge_colors: dict) -> list:
@@ -306,6 +241,7 @@ def build_maximal_fan(u: int, v: int, vertex_neighbors: dict,
         candidate_vertices = find_fan_candidates(fan, u, u_neighbors, edge_colors, free_colors)
     return fan
 
+
 def find_next_path_vertex(current_vertex: int, color: int, neighbors: dict, edge_colors: dict):
     '''
     Finds, if it exists, the next vertex in a cd_u path. It does so by finding the neighbor 
@@ -327,6 +263,7 @@ def find_next_path_vertex(current_vertex: int, color: int, neighbors: dict, edge
             return vertex
     return None
     
+
 def find_color_path(u: int, v: int, c: int, d: int, neighbors: dict, edge_colors: dict) -> list:
     '''
     Finds the cd_u path.
@@ -359,6 +296,7 @@ def find_color_path(u: int, v: int, c: int, d: int, neighbors: dict, edge_colors
         next_vertex = find_next_path_vertex(current_vertex, current_color, neighbors, edge_colors)
     return cdu_path
 
+
 def rotate_fan(fan: list, u: int, edge_colors: dict, free_colors: dict, color_patches: dict):
     '''
     Rotate the colors in a fan of vertices connected to a specified vertex.
@@ -380,19 +318,19 @@ def rotate_fan(fan: list, u: int, edge_colors: dict, free_colors: dict, color_pa
     tuple: Updated dictionaries for edge_colors, free_colors, and color_patches after rotation.
     '''
     for i in range(len(fan) - 1):
-        current_vertex = fan[i]
+        curr_vertex = fan[i]
         next_vertex = fan[i+1]
         next_color = edge_colors[(u, next_vertex)]
         
-        edge_colors[(u, current_vertex)] = next_color
-        edge_colors[(current_vertex, u)] = next_color
+        edge_colors[(u, curr_vertex)] = next_color
+        edge_colors[(curr_vertex, u)] = next_color
         edge_colors[(u, next_vertex)] = -1
         edge_colors[(next_vertex, u)] = -1
 
-        free_colors[current_vertex].remove(next_color)
+        free_colors[curr_vertex].remove(next_color)
         free_colors[next_vertex].append(next_color)
 
-        color_patches[next_color].append(order(u, current_vertex))
+        color_patches[next_color].append(order(u, curr_vertex))
         color_patches[next_color].remove(order(u, next_vertex))
 
     return edge_colors, free_colors, color_patches
