19213061c1815d85faba34210aa532df9a0e7c49
Jeremiah Hauth <jmhauth@hopper2.eth>
2025-02-03 09:59:15 -0800

    cpu version debug

--- pygsti/extras/ml/neuralnets_cpu_optimization.py
+++ pygsti/extras/ml/neuralnets_cpu_optimization.py
@@ -18,7 +18,7 @@ import copy as _copy
 class CircuitErrorVec(_keras.Model):
     def __init__(self, num_qubits: int, num_channels: int, tracked_error_gens: list, 
                  layer_snipper, layer_snipper_args: list,
-                 dense_units=[30, 20, 10, 5, 5, 132], input_shape=None, **kwargs):
+                 dense_units=[30, 20, 10, 5, 5], input_shape=None, **kwargs):
         """
         num_qubits: int
             The number of qubits that this neural network models.
@@ -39,8 +39,6 @@ class CircuitErrorVec(_keras.Model):
 
         
         dense_units: list
-            Make sure list ends in num_tracked_error_gens
-        
         """
         super().__init__()
         self.num_qubits = num_qubits
@@ -52,7 +50,6 @@ class CircuitErrorVec(_keras.Model):
         self.layer_snipper = layer_snipper
         self.layer_snipper_args = layer_snipper_args
         self.input_layer = _keras.layers.InputLayer(shape=input_shape)
-        self.local_dense = LocalizedDenseToErrVec(self.layer_snipper, self.layer_snipper_args, self.tracked_error_gens, self.dense_units)
         self.hamiltonian_mask = tf.constant([1 if error[0] == 'H' else 0 for error in tracked_error_gens], tf.int32)
         self.stochastic_mask = tf.constant([1 if error[0] == 'S' else 0 for error in tracked_error_gens], tf.int32)
 
@@ -67,15 +64,21 @@ class CircuitErrorVec(_keras.Model):
             'layer_snipper_args': self.layer_snipper_args,
         })
         return config
+    
+    def build(self):
+        self.local_dense = LocalizedDenseToErrVec(self.layer_snipper, self.layer_snipper_args, self.tracked_error_gens, self.dense_units)
 
     # @tf.function
     def calc_masked_err_rates(self, signed_M, P, mask):
         masked_M = tf.math.multiply(tf.cast(mask, tf.float32), signed_M)
+        # print(mask.dtype, P.dtype)
         masked_P = tf.math.multiply(mask, P)
         flat_masked_M, flat_masked_P = tf.reshape(masked_M, [-1]), tf.reshape(masked_P, [-1])
         unique_masked_P, idx = tf.unique(flat_masked_P) # will come back to this. Possibly move to a model input rather than an intermediate calculation. Takes lots of time...
         num_segments = tf.reduce_max(idx) + 1
+        # print(tf.math.unsorted_segment_sum(flat_masked_M, idx, num_segments).shape)
         return tf.math.unsorted_segment_sum(flat_masked_M, idx, num_segments)
+
 
     # @tf.function
     def calc_end_of_circ_error(self, M, P, S):
@@ -83,24 +86,31 @@ class CircuitErrorVec(_keras.Model):
         A function that maps the error rates (M) to an end-of-circuit error generator
         using the permutation matrix P.
         """
-        signed_M = tf.math.multiply(S, M)
+        # print('S, M', S.shape, M.shape)
+        signed_M = tf.math.multiply(S, M) # mult alpha by M, assuming correct organization of alpha
         final_stochastic_error_rates = self.calc_masked_err_rates(signed_M, P, self.stochastic_mask)
         final_hamiltonian_error_rates = self.calc_masked_err_rates(signed_M, P, self.hamiltonian_mask)
+        # print(final_stochastic_error_rates.shape, final_hamiltonian_error_rates.shape)
         return tf.reduce_sum(final_stochastic_error_rates) + tf.reduce_sum(tf.square(final_hamiltonian_error_rates))
     
     # @tf.function
-    def circuit_to_fidelity(self, input):
+    def circuit_to_fidelity(self, input): # will replace this with circ to probability dist
         """
         A function that maps a single circuit to the prediction for its process fidelity (a single real number).
         """
+        # print('l', input)
+        # C, P, S = input
+
+        # print(C.shape, P.shape, S.shape)
         C = input[:, 0:self.len_gate_encoding]
         P = tf.cast(input[:, self.len_gate_encoding:self.len_gate_encoding + self.num_tracked_error_gens], tf.int32)
         S = input[:, self.len_gate_encoding + self.num_tracked_error_gens:self.len_gate_encoding + 2 * self.num_tracked_error_gens]
-        M = self.local_dense(C)  # Directly pass C to local_dense
-        # print('i Input, C, P, S, M', input.shape, C.shape, P.shape, S.shape, M.shape)
+        
+        M = self.local_dense(C)  # 
+        
         return self.calc_end_of_circ_error(M, P, S)
 
-    @tf.function
+    # @tf.function
     def call(self, inputs):
         return tf.map_fn(self.circuit_to_fidelity, inputs) # will try to further optimize this with batching rather than slow map_fn / vectorize_map
         
@@ -138,7 +148,7 @@ class DenseSubNetwork(_keras.layers.Layer):
 
 # @_keras.utils.register_keras_serializable()
 class LocalizedDenseToErrVec(_keras.layers.Layer):
-    def __init__(self, layer_snipper, layer_snipper_args, tracked_error_gens, dense_units = [30, 20, 10, 5, 5, 132], **kwargs):
+    def __init__(self, layer_snipper, layer_snipper_args, tracked_error_gens, dense_units = [30, 20, 10, 5, 5], **kwargs):
         """
         layer_snipper: func
             A function that takes a primitive error generator and maps it to a list that encodes which parts
@@ -153,7 +163,7 @@ class LocalizedDenseToErrVec(_keras.layers.Layer):
         self.num_tracked_error_gens = len(tracked_error_gens)  # This is the output dimenision of the network
         self.tracked_error_gens = tracked_error_gens  
         self.layer_encoding_indices_for_error_gen = [layer_snipper(error_gen, *layer_snipper_args) for error_gen in tracked_error_gens]
-        self.dense_units = dense_units
+        self.dense_units = dense_units + [self.num_tracked_error_gens]
         self.layer_snipper = layer_snipper
         self.layer_snipper_args = layer_snipper_args
 
@@ -183,19 +193,20 @@ class LocalizedDenseToErrVec(_keras.layers.Layer):
     def call(self, inputs):
         # Convert the list of indices to a tensor
         indices_tensor = tf.stack(self.layer_encoding_indices_for_error_gen)
+        
         # Expand dimensions to match the batch size
         batch_size = tf.shape(inputs)[0]
-        indices_tensor = tf.tile(tf.expand_dims(indices_tensor, 0), [batch_size, 1, 1])
+        indices_tiled = tf.tile(tf.expand_dims(indices_tensor, 0), [batch_size, 1, 1])
 
         # Gather the values based on the indices
-        gathered_slices = tf.gather(inputs, indices_tensor, batch_dims=1)
+        gathered_slices = tf.gather(inputs, indices_tiled, batch_dims=1)
 
         # Reshape the gathered slices to concatenate along the last axis
         gathered_slices = tf.reshape(gathered_slices, [batch_size, -1])
-
+        
         # Pass the concatenated slices through the dense layer
         x = self.dense(gathered_slices)
-        
+
         return x
 
 # @_keras.utils.register_keras_serializable()
