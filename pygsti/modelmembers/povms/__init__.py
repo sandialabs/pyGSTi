c90803d81ebde35b90fee32f62a316c356c6d0a1
Juan G Mendoza <jgonz19@sandia.gov>
2025-05-26 21:50:37 -0600

    compactified code for non trivial vectors

--- pygsti/modelmembers/povms/__init__.py
+++ pygsti/modelmembers/povms/__init__.py
@@ -580,10 +580,8 @@ def convert(povm, to_type, basis, ideal_povm=None, flatten_structure=False, cp_p
                     _,S,Vt = _np.linalg.svd(J)
 
                     #Only return nontrivial singular vectors
-                    non_zero_mask = _np.where(_np.abs(S) > 1e-13)
-                    non_trivial_vecs = Vt[non_zero_mask]
-                    non_trivial_vecs = non_trivial_vecs.reshape(-1, Vt.shape[1])  # Reshape to ensure it's 2D
-                    return non_trivial_vecs
+                    Vt = Vt[S > 1e-13, :].reshape((-1, Vt.shape[1]))
+                    return Vt
                     
                 
                 phys_directions = calc_physical_subspace(dense_ideal_povm)
