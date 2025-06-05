68429c1b205fbb9af992f937608523197be9077c
Jeremiah Hauth <jmhauth@login02.chn.perlmutter.nersc.gov>
2025-06-05 12:22:36 -0700

    typo

--- pygsti/extras/ml/neuralnets_cpu_bitstring_probability_map.py
+++ pygsti/extras/ml/neuralnets_cpu_bitstring_probability_map.py
@@ -267,7 +267,7 @@ def layer_snipper_from_qubit_graph_with_simplified_lookback(error_gen, num_qubit
 
     indices_for_error = []
     for i in range(0, np.abs(lookback)):
-        indices_for_error += list(encoding_indices_for_error) + (num_qubits * num_channels) * i)
+        indices_for_error += list(encoding_indices_for_error) + (num_qubits * num_channels) * i
 
     return _np.array(indices_for_error)
 
