06dbc64372e688bff3b6e57271006d2f987870ec
Nick Koskelo <koskelo2@illinois.edu>
2025-07-15 13:58:28 -0700

    Improve the circuit splitting test.

--- test/unit/objects/test_circuit_splitting.py
+++ test/unit/objects/test_circuit_splitting.py
@@ -4,7 +4,11 @@ from pygsti.circuits.split_circuits_into_lanes import compute_subcircuits, compu
 import numpy as np
 
 
-def build_circuit(num_qubits: int, depth_L: int, allowed_gates: set[str]):
+def build_circuit(num_qubits: int, depth_L: int, allowed_gates: set[str]) -> _Circuit:
+    """
+    Build a random circuit of depth L which operates on num_qubits and has the allowed
+    single qubit gates specified in allowed gates.
+    """
     my_circuit = []
     for lnum in range(depth_L):
         layer = []
@@ -12,10 +16,20 @@ def build_circuit(num_qubits: int, depth_L: int, allowed_gates: set[str]):
             gate = str(np.random.choice(allowed_gates))
             layer.append((gate, qnum))
         my_circuit.append(layer)
-    return _Circuit(my_circuit)
+    return _Circuit(my_circuit, line_labels=[i for i in range(num_qubits)])
 
 
-def build_circuit_with_multiple_qubit_gates_with_designated_lanes(num_qubits: int, depth_L: int, lane_end_points: list[int], gates_to_qubits_used: dict[str, int]):
+def build_circuit_with_multiple_qubit_gates_with_designated_lanes(
+                                num_qubits: int,
+                                depth_L: int,
+                                lane_end_points: list[int],
+                                gates_to_qubits_used: dict[str, int]) -> _Circuit:
+    """
+    Builds a circuit with a known lane structure.
+    Any two + qubit lanes can be split into smaller lanes if none of the gates
+    chosen for that lane actually operate on two or more qubits.
+    """
+
 
     assert lane_end_points[-1] <= num_qubits # if < then we have a lane from there to num_qubits.
     assert lane_end_points[0] > 0
@@ -86,7 +100,9 @@ def test_find_qubit_to_lane_splitting():
 
     lane_eps = [1, 2, 4, 5]
     # So expected lane dist is (0, ), (1), (2,3), (4,), (5,)
+    minimum_num_lanes = 5
 
+    # This is a random circuit so the lanes may not be perfect.
     circuit = build_circuit_with_multiple_qubit_gates_with_designated_lanes(num_qubits, depth, lane_eps, gates_to_num_used)
 
     qubit_to_lane, lane_to_qubits = compute_qubit_to_lane_and_lane_to_qubits_mappings_for_circuit(circuit)
@@ -94,8 +110,14 @@ def test_find_qubit_to_lane_splitting():
 
     assert len(qubit_to_lane) == num_qubits
 
+    assert len(lane_to_qubits) >= minimum_num_lanes
     assert len(lane_to_qubits) <= num_qubits
 
-    circuit = _Circuit([[]])
+    for qubit in qubit_to_lane:
+        assert qubit_to_lane[qubit] in lane_to_qubits
 
-test_subcircuits_splits_can_create_empty_sub_circuit()
+
+    for lane in lane_to_qubits:
+        for qu in lane_to_qubits[lane]:
+            assert qu in qubit_to_lane
+            assert lane == qubit_to_lane[qu]
