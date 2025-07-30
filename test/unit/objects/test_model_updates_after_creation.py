c692cf8763b778ed73cce6bd0719fecd0aaecf8f
Nick Koskelo <koskelo2@illinois.edu>
2025-07-30 15:30:14 -0700

    Update test case to use unittest framework.

--- test/unit/objects/test_model_updates_after_creation.py
+++ test/unit/objects/test_model_updates_after_creation.py
@@ -1,3 +1,5 @@
+
+from typing import Literal
 import numpy as np
 from pygsti.baseobjs import qubitgraph as _qgraph
 from pygsti.baseobjs import QubitSpace
@@ -10,6 +12,9 @@ from pygsti.baseobjs.errorgenbasis import CompleteElementaryErrorgenBasis
 from pygsti.tools import slicetools as _slct
 from pygsti.modelmembers.operations import ComposedOp, EmbeddedOp
 from pygsti.algorithms import BuiltinBasis
+from pygsti.modelmembers.operations import create_from_unitary_mx
+
+from ..util import BaseCase
 
 #region Create Model
 
@@ -17,12 +22,12 @@ from pygsti.algorithms import BuiltinBasis
 def make_spam(num_qubits):
     state_space = QubitSpace(num_qubits)
     max_weights = {'H': 1, 'S': 1, 'C': 1, 'A': 1}
-    egbn_H_only = CompleteElementaryErrorgenBasis(BuiltinBasis("PP", 4),
-                                                  state_space,
-                                                  ('H', ),
-                                                  max_weights)
+    egbn_hamiltonian_only = CompleteElementaryErrorgenBasis(BuiltinBasis("PP", 4),
+                                                            state_space,
+                                                            ('H', ),
+                                                            max_weights)
 
-    rho_errgen_rates = {ell: 0.0 for ell in egbn_H_only.labels}
+    rho_errgen_rates = {ell: 0.0 for ell in egbn_hamiltonian_only.labels}
     rho_lindblad = LindbladErrorgen.from_elementary_errorgens(rho_errgen_rates,
                                                               parameterization='H',
                                                               state_space=state_space,
@@ -31,15 +36,15 @@ def make_spam(num_qubits):
     rho_ideal = ComputationalBasisState([0] * num_qubits)
     rho = ComposedState(rho_ideal, rho_errorgen)
 
-    M_errgen_rates = {ell: 0.0 for ell in egbn_H_only.labels}
-    M_lindblad = LindbladErrorgen.from_elementary_errorgens(M_errgen_rates,
-                                                            parameterization='H',
-                                                            state_space=state_space,
-                                                            evotype='densitymx')
-    M_errorgen = ExpErrorgenOp(M_lindblad)
-    M = ComposedPOVM(M_errorgen)
+    povm_errgen_rates = {ell: 0.0 for ell in egbn_hamiltonian_only.labels}
+    povm_linblad = LindbladErrorgen.from_elementary_errorgens(povm_errgen_rates,
+                                                              parameterization='H',
+                                                              state_space=state_space,
+                                                              evotype='densitymx')
 
-    return rho, M
+    measure = ComposedPOVM(ExpErrorgenOp(povm_linblad))
+
+    return rho, measure
 
 
 def make_target_model(num_qubits, independent_gates: bool = True):
@@ -60,7 +65,7 @@ def make_target_model(num_qubits, independent_gates: bool = True):
         nonstd_gate_unitaries={'Gecr': u_ecr},
         geometry=ps_geometry
     )
-    gateerrs = dict()
+    gateerrs = {}
     basis = BuiltinBasis("PP", QubitSpace(1))
     egb1 = CompleteElementaryErrorgenBasis(basis, QubitSpace(1), ('H', 'S'))
     for gn in gatenames[:-1]:
@@ -74,99 +79,103 @@ def make_target_model(num_qubits, independent_gates: bool = True):
 #endregion Create Model
 
 
-#region Test Cases
-def test_add_state_prep_after_creation_of_implicit_noisy_model():
-    num_qubits = 2
-    model = make_target_model(num_qubits)
+class TestRebuildParamVec(BaseCase):
 
-    paramlbls = model._paramlbls.copy()
-    rho, _ = make_spam(num_qubits)
-    model.prep_blks['layers']['rho0'] = rho
-    model._rebuild_paramvec()
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.num_qubits = 2
 
-    out_labels = model._paramlbls.copy()
+    def setup_model_for_test(self, where: Literal["op", "sp", "povm"]):
 
-    inds = _slct.indices_as_array(rho._gpindices)
-    avail_inds = np.arange(model.num_params)
-    cross_check = np.where(avail_inds[:, None] != inds[None, :], 1, 0)
+        model = make_target_model(self.num_qubits)
 
-    totals = np.sum(cross_check, axis=1)
-    assert len(totals) == model.num_params
+        paramlbls = model._paramlbls.copy()
+        obj_of_interest = None
+        if where in ["povm", "sp"]:
+            rho, povm = make_spam(self.num_qubits)
+            if where == "sp":
+                model.prep_blks['layers']['rho0'] = rho
+                obj_of_interest = rho
+            else:
+                model.povm_blks['layers']['Mdefault'] = povm
+                obj_of_interest = povm
 
-    used_inds = np.where(totals == len(inds))
-    assert all(paramlbls == out_labels[used_inds])
+        elif where == "op":
+            gate_name = "Gypi2"
+            egb1 = CompleteElementaryErrorgenBasis(BuiltinBasis("PP", QubitSpace(1)),
+                                                   QubitSpace(1), ('H', 'S'))
 
+            new_gate_errgen_rates = {ell: 0.0 for ell in egb1.labels}
+            my_lindbladian = LindbladErrorgen.from_elementary_errorgens(new_gate_errgen_rates,
+                                                                        parameterization='auto',
+                                                                        state_space=QubitSpace(1),
+                                                                        evotype='densitymx')
 
-def test_add_povm_after_creation_of_implicit_noisy_model():
+            comp = ComposedOp([create_from_unitary_mx(np.eye(2), "static standard",
+                                                      stdname=gate_name),
+                               ExpErrorgenOp(my_lindbladian)])
+            comp1 = comp.copy()
 
-    num_qubits = 2
-    model = make_target_model(num_qubits)
+            model.operation_blks["gates"][(gate_name, 0)] = comp
+            model.operation_blks["gates"][(gate_name, 1)] = comp1
 
-    paramlbls = model._paramlbls.copy()
-    _, M = make_spam(num_qubits)
-    model.povm_blks['layers']['Mdefault'] = M
-    model._rebuild_paramvec()
+            # Add the embedded op in a layer as well.
 
-    out_labels = model._paramlbls.copy()
+            embedded = EmbeddedOp(QubitSpace(self.num_qubits), [0], comp)
+            embedded1 = EmbeddedOp(QubitSpace(self.num_qubits), [1], comp1)
+            model.operation_blks["layers"][(gate_name, 0)] = embedded
+            model.operation_blks["layers"][(gate_name, 1)] = embedded1
 
-    inds = _slct.indices_as_array(M._gpindices)
-    avail_inds = np.arange(model.num_params)
-    cross_check = np.where(avail_inds[:, None] != inds[None, :], 1, 0)
+            obj_of_interest = (comp, comp1)
+        else:
+            raise ValueError("Unexpected value for the type of new layer to add to the model.")
 
-    totals = np.sum(cross_check, axis=1)
-    assert len(totals) == model.num_params
+        model._rebuild_paramvec()
 
-    used_inds = np.where(totals == len(inds))
-    assert all(paramlbls == out_labels[used_inds])
+        return paramlbls, model, model._paramlbls.copy(), obj_of_interest
 
+    def test_add_state_prep_after_creation_of_implicit_noisy_model(self):
 
-def test_add_gate_operation_after_creation_of_implicit_noisy_model():
+        original_lbls, model, modified_lbls, rho = self.setup_model_for_test("sp")
 
-    from pygsti.modelmembers.operations import create_from_unitary_mx
+        inds = _slct.indices_as_array(rho._gpindices)
+        avail_inds = np.arange(model.num_params)
+        cross_check = np.where(avail_inds[:, None] != inds[None, :], 1, 0)
 
-    dummy_unitary = np.eye(2)
-    num_qubits = 2
+        totals = np.sum(cross_check, axis=1)
+        self.assertEqual(len(totals), model.num_params)
 
-    model = make_target_model(num_qubits)
-    paramlbls = model._paramlbls.copy()
+        used_inds = np.where(totals == len(inds))
+        self.assertArraysEqual(original_lbls, modified_lbls[used_inds])
 
-    gate_name = "Gypi2"
-    basis = BuiltinBasis("PP", QubitSpace(1))
-    egb1 = CompleteElementaryErrorgenBasis(basis, QubitSpace(1), ('H', 'S'))
+    def test_add_povm_after_creation_of_implicit_noisy_model(self):
 
-    new_gate_errgen_rates = {ell: 0.0 for ell in egb1.labels}
-    my_lindbladian = LindbladErrorgen.from_elementary_errorgens(new_gate_errgen_rates,
-                                                                parameterization='auto',
-                                                                state_space=QubitSpace(1),
-                                                                evotype='densitymx')
-    my_new_op = ExpErrorgenOp(my_lindbladian)
-    standard_op = create_from_unitary_mx(dummy_unitary, "static standard", stdname=gate_name)
+        original_lbls, model, modified_lbls, povm = self.setup_model_for_test("povm")
 
-    comp = ComposedOp([standard_op, my_new_op])
-    comp1 = comp.copy()
+        inds = _slct.indices_as_array(povm._gpindices)
+        avail_inds = np.arange(model.num_params)
+        cross_check = np.where(avail_inds[:, None] != inds[None, :], 1, 0)
 
-    model.operation_blks["gates"][(gate_name, 0)] = comp
-    model.operation_blks["gates"][(gate_name, 1)] = comp1
+        totals = np.sum(cross_check, axis=1)
+        self.assertEqual(len(totals), model.num_params)
 
-    # Add the embedded op in a layer as well.
+        used_inds = np.where(totals == len(inds))
+        self.assertArraysEqual(original_lbls, modified_lbls[used_inds])
 
-    embedded = EmbeddedOp(QubitSpace(num_qubits), [0], comp)
-    embedded1 = EmbeddedOp(QubitSpace(num_qubits), [1], comp1)
-    model.operation_blks["layers"][(gate_name, 0)] = embedded
-    model.operation_blks["layers"][(gate_name, 1)] = embedded1
+    def test_add_gate_operation_after_creation_of_implicit_noisy_model(self):
 
-    model._rebuild_paramvec()
+        original_lbls, model, modified_lbls, (comp, comp1) = self.setup_model_for_test("op")
 
-    out_labels = model._paramlbls.copy()
+        excluded_inds = np.array(list(_slct.to_array(comp.gpindices)) + list(_slct.to_array(comp1.gpindices)))
+        avail_inds = np.arange(model.num_params)
+        cross_check = np.where(avail_inds[:, None] != excluded_inds[None, :], 1, 0)
+        totals = np.sum(cross_check, axis=1)
+        self.assertEqual(len(totals), model.num_params)
 
-    excluded_inds = np.array(list(_slct.to_array(comp.gpindices)) + list(_slct.to_array(comp1.gpindices)))
-    avail_inds = np.arange(model.num_params)
-    cross_check = np.where(avail_inds[:, None] != excluded_inds[None, :], 1, 0)
-    totals = np.sum(cross_check, axis=1)
-    assert len(totals) == model.num_params
+        norm = np.linalg.norm(comp.gpindices_as_array() - comp1.gpindices_as_array(), 2)
+        tol = 2**-53  # episilon machine for double precision.
+        msg = "Composed Op copy is treated as the same object as the original in creation of gpindices."
+        self.assertGreater(norm, tol, msg)
 
-    assert not np.allclose(comp.gpindices_as_array(), comp1.gpindices_as_array())
-    used_inds = np.where(totals == len(excluded_inds))
-
-    assert np.all(paramlbls == out_labels[used_inds])
-#endregion Test Cases
+        used_inds = np.where(totals == len(excluded_inds))
+        self.assertArraysEqual(original_lbls, modified_lbls[used_inds])
