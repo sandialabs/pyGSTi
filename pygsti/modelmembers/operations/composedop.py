b213ec460e2ca7258e258044c4a675122885e876
Corey Ostrove <ciostro@sandia.gov>
2025-08-17 20:04:12 -0600

    Errant print statements

--- pygsti/modelmembers/operations/composedop.py
+++ pygsti/modelmembers/operations/composedop.py
@@ -911,11 +911,7 @@ class ComposedOp(_LinearOperator):
                         sslbls = self.state_space.qudit_labels if isinstance(self.state_space, _statespace.QuditSpace) else self.state_space.labels[0]
                         updated_values_to_set[available_factor_coeffs_first_lbl_type.cast(key, sslbls)] = val
                 values_to_set = updated_values_to_set
-
-            print(f'{values_to_set=}')
-            print(f'{available_factor_coeffs=}')
             Ltermdict_local = {k:v for k, v in values_to_set.items() if k in available_factor_coeffs}
-            print(f'{Ltermdict_local=}')
             op.set_errorgen_coefficients(Ltermdict_local, action, logscale_nonham, truncate)
             for k in Ltermdict_local:
                 del values_to_set[k]  # remove the values that we just set
