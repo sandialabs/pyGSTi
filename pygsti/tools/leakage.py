move some functions from leakage.py to explicitmodel.py and gst.py. Fix bugs introduced in the last couple of commits.

pygsti/tools/leakage.py
@@ -24,7 +24,6 @@ from typing import Union, Dict, Optional, List, Any, TYPE_CHECKING
 if TYPE_CHECKING:
     from pygsti.protocols.gst import ModelEstimateResults, GSTGaugeOptSuite
     from pygsti.models import ExplicitOpModel
-    from pygsti.models.gaugegroup import GaugeGroupElement
 
 
 # Question: include the parenthetical in the heading below?
@@ -280,8 +279,8 @@ def subspace_superop_fro_dist(op_x, op_y, op_basis, n_leak=0):
 
 def leaky_qubit_model_from_pspec(ps_2level: QubitProcessorSpec, mx_basis: Union[str, Basis]='l2p1', levels_readout_zero=(0,)) -> ExplicitOpModel:
     """
-    Return an ExplicitOpModel `m` with three-dimensional Hilbert space, whose members are represented
-    in `mx_basis`, constructed as follows:
+    Return an ExplicitOpModel `m` whose (ideal) gates act on three-dimensional Hilbert space and whose members
+    are represented in `mx_basis`, constructed as follows:
 
         The Hermitian matrix representation of m['rho0'] is the 3-by-3 matrix with a 1 in the upper-left
         corner and all other entries equal to zero.
@@ -334,7 +333,7 @@ def leaky_qubit_model_from_pspec(ps_2level: QubitProcessorSpec, mx_basis: Union[
     E1   = np.eye(3, dtype=complex) - E0
 
     ss = ExplicitStateSpace([0],[3])
-    tm_3level = ExplicitOpModel(ss, mx_basis)
+    tm_3level = ExplicitOpModel(ss, mx_basis) # type: ignore
     tm_3level['rho0']     =  FullState(stdmx_to_vec(rho0, mx_basis))
     tm_3level['Mdefault'] =  UnconstrainedPOVM(
         [("0", stdmx_to_vec(E0, mx_basis)), ("1", stdmx_to_vec(E1, mx_basis))], evotype="default",
@@ -356,72 +355,6 @@ def leaky_qubit_model_from_pspec(ps_2level: QubitProcessorSpec, mx_basis: Union[
 
 
 # MARK: gauge optimization
-
-def transform_composed_model(mdl: ExplicitOpModel, s : GaugeGroupElement) -> ExplicitOpModel:
-    """
-    Return a copy of `mdl` whose members have been gauge-transformed by `s`,
-    while retaining the parameterization of `mdl`.
-    
-    This function's implementation requires that `mdl` use ComposedState for
-    stateprep and ComposedPOVM for measurements. It does NOT require that
-    operations be represented with ComposedOp. It ignores any factories that
-    might be present in mdl.
-    """
-    from pygsti.models import ExplicitOpModel
-    assert isinstance(mdl, ExplicitOpModel)
-    if len(mdl.factories) > 0:
-        warnings.warn('The returned model will not retain the factories in mdl.')
-    if len(mdl.instruments) > 0:
-        raise NotImplementedError('Models with instruments are not supported.')
-
-    oldmdl = mdl
-
-    def mycopy(_m):
-        # This function is a hack. It makes us robust to errors
-        # arising from copy.deepcopy in (re)linking model members
-        # to parent model objects.
-        s = _m.to_nice_serialization()
-        t = ExplicitOpModel.from_nice_serialization(s)
-        return t
-    
-    mdl = mycopy(oldmdl)
-
-    from pygsti.modelmembers.operations import ComposedOp, StaticArbitraryOp
-    from pygsti.modelmembers.povms import ComposedPOVM
-    from pygsti.modelmembers.states import ComposedState
-
-    U    = StaticArbitraryOp(s.transform_matrix,         basis=oldmdl.basis)
-    invU = StaticArbitraryOp(s.transform_matrix_inverse, basis=oldmdl.basis) 
-
-    # NOTE: the operations passed to ComposedOp are interpreted in
-    # reverse order. For example, ComposedOp([X,Y,Z]) is applied to
-    # a vector v as Z @ Y @ X @ v.
-
-    for key, rho in oldmdl.preps.items():
-        # replace each ComposedState superket `rhoVec` with `invU @ rhoVec`;
-        # do this by packing invU into a new ComposedState's error map.
-        assert isinstance(rho, ComposedState)
-        static_rho = rho.state_vec
-        errmap  = ComposedOp([rho.error_map, invU])
-        mdl.preps[key] = ComposedState(static_rho, errmap)
-
-    for key, povm in oldmdl.povms.items():
-        # replace each ComposedPOVM `p` with another ComposedPOVM `q`, where
-        # effects `Evec` belonging to `p` are mapped to effects `EVec @ U` 
-        # belonging to `q`. Do this by packing U into the error map of `q`.
-        assert isinstance(povm, ComposedPOVM)
-        static_povm = povm.base_povm
-        errmap = ComposedOp([U, povm.error_map])
-        mdl.povms[key] = ComposedPOVM(errmap, static_povm, mx_basis=oldmdl.basis)
-
-    for key, op in oldmdl.operations.items():
-        # replace each operation `G` with `invU @ G @ U`.
-        op_s = ComposedOp([U, op, invU])
-        mdl.operations[key] = op_s
-
-    mdl._clean_paramvec()  # transform may leave dirty members
-    return mdl
-
 
 def lagoified_gopparams_dicts(gopparams_dicts: List[Dict]) -> List[Dict]:
     """
@@ -473,40 +406,6 @@ def std_lago_gopsuite(model: ExplicitOpModel) -> dict[str, list[dict]]:
     return gop_params
 
 
-def add_param_preserving_gauge_opt(results: ModelEstimateResults, est_key: str, gop_params: GSTGaugeOptSuite, verbosity: int = 0):
-    """
-    This function adds one or more models to the `results[est_key].estimates` dict,
-    in a way that's similar to the _add_gauge_opt function in gst.py.
-    
-    It starts by calling _add_gauge_opt with (results, est_key, gop_params, verbosity),
-    then it extracts the gauge group elements from each step of the gauge-optimization
-    suite, computes their composition, and finally applies a parameterization-preserving
-    gauge transformation with that composition.
-    """
-    from pygsti.protocols.gst import _add_gauge_opt
-    from pygsti.models.gaugegroup import FullGaugeGroupElement
-    from pygsti.models import ExplicitOpModel
-    est = results.estimates[est_key]
-    seed_mdl = est.models['final iteration estimate']
-    seed_mdl = ExplicitOpModel.from_nice_serialization(seed_mdl._to_nice_serialization())
-    _add_gauge_opt(results, est_key, gop_params, seed_mdl, unreliable_ops=(), verbosity=verbosity)
-    # ^ That can convert to whatever parameterization it wants
-    #   It'll write to est._gaugeopt_suite.
-    for gop_name, gop_dictorlist in est._gaugeopt_suite.gaugeopt_argument_dicts.items():
-        if isinstance(gop_dictorlist, list):
-            ggel_mx = np.eye(seed_mdl.basis.dim)
-            for sub_gopdict in gop_dictorlist:
-                assert isinstance(sub_gopdict, dict)
-                assert '_gaugeGroupEl' in sub_gopdict
-                ggel_mx = ggel_mx @ sub_gopdict['_gaugeGroupEl'].transform_matrix
-            ggel = FullGaugeGroupElement(ggel_mx)
-        else:
-            ggel = gop_dictorlist['_gaugeGroupEl']
-        model_implicit_gauge = transform_composed_model(est.models['final iteration estimate'], ggel)
-        est.models[gop_name] = model_implicit_gauge
-    return
-
-
 def add_lago_models(results: ModelEstimateResults, est_key: Optional[str] = None, gos: Optional[GSTGaugeOptSuite] = None, verbosity: int = 0):
     """
     Update each estimate in results.estimates (or just results.estimates[est_key],
@@ -517,7 +416,7 @@ def add_lago_models(results: ModelEstimateResults, est_key: Optional[str] = None
     with "stdgaugeopt" suite and then changing the settings to be suitable for 
     leakage-aware gauge optimization.
     """
-    from pygsti.protocols.gst import GSTGaugeOptSuite
+    from pygsti.protocols.gst import GSTGaugeOptSuite, _add_param_preserving_gauge_opt
     if gos is None:
         existing_est  = results.estimates[est_key]
         std_gos_lods  = existing_est.goparameters['stdgaugeopt']
@@ -525,7 +424,7 @@ def add_lago_models(results: ModelEstimateResults, est_key: Optional[str] = None
         gop_params = {'LAGO': lago_gos_lods}
         gos = GSTGaugeOptSuite(gaugeopt_argument_dicts=gop_params)
     if isinstance(est_key, str):
-        add_param_preserving_gauge_opt(results, est_key, gos, verbosity)
+        _add_param_preserving_gauge_opt(results, est_key, gos, verbosity)
     elif est_key is None:
         for est_key in results.estimates.keys():
             add_lago_models(results, est_key, gos, verbosity)
@@ -533,6 +432,8 @@ def add_lago_models(results: ModelEstimateResults, est_key: Optional[str] = None
         raise ValueError()
     return
 
+
+# MARK: reports
 
 def construct_leakage_report(
         results : ModelEstimateResults,