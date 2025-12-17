0f2c5e2426d30be7467126df6c4a306d2360288a
Timothy Proctor <tjproct@sandia.gov>
2025-12-17 15:06:47 -0600

    Have finally confirmed that the  convenient I think is correct does give better QPANN performance

--- pygsti/extras/ml/snippers.py
+++ pygsti/extras/ml/snippers.py
@@ -39,67 +39,30 @@ def layer_snipper_from_qubit_graph(error_generators, encoder, adjacency_matrix, 
     """
     TODO
     """
-    # Compute the set of qubits that are within `hops` steps on the adjacency graph of each qubit.
+    # Compute the set of qubits that are within `hops` steps on the adjacency graph of each qubit,
+    # by computing the Lapalacian and taking its `hops` power.
     degree_matrix = _np.diag(_np.sum(adjacency_matrix, axis = 1))
     laplacian = degree_matrix - adjacency_matrix
     laplace_power = _np.linalg.matrix_power(laplacian, hops)
     nodes_within_hops = [list(_np.arange(encoder.pspec.num_qubits)[abs(laplace_power[i, :]) > 0]) for i in range(encoder.pspec.num_qubits)]
-
+    #
+    # Init the list that this function will return, specifying the relevant encoding indices for each error generator in `error_generators`
     encoding_indices = []
     for error_generator in error_generators:
-        # The Pauli that labels the error gen, as a string of length num_qubits containing 'I', 'X', 'Y', and 'Z'.
+        # The Pauli that labels the error gen, as a string containing 'I', 'X', 'Y', and 'Z'.
         pauli_string = error_generator[1][0]
-        # TODO : IS THIS CORRECT ?
-        # I COMMENTED THIS OUT ON THE RE-WRITE, BECAUSE I THINK IT'S WRONG. NEED TO CHECK I HAVEN'T MADE A 
-        # MISTAKE THOUGH
-        pauli_string = pauli_string[::-1] # for reverse indexing
-
-        # The indices of `pauli` that are not equal to 'I'.
-        qubits_acted_on_by_error = _np.where(_np.array(list(pauli_string)) != 'I')[0]
-        qubits_acted_on_by_error = list(qubits_acted_on_by_error)
-
-        # All the qubits that are within `hops` steps of the qubits acted on by the error
+        # The following commented-out line is *wrong* but it used to be in the code, so leaving it here 
+        # but commented out for now. It is unclear if somehow this was the correct thing to do in older
+        # versions of the QPANN code before my (Tim's) rewrite.
+        # pauli_string = pauli_string[::-1] # for reverse indexing
+        #
+        # The indices of `pauli` that are not equal to 'I' are the qubits that the error acts on
+        qubits_acted_on_by_error = list(_np.where(_np.array(list(pauli_string)) != 'I')[0])
+        # All the qubits that are within `hops` steps on the graph of the qubits acted on by the error
         relevant_qubits = _np.unique(_np.concatenate([nodes_within_hops[i] for i in qubits_acted_on_by_error]))
-
-        relevant_encoding_indices_for_error_generator = encoder.indices_for_qubits(relevant_qubits)
-
-        encoding_indices.append(relevant_encoding_indices_for_error_generator)
+        # The encoding indices that encode what is happening to these qubits
+        relevant_encoding_indices = encoder.indices_for_qubits(relevant_qubits)
+        # Add to the list specifying the relevant encoding indices for each error generator in `error_generators`
+        encoding_indices.append(relevant_encoding_indices)
 
     return encoding_indices
-
-# FUTURE TODO : CONSIDER IF THESE NEED TO BE REVIVED OR NOT        
-# @_keras.utils.register_keras_serializable()
-# def layer_snipper_from_qubit_graph_with_lookback(error_gen, num_qubits, num_channels, qubit_graph_laplacian, 
-#                                                  num_hops, lookback=-1):
-#     """
-
-#     """
-#     encoding_indices_for_error = layer_snipper_from_qubit_graph(error_gen=error_gen, num_qubits=num_qubits, 
-#                                                                 num_channels=num_channels, 
-#                                                                 qubit_graph_laplacian= qubit_graph_laplacian,
-#                                                                 num_hops=num_hops)
-
-#     indices_for_error = []
-#     for relative_layer_index in range(lookback, 1):
-#         indices_for_error += [[relative_layer_index, i] for i in encoding_indices_for_error]
-
-#     return _np.array(indices_for_error)
-
-
-# # @_keras.utils.register_keras_serializable()
-# def layer_snipper_from_qubit_graph_with_simplified_lookback(error_gen, num_qubits, num_channels, qubit_graph_laplacian, 
-#                                                  num_hops, lookback=-1):
-#     """
-
-#     """
-#     encoding_indices_for_error = layer_snipper_from_qubit_graph(error_gen=error_gen, num_qubits=num_qubits, 
-#                                                                 num_channels=num_channels, 
-#                                                                 qubit_graph_laplacian= qubit_graph_laplacian,
-#                                                                 num_hops=num_hops)
-
-#     indices_for_error = []
-#     for i in range(0, _np.abs(lookback)):
-#         indices_for_error += list(encoding_indices_for_error + ((num_qubits * num_channels) * i))
-
-#     return _np.array(indices_for_error)
-    
