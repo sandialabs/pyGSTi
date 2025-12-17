604750a2a54f520efab40f24a5594013935f0de0
Riley Murray <rjmurr@sandia.gov>
2025-12-16 21:50:28 -0800

    update construct_leakage_report so that it can handle error bars

--- pygsti/tools/leakage.py
+++ pygsti/tools/leakage.py
@@ -535,16 +535,55 @@ def add_lago_models(results: ModelEstimateResults, est_key: Optional[str] = None
 
 # MARK: reports
 
+
+def _add_all_hessians(mer: ModelEstimateResults, kwargs_for_projhess=None):
+    # NOTE: this function is not leakage-specific.
+    if kwargs_for_projhess is None:
+        kwargs_for_projhess = {'projection_type': 'instrinsic error'}
+
+    from pygsti.forwardsims import MatrixForwardSimulator, MapForwardSimulator
+    for est in mer.estimates.values():
+        for gop_name in est.goparameters:
+            mdl = est.models[gop_name]
+            if isinstance(mdl.sim, MatrixForwardSimulator):
+                # Replace with MapForwardSimulator, since that's the only
+                # ForwardSimulator that can compute objective function 
+                # Hessians with a reasonable amount of memory.
+                mdl.sim = MapForwardSimulator
+            crf = est.add_confidence_region_factory(gop_name, 'final')
+            crf.compute_hessian()
+            crf.project_hessian(**kwargs_for_projhess)
+    return
+
+
+def _add_lago_estimates(mer: ModelEstimateResults, gaugeopt_verbosity: int = 0):
+    # This is broken out into its own function in case someone wants to use
+    # it in a report-generation function other than construct_leakage_report.
+    for ek in mer.estimates:
+        assert isinstance(ek, str)
+        if ek == 'Target':
+            # There's no need to gauge-optimize in this case.
+            continue
+        add_lago_models(mer, ek, verbosity=gaugeopt_verbosity)
+    return
+
+
 def construct_leakage_report(
         results : ModelEstimateResults,
         title : str = 'auto',
-        extra_report_kwargs : Optional[dict[str,Any]] = None,
-        gaugeopt_verbosity : int = 0,
+        *, # no positional args past "title."
+        confidence_level    : Optional[Union[int,float]] = None,
+        kwargs_projhess     : Optional[dict[str,Any]]    = None,
+        kwargs_stdreport    : Optional[dict[str,Any]]    = None,
+        gaugeopt_verbosity  : int = 0,
     ):
     """
-    This is a small wrapper around construct_standard_report. It generates a Report object
+    This is a wrapper around construct_standard_report. It generates a Report object
     with leakage analysis, and returns that object along with a copy of ``results`` which
     contains gauge-optimized models created during leakage analysis.
+    
+    If provided arguments indicate a desire for confidence intervals in the report,
+    then this function also computes the necessary likelihood function Hessians.
 
     Notes
     -----
@@ -553,30 +592,28 @@ def construct_leakage_report(
     reflects how the target gates in a leakage model are only _really_ defined on the
     computational subspace.
     """
+    if kwargs_stdreport is None:
+        kwargs_stdreport = dict()
 
-    if extra_report_kwargs is None:
-        extra_report_kwargs = {'title': title}
-    
-    if extra_report_kwargs.get('title', title) != title:
-        # Yes, we let you pass keyword arguments through to construct_standard_report,
-        # but that doesn't mean you should use that ability to pass in contradictory
-        # arguments to this function.
-        ktitle = extra_report_kwargs['title']
-        msg  = f"Replacing report title in extra_report_kwargs ({ktitle}) "
-        msg += f"with this function's title argument ({title})."
-        warnings.warn(msg)
-        extra_report_kwargs['title'] = title
-    
-    results = copy.deepcopy(results)
-    est_key = results.estimates.keys()
-    for ek in est_key:
-        assert isinstance(ek, str)
-        if ek == 'Target':
-            # There's no need to gauge-optimize in this case.
-            continue
-        add_lago_models(results, ek, verbosity=gaugeopt_verbosity)
+    # Prep work. Pack title and confidence_level into kwargs_stdreport.
+    clobbering_kwargs = {'title': title, 'confidence_level': confidence_level}
+    for k, a in clobbering_kwargs.items():
+        kwargs_stdreport[k] = kwargs_stdreport.get(k, a)
+        if a != (clobberable_a := kwargs_stdreport[k]):
+            msg  = f"Clobbering {k} in kwargs_stdreport ({clobberable_a}) "
+            msg += f"with this function's {k} argument ({a})."
+            warnings.warn(msg)
+            kwargs_stdreport[k] = a
+
+    # Actual work. Deep-copy results and mutate that object in-place.
+    results_out = copy.deepcopy(results)
+    _add_lago_estimates( results_out, gaugeopt_verbosity )
+    if kwargs_stdreport['confidence_level'] is not None:
+        _add_all_hessians( results_out, kwargs_projhess )
+
+    # Wrap it up in a bow.
     from pygsti.report import construct_standard_report
     report = construct_standard_report(
-        results, advanced_options={'n_leak': 1}, **extra_report_kwargs
+        results_out, advanced_options={'n_leak': 1}, **kwargs_stdreport
     )
-    return report, results
+    return report, results_out
