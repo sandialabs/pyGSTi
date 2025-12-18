46acef7393d202dd9a0202737fc48312637096f8
Juan G Mendoza <jgonz19@sandia.gov>
2025-12-18 11:46:38 -0700

    brought back exact AMS

--- pygsti/algorithms/modelselection.py
+++ pygsti/algorithms/modelselection.py
@@ -180,8 +180,7 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
         
     red_row_H = H
     red_rowandcol_H = H
-    parent_model_projector = np.eye(initial_model.num_params)
-    deltalogl_model_projector = parent_model_projector.copy()
+    deltalogl_model_projector = np.eye(initial_model.num_params)
     
     approx_logl_fn = create_approx_logl_fn(H, expansion_point_x0, prev_dlogl)
     exceeded_threshold = False
@@ -202,8 +201,7 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
         
             vec = _reduced_model_approx_GST_fast(red_rowandcol_H, red_row_H, expansion_point_x0, i)
 
-            #reduced_model_projector_matrix = np.delete(parent_model_projector, i, axis=1)
-            deltalogl_fn.model.from_vector(np.delete(parent_model_projector, i, axis=1) @ vec)
+            deltalogl_fn.model.from_vector(np.delete(deltalogl_model_projector, i, axis=1) @ vec)
 
             quantity = (deltalogl_fn.fn()- prev_dlogl)*2
             if  verbosity > 1:
@@ -218,10 +216,9 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
 
         if (left_over_start_index + rank) < num_total_models:
 
-            i = left_over_start_index + rank
-            #reduced_model_projector_matrix = np.delete(parent_model_projector, i, axis=1)     
+            i = left_over_start_index + rank 
             vec = _reduced_model_approx_GST_fast(red_rowandcol_H, red_row_H, expansion_point_x0, i)
-            deltalogl_fn.model.from_vector(np.delete(parent_model_projector, i, axis=1) @ vec)
+            deltalogl_fn.model.from_vector(np.delete(deltalogl_model_projector, i, axis=1) @ vec)
 
             quantity = (deltalogl_fn.fn() - prev_dlogl )*2
             
@@ -273,7 +270,6 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
             H = pygsti.tools.logl_hessian(red_model_fit, data.dataset, comm=comm, mem_limit=mem_limit, verbosity = verbosity)
             if comm is not None:
                 H = comm.bcast(H, root = 0)
-                
                 comm.free_all_children()
 
             expansion_point_x0 = red_model_fit.to_vector().copy()
@@ -335,14 +331,10 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
             if recompute_Hessian:
                 recompute_Hessian = False
                 temp_model = None
-                parent_model_projector = np.eye(len(expansion_point_x0))
                 deltalogl_model_projector = np.eye(len(expansion_point_x0))
                 
             else:
-                parent_model_projector = np.delete(parent_model_projector, best_model[2], axis=1)
                 deltalogl_model_projector = np.delete(deltalogl_model_projector, best_model[2], axis=1)
-                #TODO if assertion below is not raised get rid of parent_model_projector and replace with deltalogl_projector
-                assert np.allclose(deltalogl_model_projector, parent_model_projector), 'We cannot get rid of this variable'
                 red_row_H = np.delete(red_row_H, best_model[2], axis=0)
                 red_rowandcol_H = np.delete(np.delete(red_rowandcol_H,best_model[2] , axis=0), best_model[2], axis=1)
 
@@ -360,3 +352,205 @@ def do_greedy_from_full_fast(initial_model, data, er_thresh=2.0, verbosity=2, ma
     return graph_levels
 
 
+
+def do_greedy_from_full_exact(initial_model, data, er_thresh=2.0, verbosity=2, maxiter=100, tol=1e-7, prob_clip=1e-3, comm = None, mem_limit = None):
+    """
+    An automated model selection greedy algorithm. Specifically made for FOGI models, but it should be compatible
+    with any model that has a linear interposer. It is considered "exact" because every model considered is found through GST, as opposed to
+    some MLE approximations. Because of this, calling this function can yield O(initial_model.num_params^2) GST calculations (expensive!!).
+    
+    The high level picture of the algorithm goes as follows:
+    
+        1) Do a GST fit on full_model
+
+        2) Consider a list of full_model.num_params reduced models of full_model, each missing a single and unique parameter.
+
+        3) Find the GST fit of every model in the list constructed in step (2). If multiple processes
+        are available, the list of reduced models to be evaluated gets evenly split amongst them.
+
+        4) Grab the reduced model with the lowest difference in MLE (MLE(full_model) - MLE(red_model)), use this as your full_model
+        and go back to step 1. If this difference is greater than er_thresh/2, then we stop.
+
+    Parameters
+    ----------
+    full_model : Model
+
+        pyGSTi model with a linear interposer to be used as a starting point to AMS. Reduced models
+        are created from this model by removing a single parameter at a time.
+
+    data :  ProtocolData
+        The input data to be used to fit models 
+
+    er_thresh : float, optional
+        The threshold that determines how much likelihood we are willing to lose per 
+        parameter removed. In other words, if after removing a parameter, all resulting model fits
+        are above the treshold
+            L(full_model) - L(red_models) > er_thresh/2
+        Then the algorithm rejects all current models and stops. This is called the "evidence ratio"
+        within the field of model selection. If not provided, it is set to the Akaike information
+        criterion (2).
+
+    verbosity : int, optional
+        Amount of detail to print to stdout.
+        
+    maxiter : int
+        The maximum number of (outer) interations for the optimizer.
+
+    tol : float
+        Tolerance value used for optimization algorithms. This parameters will index the 'f' tolerance
+        (also called f_norm2_tol) in optimization subroutines, described as follows:
+            Tolerace for `F^2` where `F = `norm( sum(obj_fn(x)**2) )` is the
+            least-squares residual.  If `F**2 < f_norm2_tol`, then mark converged.  
+
+    prob_clip : float
+        The minimum probability treated normally in the evaluation of the log-likelihood.
+        A penalty function replaces the true log-likelihood for probabilities that lie
+        below this threshold so that the log-likelihood never becomes undefined (which improves
+        optimizer performance).
+
+    graph_checkpoint : TODO this will likely get heavily modified
+
+    Returns
+    -------
+    trace:
+        A list of every level computed within AMS, each level i contains the reduced model with the best evidence ratio
+        with i parameters removed from the full model
+    trace[i][j]
+        
+        i: indexes AMS levels, where each level i contains the best model found after removing i parameters
+
+        j: indexes different characteristics for the jth best model at level i
+            [param_vec , evidence_ratio, parameter_that_was_removed]
+
+    lowest_vec : numpy array
+        vector corresponding to the fitted best reduced model found at its corresponding level
+    """
+    new_checkpoint = None
+    if comm is not None:
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+    else:
+        rank = 0
+        size = 1
+
+    graph_levels = []
+    initial_model.param_interposer.full_span_inv_transform_matrix = initial_model.param_interposer.inv_transform_matrix
+    initial_model.param_interposer.inv_transform_matrix_projector = np.eye(initial_model.num_params)
+    builders = _custom_builders(prob_clip)
+    deltalogl_fn =  builders.final_builders[0].build(initial_model, data.dataset, list(data.dataset.keys()))
+    H = None
+    original_dlogl = None
+
+    if original_dlogl is None:
+        if rank == 0: 
+            if verbosity > 0:
+                print('starting GST ', size)
+        
+            start = time.time()
+
+            deltalogl_fn.model.from_vector(_parallel_GST(initial_model, data, builders, tol, maxiter, verbosity).estimates['GateSetTomography'].models['final iteration estimate'].to_vector().copy())
+        
+        if comm is not None:
+            deltalogl_fn.model.from_vector(comm.bcast(deltalogl_fn.model.to_vector(), root = 0))
+        
+        
+        if comm is not None:
+            comm.free_all_children()
+        original_dlogl = deltalogl_fn.fn()
+        prev_dlogl = original_dlogl
+    
+    if H is None:
+        if rank == 0:
+            if verbosity > 0: 
+                print("computing Hessian", flush=True)        
+            H = pygsti.tools.logl_hessian(deltalogl_fn.model, data.dataset, verbosity = verbosity)
+
+        if comm is not None and rank == 1:
+            comm.free_all_children()
+
+        if comm is not None:
+            H = comm.bcast(H, root = 0)
+
+    if len(graph_levels) == 0:
+        print(f'{original_dlogl=}')
+        graph_levels.append([deltalogl_fn.model.to_vector(),original_dlogl, 0])
+        
+    deltalogl_model_projector = np.eye(deltalogl_fn.model.num_params)
+    reduced_model = deltalogl_fn.model.copy()
+    
+    exceeded_threshold = False
+    while not exceeded_threshold:
+
+        if rank == 0 and verbosity > 0:
+            print(f'>> Working on level {len(graph_levels)} <<',flush = True)
+            start = time.time()
+
+        num_total_models = len(graph_levels[-1][0])
+        bucket_size = num_total_models // size
+        chunk_range = range(rank*bucket_size, (rank+1)*bucket_size)
+        level_chunk = []
+        for i in chunk_range:
+            
+            vec = _parallel_GST(remove_param(reduced_model, i), data, builders, maxiter=maxiter, tol=tol, verbosity=0).estimates['GateSetTomography'].models['final iteration estimate'].to_vector()
+
+            deltalogl_fn.model.from_vector(np.delete(deltalogl_model_projector, i, axis=1) @ vec)
+
+            quantity = (deltalogl_fn.fn()- prev_dlogl)*2
+            if  verbosity > 1:
+                if i  % 5 == 0:
+                    print('Model ', i, ' has ev. ratio of ', quantity, flush=True)
+            level_chunk.append([vec, quantity, i])
+
+            
+        left_over_start_index = size * bucket_size
+
+        if (left_over_start_index + rank) < num_total_models:
+
+            i = left_over_start_index + rank   
+            vec = _parallel_GST(remove_param(reduced_model, i), data, builders, maxiter=maxiter, tol=tol, verbosity=0).estimates['GateSetTomography'].models['final iteration estimate'].to_vector()
+            deltalogl_fn.model.from_vector(np.delete(deltalogl_model_projector, i, axis=1) @ vec)
+            quantity = (deltalogl_fn.fn() - prev_dlogl )*2
+            level_chunk.append([vec, quantity, i])
+        
+        if comm is not None:
+            level_raw = comm.gather(level_chunk, root = 0)
+            level = [item for sublist in level_raw for item in sublist]
+
+        else:
+            level = level_chunk
+        if rank == 0:
+            #Purge out all entries from processes that did not compute anything
+            level = [candidate for candidate in level if candidate[2] != -1]
+            
+            best_model = sorted(level, key=lambda x: x[1])[0]
+        else:
+            best_model = None
+        if comm is not None:
+            best_model = comm.bcast(best_model, root = 0)
+
+        deltalogl_fn.model.from_vector(np.delete(deltalogl_model_projector, best_model[2], axis=1) @ best_model[0])
+
+        if verbosity and rank == 0:
+            print(f'Model {best_model[2]} has lowest evidence ratio {best_model[1]:.4f}')
+        
+        if best_model[1] > er_thresh or len(best_model[0]) == 1:
+                if rank == 0 and verbosity > 0:
+                    print('All models from this level exceeded evidence ratio threshold, model rejected. Stopping!')
+                exceeded_threshold = True
+                if rank == 0 and verbosity > 0:
+                    print('Removed ', len(graph_levels)-1, ' parameters')
+                final_costs = [candidate[1] for candidate in level]
+
+        else:
+            prev_dlogl = prev_dlogl + best_model[1]/2
+            graph_levels.append(best_model)
+                
+            deltalogl_model_projector = np.delete(deltalogl_model_projector, best_model[2], axis=1)
+            reduced_model = remove_param(reduced_model, i)
+
+        if rank == 0 and exceeded_threshold == False and verbosity > 1:
+            print("next level")
+            end = time.time()
+            print('time this level ', end-start)
+
+    return graph_levels, final_costs
