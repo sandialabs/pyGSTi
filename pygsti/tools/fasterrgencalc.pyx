fb5fff599ad0a12ac63cb8d110d3bbbf2de34f40
Corey Ostrove <ciostro@sandia.gov>
2026-04-13 21:51:45 -0600

    typo fix

--- pygsti/tools/fasterrgencalc.pyx
+++ pygsti/tools/fasterrgencalc.pyx
@@ -899,7 +899,7 @@ cpdef np.ndarray[np.complex128_t, ndim=1] fast_bulk_phi(object tableau, str desi
     # Cache amplitude_of_state for each unique bitstring.
     cdef dict cached_amplitudes = {}
     cdef str bitstr
-    cdef np.ndarray[np.complex128_t, ndim=1] unique_amplitudes = bulk_amplitude_of_state(tableau, unique_bitstrings, True)
+    cdef np.ndarray[np.complex128_t, ndim=1] unique_amplitudes = fast_bulk_amplitude_of_state(tableau, unique_bitstrings, True)
     cdef np.complex128_t[::1] unique_amplitudes_view = unique_amplitudes
     cdef int num_unique_amplitudes = len(unique_amplitudes)
 
